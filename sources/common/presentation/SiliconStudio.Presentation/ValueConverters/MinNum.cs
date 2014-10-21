b8ddb6e7ea77eafd34806e5359f9e225c1e37b99
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2014-10-21 16:21:30 +0900

    Merge remote-tracking branch 'origin/master' into editor
    Conflicts:
    	deps

---
+++ sources/common/presentation/SiliconStudio.Presentation/ValueConverters/MinNum.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace SiliconStudio.Presentation.ValueConverters
+{
+    /// <summary>
+    /// This converter will return the minimal result between the converter value and the converter parameter.
+    /// </summary>
+    public class MinNum : OneWayValueConverter<MinNum>
+    {
+        /// <inheritdoc/>
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doubleValue = (double)System.Convert.ChangeType(value ?? 0, typeof(double));
+            var doubleParameter = (double)System.Convert.ChangeType(parameter ?? 0, typeof(double));
+            return System.Convert.ChangeType(Math.Min(doubleValue, doubleParameter), value != null ? value.GetType() : targetType);
+        }
+    }
+}
