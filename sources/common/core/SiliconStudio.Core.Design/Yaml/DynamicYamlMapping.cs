c891c165f9901dc47775c1a91ad0180897a12c82
Virgile Bello <virgile@siliconstudio.co.jp>
2014-11-05 18:40:43 +0900

    [Core.Design] Added Dynamic Yaml objects for easier asset upgrade.

---
+++ sources/common/core/SiliconStudio.Core.Design/Yaml/DynamicYamlMapping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Dynamic;
+using SharpYaml.Serialization;
+
+namespace SiliconStudio.Core.Yaml
+{
+    /// <summary>
+    /// Dynamic version of <see cref="YamlMappingNode"/>.
+    /// </summary>
+    public class DynamicYamlMapping : DynamicYamlObject
+    {
+        internal YamlMappingNode node;
+
+        public DynamicYamlMapping(YamlMappingNode node)
+        {
+            this.node = node;
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            if (binder.Type.IsAssignableFrom(node.GetType()))
+            {
+                result = node;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+            return true;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = GetValue(new YamlScalarNode(binder.Name));
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            var key = new YamlScalarNode(binder.Name);
+
+            if (value is DynamicYamlEmpty)
+                node.Children.Remove(key);
+            else
+                node.Children[key] = ConvertFromDynamic(value);
+            return true;
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            var key = ConvertFromDynamic(indexes[0]);
+            if (value is DynamicYamlEmpty)
+                node.Children.Remove(key);
+            else
+                node.Children[key] = ConvertFromDynamic(value);
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            var key = ConvertFromDynamic(indexes[0]);
+            result = GetValue(key);
+            return true;
+        }
+
+        private object GetValue(YamlNode key)
+        {
+            YamlNode result;
+            if (node.Children.TryGetValue(key, out result))
+            {
+                return ConvertToDynamic(result);
+            }
+            return null;
+        }
+    }
+}
---
+++ sources/common/core/SiliconStudio.Core.Design/Yaml/DynamicYamlObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+using SharpYaml.Serialization;
+
+namespace SiliconStudio.Core.Yaml
+{
+    public abstract class DynamicYamlObject : DynamicObject
+    {
+        protected static YamlNode ConvertFromDynamic(object obj)
+        {
+            if (obj == null)
+                return new YamlScalarNode("null");
+
+            if (obj is YamlNode)
+                return (YamlNode)obj;
+
+            if (obj is DynamicYamlMapping)
+                return ((DynamicYamlMapping)obj).node;
+            if (obj is DynamicYamlArray)
+                return ((DynamicYamlArray)obj).node;
+            if (obj is DynamicYamlScalar)
+                return ((DynamicYamlScalar)obj).node;
+
+            return new YamlScalarNode(String.Format(CultureInfo.InvariantCulture, "{0}", obj));
+        }
+
+        protected static object ConvertToDynamic(object obj)
+        {
+            if (obj is YamlScalarNode)
+                return new DynamicYamlScalar((YamlScalarNode)obj);
+            if (obj is YamlMappingNode)
+                return new DynamicYamlMapping((YamlMappingNode)obj);
+            if (obj is YamlSequenceNode)
+                return new DynamicYamlArray((YamlSequenceNode)obj);
+
+            return obj;
+
+        }
+    }
+}
