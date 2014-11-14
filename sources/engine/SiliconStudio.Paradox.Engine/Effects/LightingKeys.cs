96f1f6c15c00b427be168f5207baa81a89193d93
Aurelien Serandour <aurelien.serandour@siliconstudio.co.jp>
2014-11-14 20:44:41 +0900

    [Assets] user documentation for lighting configuration asset PDX-1206 user documentation for effect library asset PDX-1203

--- sources/engine/SiliconStudio.Paradox.Engine/Effects/LightingKeys.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Effects/LightingKeys.cs
@@ -14,61 +14,81 @@ namespace SiliconStudio.Paradox.Effects
         /// <summary>
         /// Maximum number of directional lights.
         /// </summary>
+        /// <userdoc>
+        /// Maximum number of supported directional lights.
+        /// </userdoc>
         public static readonly ParameterKey<int> MaxDirectionalLights = ParameterKeys.New(0);
         
         /// <summary>
         /// Maximum number of point lights.
         /// </summary>
+        /// <userdoc>
+        /// Maximum number of supported point lights.
+        /// </userdoc>
         public static readonly ParameterKey<int> MaxPointLights = ParameterKeys.New(0);
 
         /// <summary>
         /// Maximum number of spot lights.
         /// </summary>
+        /// <userdoc>
+        /// Maximum number of supported spot lights.
+        /// </userdoc>
         public static readonly ParameterKey<int> MaxSpotLights = ParameterKeys.New(0);
 
         /// <summary>
         /// A flag stating if directional light loop should be unrolled.
         /// </summary>
+        /// <userdoc>
+        /// When checked, the effect will support exactly the set number of directional lights.
+        /// </userdoc>
         public static readonly ParameterKey<bool> UnrollDirectionalLightLoop = ParameterKeys.New(false);
 
         /// <summary>
         /// A flag stating if point light loop should be unrolled.
         /// </summary>
+        /// <userdoc>
+        /// When checked, the effect will support exactly the set number of point lights.
+        /// </userdoc>
         public static readonly ParameterKey<bool> UnrollPointLightLoop = ParameterKeys.New(false);
 
         /// <summary>
         /// A flag stating if spot light loop should be unrolled.
         /// </summary>
+        /// <userdoc>
+        /// When checked, the effect will support exactly the set number of spot lights.
+        /// </userdoc>
         public static readonly ParameterKey<bool> UnrollSpotLightLoop = ParameterKeys.New(false);
 
         /// <summary>
         /// Flag stating if the mesh casts shadows.
         /// </summary>
+        /// <userdoc>
+        /// When checked, the mesh will cast shadows.
+        /// </userdoc>
         public static readonly ParameterKey<bool> CastShadows = ParameterKeys.New(false);
 
         /// <summary>
         /// Flag stating if the mesh receives shadows.
         /// </summary>
+        /// <userdoc>
+        /// When checked, the mesh will receive shadows.
+        /// </userdoc>
         public static readonly ParameterKey<bool> ReceiveShadows = ParameterKeys.New(false);
 
         /// <summary>
-        /// Maximum number of supported nearest filtered shadows.
-        /// </summary>
-        public static readonly ParameterKey<int> MaxDirectionalNearestFilterShadowMap = ParameterKeys.New(0);
-
-        /// <summary>
-        /// Maximum number of supported Pcf shadows.
-        /// </summary>
-        public static readonly ParameterKey<int> MaxDirectionalPcfFilterShadowMap = ParameterKeys.New(0);
-        
-        /// <summary>
         /// Maximum number of deferred lights.
         /// </summary>
+        /// <userdoc>
+        /// Maximum number of deferred point lights.
+        /// </userdoc>
         public static readonly ParameterKey<int> MaxDeferredLights = ParameterKeys.New(64);
 
         /// <summary>
         /// A flag stating if deferred light loop should be unrolled.
         /// </summary>
+        /// <userdoc>
+        /// When checked, the deferred effect will support exactly the set number of point lights.
+        /// </userdoc>
         public static readonly ParameterKey<bool> UnrollDeferredLightLoop = ParameterKeys.New(false);
     }
 }
