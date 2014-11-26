63a655afc371c1ac624d7fa4d3037ca10a45f85f
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-11-26 23:10:55 +0900

    [Shaders] Use ParameterKey permutations for GaussianBlur

--- sources/engine/SiliconStudio.Paradox.Assets/Effect/Generators/DefaultCompilerParametersGenerator.cs
+++ sources/engine/SiliconStudio.Paradox.Assets/Effect/Generators/DefaultCompilerParametersGenerator.cs
@@ -1,9 +1,14 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
+
 using SiliconStudio.Assets.Compiler;
 using SiliconStudio.Core.Diagnostics;
 using SiliconStudio.Paradox.Effects;
+using SiliconStudio.Paradox.Shaders;
 using SiliconStudio.Paradox.Shaders.Compiler;
 
 namespace SiliconStudio.Paradox.Assets.Effect.Generators
@@ -24,9 +29,87 @@ namespace SiliconStudio.Paradox.Assets.Effect.Generators
             }
         }
 
-        public IEnumerable<CompilerParameters> Generate(AssetCompilerContext context, CompilerParameters baseParameters, ILogger log)
+        public IEnumerable<CompilerParameters> Generate(AssetCompilerContext context, CompilerParameters parameters, ILogger log)
         {
-            return new List<CompilerParameters>() { baseParameters.Clone() };
+            var effectName = parameters.Get(EffectKeys.Name);
+            if (effectName != null)
+            {
+                List<ParameterKey> keys = null;
+                FindAllKeys(effectName, ref keys);
+
+                if (keys != null)
+                {
+                    var cloneParameters = parameters.Clone();
+                    foreach (var newParams in GeneratePermutations(keys, 0, cloneParameters))
+                    {
+                        yield return newParams;
+                    }
+
+                    yield break;
+                }
+            }
+
+            yield return parameters.Clone();
+        }
+
+        private IEnumerable<CompilerParameters> GeneratePermutations(List<ParameterKey> keys, int fromIndex, CompilerParameters parameters)
+        {
+            var key = keys[fromIndex];
+            var values = key.Metadatas.OfType<ParameterKeyPermutationsMetadata>().First().Values;
+            foreach (var value in values)
+            {
+                parameters.SetObject(key, value);
+                var nextKeyIndex = fromIndex + 1;
+                if (nextKeyIndex < keys.Count)
+                {
+                    foreach (var subParams in GeneratePermutations(keys, nextKeyIndex, parameters))
+                    {
+                        yield return subParams;
+                    }
+                }
+                else
+                {
+                    yield return parameters.Clone();
+                }
+            }
+        }
+
+        private void FindAllKeys(string effectName, ref List<ParameterKey> keys)
+        {
+            if (effectName == null) throw new ArgumentNullException("effectName");
+            IShaderMixinBuilder builder;
+            if (ShaderMixinManager.TryGet(effectName, out builder))
+            {
+                FindAllKeys(builder, ref keys);
+            }
+        }
+
+        private void FindAllKeys(IShaderMixinBuilder builder, ref List<ParameterKey> keys)
+        {
+            if (builder == null) throw new ArgumentNullException("builder");
+            var extendedBuilder = builder as IShaderMixinBuilderExtended;
+            if (extendedBuilder == null)
+            {
+                return;
+            }
+
+            foreach (var mixin in extendedBuilder.Mixins)
+            {
+                FindAllKeys(mixin, ref keys);
+            }
+
+            foreach (var key in extendedBuilder.Keys.Where(key => key.Metadatas.OfType<ParameterKeyPermutationsMetadata>().Any()))
+            {
+                if (keys == null)
+                {
+                    keys = new List<ParameterKey>();
+                }
+
+                if (!keys.Contains(key))
+                {
+                    keys.Add(key);
+                }
+            }
         }
     }
 }
--- sources/shaders/Images/GaussianBlur/GaussianBlur.cs
+++ sources/shaders/Images/GaussianBlur/GaussianBlur.cs
@@ -1,15 +1,28 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
+using System.Linq;
+
 using SiliconStudio.Core;
 
 namespace SiliconStudio.Paradox.Effects.Images
 {
     public class GaussianBlur : ImageEffectBase
     {
-        internal static readonly ParameterKey<int> RadiusKey = ParameterKeys.New<int>();
-        internal static readonly ParameterKey<bool> VerticalBlurKey  = ParameterKeys.New<bool>();
-        internal static readonly ParameterKey<float> SigmaRatioKey = ParameterKeys.New<float>();
+        internal static readonly ParameterKey<int> RadiusKey;
+
+        internal static readonly ParameterKey<bool> VerticalBlurKey;
+
+        internal static readonly ParameterKey<float> SigmaRatioKey;
+
+        static GaussianBlur()
+        {
+            var defaultRadius = Enumerable.Range(1, 20).Select(value => (object)value).ToArray();
+
+            RadiusKey = ParameterKeys.New<int>(0, "GaussianBlur.RadiusKey", new ParameterKeyPermutationsMetadata(defaultRadius));
+            VerticalBlurKey = ParameterKeys.New<bool>(false, "GaussianBlur.VerticalBlurKey", new ParameterKeyPermutationsMetadata(true, false));
+            SigmaRatioKey = ParameterKeys.New<float>(2.0f, "GaussianBlur.SigmaRatioKey", new ParameterKeyPermutationsMetadata(2.0f, 3.0f));
+        }
 
         private readonly ImageEffect blurH;
         private readonly ImageEffect blurV;
