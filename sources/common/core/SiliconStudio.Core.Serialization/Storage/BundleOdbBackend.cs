c73fd044ef449305ffcac5c2742923b441a7d780
Virgile Bello <virgile@siliconstudio.co.jp>
2014-11-26 20:21:44 +0900

    [Core] Storage: Add missing using.

--- sources/common/core/SiliconStudio.Core.Serialization/Storage/BundleOdbBackend.cs
+++ sources/common/core/SiliconStudio.Core.Serialization/Storage/BundleOdbBackend.cs
@@ -2,6 +2,7 @@
 // This file is distributed under GPL v3. See LICENSE.md for details.
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
