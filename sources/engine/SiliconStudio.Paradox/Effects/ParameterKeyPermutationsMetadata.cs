151fe4f72b0fb7329baf675717ea29cf6f27330d
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-11-26 23:20:08 +0900

    [Effects] Add comment to ParameterKeyPermutationsMetadata

--- sources/engine/SiliconStudio.Paradox/Effects/ParameterKeyPermutationsMetadata.cs
+++ sources/engine/SiliconStudio.Paradox/Effects/ParameterKeyPermutationsMetadata.cs
@@ -5,13 +5,26 @@ using SiliconStudio.Core;
 
 namespace SiliconStudio.Paradox.Effects
 {
+    /// <summary>
+    /// Class ParameterKeyPermutationsMetadata.
+    /// </summary>
     public class ParameterKeyPermutationsMetadata : PropertyKeyMetadata
     {
+        // TODO: Should we provide a callback GetValues() instead?
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParameterKeyPermutationsMetadata"/> class.
+        /// </summary>
+        /// <param name="values">The values.</param>
         public ParameterKeyPermutationsMetadata(params object[] values)
         {
             Values = values;
         }
 
+        /// <summary>
+        /// Gets the values.
+        /// </summary>
+        /// <value>The values.</value>
         public object[] Values { get; private set; }
     }
 }
