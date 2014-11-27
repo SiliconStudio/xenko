a5f97a6d1954e24e40e4a2162a1b8a9b8092278c
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-11-27 13:11:45 +0900

    [Effects] Remove usage of permutations in GaussianBlur

--- sources/shaders/Images/GaussianBlur/GaussianBlurKeys.cs
+++ sources/shaders/Images/GaussianBlur/GaussianBlurKeys.cs
@@ -10,23 +10,10 @@ namespace SiliconStudio.Paradox.Effects.Images
     /// </summary>
     internal static class GaussianBlurKeys
     {
-        public static readonly ParameterKey<int> Radius;
+        public static readonly ParameterKey<int> Radius = ParameterKeys.New<int>();
 
-        public static readonly ParameterKey<bool> VerticalBlur;
+        public static readonly ParameterKey<bool> VerticalBlur = ParameterKeys.New<bool>();
 
-        public static readonly ParameterKey<float> SigmaRatio;
-
-        static GaussianBlurKeys()
-        {
-            // Predefine supported radius from [1,20]
-            var defaultRadius = Enumerable.Range(1, 20).Select(value => (object)value).ToArray();
-            Radius = ParameterKeys.NewWithMetas<int>(new ParameterKeyPermutationsMetadata(defaultRadius));
-
-            // Predefine vertical and horizontal blur
-            VerticalBlur = ParameterKeys.NewWithMetas<bool>(new ParameterKeyPermutationsMetadata(true, false));
-
-            // Predefine SigmaRatio to 2.0f and 3.0f
-            SigmaRatio = ParameterKeys.NewWithMetas<float>(new ParameterKeyPermutationsMetadata(2.0f, 3.0f));
-        }
+        public static readonly ParameterKey<float> SigmaRatio = ParameterKeys.New<float>();
     }
 }
