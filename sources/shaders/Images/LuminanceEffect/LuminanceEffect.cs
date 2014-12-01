da2af3423ddf3e09d99eaf7bcc5cb0189fd2bb69
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-12-01 23:01:55 +0900

    [Effects] Add some comments to  LuminanceEffect

--- sources/shaders/Images/LuminanceEffect/LuminanceEffect.cs
+++ sources/shaders/Images/LuminanceEffect/LuminanceEffect.cs
@@ -10,6 +10,9 @@ using SiliconStudio.Paradox.Graphics;
 
 namespace SiliconStudio.Paradox.Effects.Images
 {
+    /// <summary>
+    /// Luminance effect.
+    /// </summary>
     public class LuminanceEffect : ImageEffectBase
     {
         private readonly ImageEffect luminanceLogEffect;
@@ -19,7 +22,12 @@ namespace SiliconStudio.Paradox.Effects.Images
         private readonly bool[] luminanceStagingUsed;
         private int currentLuminanceIndex;
         private readonly Half[] mapValue = new Half[1];
+        private readonly List<RenderTarget> tempMipmaps;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LuminanceEffect"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
         public LuminanceEffect(ImageEffectContext context) : base(context)
         {
             luminanceLogEffect = new ImageEffect(Context, "LuminanceLogEffect");
@@ -35,25 +43,57 @@ namespace SiliconStudio.Paradox.Effects.Images
                 luminanceStagingUsed[i] = false;
             }
 
+            tempMipmaps =  new List<RenderTarget>();
+
             blur = new GaussianBlur(context).DisposeBy(this);
             blur.Radius = 4;
         }
 
         public int UpscaleCount { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to enable calculation of <see cref="AverageLuminance"/> (default is false).
+        /// </summary>
+        /// <value><c>true</c> if to enable calculation of <see cref="AverageLuminance"/>; otherwise, <c>false</c>.</value>
+        public bool EnableAverageLuminanceReadback { get; set; }
+
+        /// <summary>
+        /// Gets the average luminance calculated on the GPU. See remarks.
+        /// </summary>
+        /// <value>The average luminance.</value>
+        /// <remarks>
+        /// The average luminance is calculated on the GPU and readback with a few frames of delay, depending on the number of 
+        /// frames in advance between command scheduling and actual execution on GPU.
+        /// </remarks>
         public float AverageLuminance { get; private set; }
+
+        /// <summary>
+        /// Gets the average luminance 1x1 texture available after drawing this effect.
+        /// </summary>
+        /// <value>The average luminance texture.</value>
+        public Texture AverageLuminanceTexture
+        {
+            get
+            {
+                return luminance1x1;
+            }
+        }
 
         protected override void DrawCore()
         {
-
             var inputTexture = GetSafeInput(0);
+            var outputRenderTexture = GetOutput(0);
 
-            var outputRenderTexture = GetSafeOutput(0);
-            var lastSize = outputRenderTexture.Size;
-            for (int i = 0; i < UpscaleCount; i++)
-                lastSize = Texture.NextMip(lastSize);
+            // If no output, we are only calculating the average luminance.
+            var lastSize = new Size3(1, 1, 1);
+            if (outputRenderTexture != null)
+            {
+                lastSize = outputRenderTexture.Size;
+                for (int i = 0; i < UpscaleCount; i++)
+                    lastSize = Texture.NextMip(lastSize);
+            }
 
-            var tempMipmaps = new List<RenderTarget>();
+            tempMipmaps.Clear();
             var luminanceMap = NewScopedRenderTarget2D(inputTexture.Width, inputTexture.Height, PixelFormat.R16_Float, 1);
             tempMipmaps.Add(luminanceMap);
 
@@ -65,7 +105,7 @@ namespace SiliconStudio.Paradox.Effects.Images
             var mipCount = Texture.CalculateMipLevels(inputTexture.Width, inputTexture.Height, 0) - 1;
 
             var nextSize = inputTexture.Size;
-            int upscaleBaseIndex = 1;
+            int upscaleBaseIndex = 0;
             for (int i = 0; i < mipCount; i++)
             {
                 nextSize = Texture.NextMip(nextSize);
@@ -77,9 +117,9 @@ namespace SiliconStudio.Paradox.Effects.Images
                 }
                 else
                 {
-                    if (upscaleBaseIndex == 1 && nextSize.Width <= lastSize.Width && nextSize.Height <= lastSize.Height)
+                    if (upscaleBaseIndex <= 1 && nextSize.Width <= lastSize.Width && nextSize.Height <= lastSize.Height)
                     {
-                        upscaleBaseIndex += i;
+                        upscaleBaseIndex = i + 1;
                     }
 
                     var renderTarget2D = NewScopedRenderTarget2D(nextSize.Width, nextSize.Height, PixelFormat.R16_Float, 1);
@@ -93,23 +133,29 @@ namespace SiliconStudio.Paradox.Effects.Images
                 Scaler.Draw("Down/2");
             }
 
-            // Copy to staging resource
-            GraphicsDevice.Copy(luminance1x1, luminancesStaging[currentLuminanceIndex]);
-            luminanceStagingUsed[currentLuminanceIndex] = true;
-
-            // Read-back to CPU using a ring of staging buffers
-            for (int i = luminancesStaging.Length - 1; i >= 1; i--)
+            // Calculate average luminance only if needed
+            if (EnableAverageLuminanceReadback)
             {
-                var oldStagingIndex = (currentLuminanceIndex + i) % luminancesStaging.Length;
-                var oldLuminanceStaging = luminancesStaging[oldStagingIndex];
-                if (luminanceStagingUsed[oldStagingIndex] && oldLuminanceStaging.GetData(mapValue, 0, 0, true))
+                // TODO: This code could be moved to a general readback Effect utility.
+
+                // Copy to staging resource
+                GraphicsDevice.Copy(luminance1x1, luminancesStaging[currentLuminanceIndex]);
+                luminanceStagingUsed[currentLuminanceIndex] = true;
+
+                // Read-back to CPU using a ring of staging buffers
+                for (int i = luminancesStaging.Length - 1; i >= 1; i--)
                 {
-                    AverageLuminance = (float)Math.Pow(2.0, mapValue[0]);
-                    //Debug.WriteLine(string.Format("Buffer {0}:{1} Lum: {2}", currentLuminanceIndex, oldStagingIndex, AverageLuminance));
-                    break;
+                    var oldStagingIndex = (currentLuminanceIndex + i) % luminancesStaging.Length;
+                    var oldLuminanceStaging = luminancesStaging[oldStagingIndex];
+                    if (luminanceStagingUsed[oldStagingIndex] && oldLuminanceStaging.GetData(mapValue, 0, 0, true))
+                    {
+                        AverageLuminance = (float)Math.Pow(2.0, mapValue[0]);
+                        //Debug.WriteLine(string.Format("Buffer {0}:{1} Lum: {2}", currentLuminanceIndex, oldStagingIndex, AverageLuminance));
+                        break;
+                    }
                 }
+                currentLuminanceIndex = (currentLuminanceIndex + 1) % luminancesStaging.Length;
             }
-            currentLuminanceIndex = (currentLuminanceIndex + 1) % luminancesStaging.Length;
 
             // Upscale only if output is larger
             if (upscaleBaseIndex > 1 && UpscaleCount > 0)
