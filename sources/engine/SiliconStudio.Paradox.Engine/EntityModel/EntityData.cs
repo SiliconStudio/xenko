b1217565d8b546afc483222a0c6845a1d764575e
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2014-12-02 15:07:24 +0900

    [Engine] Added a GetTransformation method to retrieve (or possibly create) the TransformationComponentData of an EntityData + added a bit of documentation in EntityData

--- sources/engine/SiliconStudio.Paradox.Engine/EntityModel/EntityData.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/EntityModel/EntityData.cs
@@ -4,32 +4,74 @@ using System;
 using System.Linq;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Collections;
+using SiliconStudio.Paradox.Engine;
+using SiliconStudio.Paradox.Engine.Data;
 
 namespace SiliconStudio.Paradox.EntityModel.Data
 {
     // Entities as they are stored in the git shared scene file
     public partial class EntityData
     {
+        /// <summary>
+        /// The <see cref="EntityHierarchyData"/> that is containing this entity.
+        /// </summary>
         [DataMemberIgnore]
         public EntityHierarchyData Container;
 
+        /// <summary>
+        /// The identifier of this entity.
+        /// </summary>
         [DataMember(5)]
         public Guid Id;
 
+        /// <summary>
+        /// The name of this entity.
+        /// </summary>
+        /// <userdoc>
+        /// The name of this entity.
+        /// </userdoc>
         [DataMember(10)]
         public string Name;
 
+        /// <summary>
+        /// Gets the collection of <see cref="EntityComponentData"/> of this entity.
+        /// </summary>
+        /// <userdoc>
+        /// The collection of components of this entity.
+        /// </userdoc>
         [DataMember(20)]
         public TrackingDictionary<PropertyKey, EntityComponentData> Components { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityData"/> class.
+        /// </summary>
         public EntityData()
         {
             Id = Guid.NewGuid();
             Components = new TrackingDictionary<PropertyKey, EntityComponentData>();
-            Components.CollectionChanged += Components_CollectionChanged;
+            Components.CollectionChanged += ComponentsCollectionChanged;
         }
 
-        void Components_CollectionChanged(object sender, TrackingCollectionChangedEventArgs e)
+        /// <summary>
+        /// Gets the transformation component of the <see cref="EntityData"/>. If there is no transformation component in
+        /// the entity and <see cref="createIfMissing"/> is true, it will create a new <see cref="TransformationComponentData"/>
+        /// and add it to the entity before returning it.
+        /// </summary>
+        /// <param name="createIfMissing">If <c>true</c> and if the entity does not have a transformation component, this method will create and add a new one.</param>
+        /// <returns>The transformation component of this entity.</returns>
+        public TransformationComponentData GetTransformation(bool createIfMissing = true)
+        {
+            EntityComponentData component;
+            if (!Components.TryGetValue(TransformationComponent.Key, out component) && createIfMissing)
+            {
+                var result = new TransformationComponentData();
+                Components.Add(TransformationComponent.Key, result);
+                return result;
+            }
+            return (TransformationComponentData)component;
+        }
+
+        void ComponentsCollectionChanged(object sender, TrackingCollectionChangedEventArgs e)
         {
             // Remove entity owner from previous EntityComponentData.
             if (e.OldItem is EntityComponentData)
@@ -44,6 +86,7 @@ namespace SiliconStudio.Paradox.EntityModel.Data
             }
         }
 
+        /// <inheritdoc/>
         public override string ToString()
         {
             return string.Format("{0} {1} ({2})", Id, Name, string.Join(", ", Components.Values.Select(x => x.GetType().Name)));
