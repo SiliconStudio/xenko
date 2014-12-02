221357a168af7058396408cb6ab49755db43f107
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-12-02 17:58:22 +0900

    [Effects] Start to add ToneMap effect and operators (wip)

---
+++ sources/shaders/Images/ToneMap/ToneMap.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+namespace SiliconStudio.Paradox.Effects.Images.ToneMap
+{
+    public class ToneMap : ImageEffectBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToneMap"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public ToneMap(ImageEffectContext context) : base(context)
+        {
+        }
+
+
+
+
+
+
+    }
+}
