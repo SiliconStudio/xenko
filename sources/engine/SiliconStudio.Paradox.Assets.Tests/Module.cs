0b367dbf66fbe55830e93559206b1439cfb9f23e
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-12-09 09:50:53 +0900

    [Core] Move all folders init from Platform to PlatformFolders to avoid initializing folders when accessing Platform informations (like Platform.IsWindowsDesktop)

--- sources/engine/SiliconStudio.Paradox.Assets.Tests/Module.cs
+++ sources/engine/SiliconStudio.Paradox.Assets.Tests/Module.cs
@@ -19,8 +19,8 @@ namespace SiliconStudio.Paradox.Assets.Tests
         [ModuleInitializer]
         internal static void Initialize()
         {
-            if (!Platform.IsVirtualFileSystemInitialized)
-                Platform.ApplicationDataSubDirectory = typeof(Module).Assembly.GetName().Name;
+            if (!PlatformFolders.IsVirtualFileSystemInitialized)
+                PlatformFolders.ApplicationDataSubDirectory = typeof(Module).Assembly.GetName().Name;
         }
     }
 }
