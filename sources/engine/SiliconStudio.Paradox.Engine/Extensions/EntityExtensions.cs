e32e35115475a493ddb4a094ee44282ef156213c
Pierre Rahier <pierre@siliconstudio.co.jp>
2014-12-16 15:21:59 +0900

    [Preview] Calculate bounding sphere of the whole scene instead of the root entity when calculating thumbnail/preview camera parameters.

--- sources/engine/SiliconStudio.Paradox.Engine/Extensions/EntityExtensions.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Extensions/EntityExtensions.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
 using System.Collections.Generic;
 using System.Linq;
-using SiliconStudio.Paradox.Engine;
 using SiliconStudio.Paradox.EntityModel;
-using SiliconStudio.Paradox.Games;
 using SiliconStudio.Core;
 
 namespace SiliconStudio.Paradox.Extensions
