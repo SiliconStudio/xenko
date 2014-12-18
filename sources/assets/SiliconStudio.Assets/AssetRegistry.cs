dcc6b14396c699a2e5142d62e03e492cd7458158
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-12-18 10:21:43 +0900

    [Assets] Fix issue with AssetRegistry.GetDescribedTypes not returning the right list (PDX-1258)

--- sources/assets/SiliconStudio.Assets/AssetRegistry.cs
+++ sources/assets/SiliconStudio.Assets/AssetRegistry.cs
@@ -28,6 +28,7 @@ namespace SiliconStudio.Assets
         private static readonly SolutionPlatformCollection supportedPlatforms = new SolutionPlatformCollection();
         private static readonly Dictionary<Type, string> RegisteredDefaultAssetExtension = new Dictionary<Type, string>();
         private static readonly Dictionary<Type, bool> RegisteredDynamicThumbnails = new Dictionary<Type, bool>();
+        private static readonly HashSet<Type> AssetTypes = new HashSet<Type>();
 
         private static readonly Dictionary<Guid, IAssetImporter> RegisteredImportersInternal = new Dictionary<Guid, IAssetImporter>();
         private static readonly Dictionary<Type, int> RegisteredFormatVersions = new Dictionary<Type, int>();
@@ -178,7 +179,7 @@ namespace SiliconStudio.Assets
         /// <returns>An array of <see cref="Type"/> elements.</returns>
         public static Type[] GetInstantiableTypes()
         {
-            return ObjectFactory.FindRegisteredFactories().Where(type => typeof(Asset).IsAssignableFrom(type)).ToArray();
+            return ObjectFactory.FindRegisteredFactories().Where(type => typeof(Asset).IsAssignableFrom(type) && type.IsPublic).ToArray();
         }
 
         /// <summary>
@@ -200,7 +201,7 @@ namespace SiliconStudio.Assets
         /// <returns>An array of <see cref="Type"/> elements.</returns>
         public static Type[] GetDescribedTypes()
         {
-            return RegisteredDynamicThumbnails.Keys.ToArray();
+            return AssetTypes.ToArray();
         }
 
         /// <summary>
@@ -383,6 +384,12 @@ namespace SiliconStudio.Assets
                 if (!typeof(Asset).IsAssignableFrom(assetType) || !assetType.IsClass)
                 {
                     continue;
+                }
+
+                // Store in a list all asset types loaded
+                if (assetType.IsPublic)
+                {
+                    AssetTypes.Add(assetType);
                 }
 
                 var isSourceCodeAsset = typeof(SourceCodeAsset).IsAssignableFrom(assetType);
