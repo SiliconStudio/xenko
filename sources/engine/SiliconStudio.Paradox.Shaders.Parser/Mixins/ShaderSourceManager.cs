275fda13d2e139117ba0d0133003bd8f4599861d
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2014-12-20 23:17:04 +0900

    [Shaders.Parser] Use the case-sensitive name of the original shader file name to match against a type (fix error when trying to resolve a variable in lower-case which happen to have the same name as a shader)

--- sources/engine/SiliconStudio.Paradox.Shaders.Parser/Mixins/ShaderSourceManager.cs
+++ sources/engine/SiliconStudio.Paradox.Shaders.Parser/Mixins/ShaderSourceManager.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 using SiliconStudio.Core;
@@ -215,13 +216,71 @@ namespace SiliconStudio.Paradox.Shaders.Parser.Mixins
 
         private bool FileExists(string path)
         {
-            return UseFileSystem ? File.Exists(path) : AssetManager.FileProvider.FileExists(path);
+            if (UseFileSystem)
+            {
+                var fileInfo = new FileInfo(path);
+                if (fileInfo.Exists)
+                {
+                    var shaderName = Path.GetFileNameWithoutExtension(path);
+                    var realPath = GetWindowsPhysicalPath(path);
+                    if (!string.IsNullOrWhiteSpace(realPath))
+                    {
+                        var shaderNameOnDisk = Path.GetFileNameWithoutExtension(realPath);
+                        return string.CompareOrdinal(shaderName, shaderNameOnDisk) == 0;
+                    }
+                }
+            }
+            else
+            {
+                return AssetManager.FileProvider.FileExists(path);
+            }
+            return false;
         }
 
         private Stream OpenStream(string path)
         {
             return UseFileSystem ? File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read) : AssetManager.FileProvider.OpenStream(path, VirtualFileMode.Open, VirtualFileAccess.Read, VirtualFileShare.Read);
         }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint GetLongPathName(string shortPath, StringBuilder sb, int buffer);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetShortPathName(string longpath, StringBuilder sb, int buffer);
+
+        private static string GetWindowsPhysicalPath(string path)
+        {
+            var builder = new StringBuilder(255);
+
+            // names with long extension can cause the short name to be actually larger than
+            // the long name.
+            GetShortPathName(path, builder, builder.Capacity);
+
+            path = builder.ToString();
+
+            uint result = GetLongPathName(path, builder, builder.Capacity);
+
+            if (result > 0 && result < builder.Capacity)
+            {
+                //Success retrieved long file name
+                builder[0] = char.ToLower(builder[0]);
+                return builder.ToString(0, (int)result);
+            }
+
+            if (result > 0)
+            {
+                //Need more capacity in the buffer
+                //specified in the result variable
+                builder = new StringBuilder((int)result);
+                result = GetLongPathName(path, builder, builder.Capacity);
+                builder[0] = char.ToLower(builder[0]);
+                return builder.ToString(0, (int)result);
+            }
+
+            return null;
+        }
+
+
 
         public struct ShaderSourceWithHash
         {
