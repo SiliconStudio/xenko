a37257ab083a68d353d78ae47060e444896db145
Adrian Courreges <adrian@siliconstudio.co.jp>
2015-01-15 18:24:53 +0900

    [Effect] Added the "BokehTechnique" enum to list the different techniques available.

---
+++ sources/shaders/Images/DepthOfField/BokehTechnique/BokehTechnique.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
+
+using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Paradox.Graphics;
+using System.Collections.Generic;
+
+namespace SiliconStudio.Paradox.Effects.Images
+{
+
+    /// <summary>
+    /// Techniques available to perform a DoF effect on a level.
+    /// The technique directly affects the visual result (bokeh shape) as well as the performance. 
+    /// </summary>
+    public enum BokehTechnique
+    {
+        /// <summary>
+        /// Circular blur using a gaussian. 
+        /// </summary>
+        /// <remarks>
+        /// Fast and cheap technique but the final bokeh shapes are not very realistic.
+        /// </remarks>
+        CircularGaussian,
+
+        /// <summary>
+        /// Hexagonal blur using the McIntosh technique.
+        /// </summary>
+        HexagonalMcIntosh,
+
+        /// <summary>
+        /// Hexagonal blur using a combination of 3 rhombi blurs. 
+        /// </summary>
+        HexagonalTripleRhombi
+    }
+
+
+    // Extension methods to directly instantiate a blur image effect from a bokeh technique name.
+    public static class BokehTechniqueExtensions
+    {
+        /// <summary>
+        /// Instantiates a new <see cref="BokehBlur"/> from a technique name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="context"></param>
+        /// <returns>A Bokeh blur corresponding to the tehcnique specified.</returns>
+        public static BokehBlur ToBlurInstance(this BokehTechnique name, ImageEffectContext context)
+        {
+            switch (name)
+            {
+                case BokehTechnique.CircularGaussian:
+                    return new GaussianBokeh(context);
+
+                case BokehTechnique.HexagonalMcIntosh:
+                    return new McIntoshBokeh(context);
+
+                case BokehTechnique.HexagonalTripleRhombi:
+                    return new TripleRhombiBokeh(context);
+
+                default:
+                    throw new ArgumentOutOfRangeException("Unknown bokeh technique: " + name);
+            }
+        }
+    }
+       
+}
