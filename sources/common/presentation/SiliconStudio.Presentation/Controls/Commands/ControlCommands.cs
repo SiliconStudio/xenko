9d46f83d2c754bd8562dd2d6263b9d82506a6c51
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-01-21 21:04:24 +0900

    [Presentation] Reworked Vector3Editor template

--- sources/common/presentation/SiliconStudio.Presentation/Controls/Commands/ControlCommands.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Controls/Commands/ControlCommands.cs
@@ -1,8 +1,12 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Markup;
+
+using SiliconStudio.Presentation.Extensions;
 
 namespace SiliconStudio.Presentation.Controls.Commands
 {
@@ -19,6 +23,8 @@ namespace SiliconStudio.Presentation.Controls.Commands
         {
             ClearSelectionCommand = new RoutedCommand("ClearSelectionCommand", typeof(Selector));
             CommandManager.RegisterClassCommandBinding(typeof(Selector), new CommandBinding(ClearSelectionCommand, OnClearSelectionCommand));
+            SetAllVector3Components = new RoutedCommand("SetAllVector3Components", typeof(Vector3Editor));
+            CommandManager.RegisterClassCommandBinding(typeof(Vector3Editor), new CommandBinding(SetAllVector3Components, OnSetAllVector3Components));
         }
 
         /// <summary>
@@ -26,12 +32,36 @@ namespace SiliconStudio.Presentation.Controls.Commands
         /// </summary>
         public static RoutedCommand ClearSelectionCommand { get; private set; }
 
+        /// <summary>
+        /// Sets all the component of a <see cref="Vector3Editor"/> to the value given as parameter.
+        /// </summary>
+        public static RoutedCommand SetAllVector3Components { get; private set; }
+
         private static void OnClearSelectionCommand(object sender, ExecutedRoutedEventArgs e)
         {
             var selector = sender as Selector;
             if (selector != null)
             {
                 selector.SelectedItem = null;
+            }
+        }
+        
+        private static void OnSetAllVector3Components(object sender, ExecutedRoutedEventArgs e)
+        {
+            var vectorEditor = sender as Vector3Editor;
+            if (vectorEditor != null)
+            {
+                try
+                {
+                    var value = Convert.ToSingle(e.Parameter);
+                    vectorEditor.SetCurrentValue(Vector3Editor.XProperty, value);
+                    vectorEditor.SetCurrentValue(Vector3Editor.YProperty, value);
+                    vectorEditor.SetCurrentValue(Vector3Editor.ZProperty, value);
+                }
+                catch (Exception ex)
+                {
+                    ex.Ignore();
+                }
             }
         }
     }
