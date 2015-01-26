c477fc7fcf428152d9b7a5609681b346723402c4
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-01-26 14:32:22 +0900

    [Presentation] The color picker is now capable of hiding the Alpha component for Color3

--- sources/common/presentation/SiliconStudio.Presentation/Controls/ColorPicker.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Controls/ColorPicker.cs
@@ -84,45 +84,55 @@ namespace SiliconStudio.Presentation.Controls
         public static readonly DependencyProperty AlphaProperty = DependencyProperty.Register("Alpha", typeof(byte), typeof(ColorPicker), new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnRGBAPropertyChanged));
 
         /// <summary>
-        /// The color associated to this color picker.
+        /// Identifies the <see cref="ShowAlpha"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShowAlphaProperty = DependencyProperty.Register("ShowAlpha", typeof(bool), typeof(ColorPicker), new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// Gets or sets the color associated to this color picker.
         /// </summary>
         /// <remarks>The float values of each component of the color are always equals to the float conversion of a <see cref="byte"/> value divided by <b>255</b>.</remarks>
         public Color4 Color { get { return (Color4)GetValue(ColorProperty); } set { SetValue(ColorProperty, value); } }
 
         /// <summary>
-        /// The hue of the color associated to this color picker.
+        /// Gets or sets the hue of the color associated to this color picker.
         /// </summary>
         public float Hue { get { return (float)GetValue(HueProperty); } set { SetValue(HueProperty, value); } }
 
         /// <summary>
-        /// The saturation of the color associated to this color picker.
+        /// Gets or sets the saturation of the color associated to this color picker.
         /// </summary>
         public float Saturation { get { return (float)GetValue(SaturationProperty); } set { SetValue(SaturationProperty, value); } }
 
         /// <summary>
-        /// The brightness of the color associated to this color picker.
+        /// Gets or sets the brightness of the color associated to this color picker.
         /// </summary>
         public float Brightness { get { return (float)GetValue(BrightnessProperty); } set { SetValue(BrightnessProperty, value); } }
 
         /// <summary>
-        /// The red component of the color associated to this color picker.
+        /// Gets or sets the red component of the color associated to this color picker.
         /// </summary>
         public byte Red { get { return (byte)GetValue(RedProperty); } set { SetValue(RedProperty, value); } }
 
         /// <summary>
-        /// The green component of the color associated to this color picker.
+        /// Gets or sets the green component of the color associated to this color picker.
         /// </summary>
         public byte Green { get { return (byte)GetValue(GreenProperty); } set { SetValue(GreenProperty, value); } }
 
         /// <summary>
-        /// The blue component of the color associated to this color picker.
+        /// Gets or sets the blue component of the color associated to this color picker.
         /// </summary>
         public byte Blue { get { return (byte)GetValue(BlueProperty); } set { SetValue(BlueProperty, value); } }
 
         /// <summary>
-        /// The alpha component of the color associated to this color picker.
+        /// Gets or sets the alpha component of the color associated to this color picker.
         /// </summary>
         public byte Alpha { get { return (byte)GetValue(AlphaProperty); } set { SetValue(AlphaProperty, value); } }
+
+        /// <summary>
+        /// Gets or sets whether the alpha component of the color should be displayed in the color picker
+        /// </summary>
+        public bool ShowAlpha { get { return (bool)GetValue(ShowAlphaProperty); } set { SetValue(ShowAlphaProperty, value); } }
 
         /// <summary>
         /// An internal representation of the color associated to this color picker. Its value never rounded to match a byte division by 255.
