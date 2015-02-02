8e4498fbddb93df50077d7e9fb3033c439b33a93
Adrian Courreges <adrian@siliconstudio.co.jp>
2015-02-02 17:01:20 +0900

    [Effects] Integrated oF into ImageEffectBundle. TODO provide automatically the camera information (and the depth?).

--- sources/shaders/Images/ImageEffectBundle.cs
+++ sources/shaders/Images/ImageEffectBundle.cs
@@ -2,6 +2,8 @@
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
 using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Paradox.Engine;
 using SiliconStudio.Paradox.Graphics;
 
 namespace SiliconStudio.Paradox.Effects.Images
@@ -17,6 +19,7 @@ namespace SiliconStudio.Paradox.Effects.Images
         private readonly ColorTransformGroup colorTransformGroup;
         private readonly ToneMap toneMap;
         private readonly FXAAEffect fxaa;
+        private readonly DepthOfField depthOfField;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ImageEffectBundle"/> class.
@@ -41,7 +44,15 @@ namespace SiliconStudio.Paradox.Effects.Images
             fxaa = new FXAAEffect(Context);
             toneMap = new ToneMap();
             colorTransformGroup.Transforms.Add(toneMap);
+            depthOfField = new DepthOfField(context);
+            // Example of DoF configuration
+            depthOfField.Technique = BokehTechnique.HexagonalTripleRhombi;
+            depthOfField.DOFAreas = new Vector4(17f, 34f, 50f, 100f);
+            depthOfField.LevelCoCValues = new float[] { 0.25f, 0.5f, 1.0f };
+            depthOfField.LevelDownscaleFactors = new int[] { 1, 1, 1 };
+            depthOfField.MaxBokehSize = 10f / 1280f;
         }
+        public CameraComponent cameraComponentTmp; // TODO automatically pass the camera
 
         public BrightFilter BrightFilter
         {
@@ -83,6 +94,14 @@ namespace SiliconStudio.Paradox.Effects.Images
             }
         }
 
+        public DepthOfField DepthOfField
+        {
+            get
+            {
+                return depthOfField;
+            }
+        }
+
         protected override void DrawCore()
         {
             var input = GetInput(0);
@@ -92,15 +111,32 @@ namespace SiliconStudio.Paradox.Effects.Images
                 return;
             }
 
-            // TODO: Add DOF/MotionBlur pass
+            
+            var currentInput = input;
+
+            if (depthOfField.Enabled 
+                && InputCount > 1 // needs the depth
+                && cameraComponentTmp != null // needs camera configuration
+                )
+            {
+                // DoF
+                var dofOutput = NewScopedRenderTarget2D(input.Width, input.Height, input.Format);
+                var inputDepthTexture = GetInput(1); // Depth
+                depthOfField.SetColorDepthInput(input, inputDepthTexture);
+                depthOfField.SetOutput(dofOutput);
+                depthOfField.Camera = cameraComponentTmp;
+                depthOfField.Draw();
+                currentInput = dofOutput;
+            }
+
             // Luminance pass (only if tone mapping is enabled)
             if (toneMap.Enabled)
             {
                 const int LocalLuminanceDownScale = 3;
-                var lumSize = input.Size.Down2(LocalLuminanceDownScale);
+                var lumSize = currentInput.Size.Down2(LocalLuminanceDownScale);
                 var luminanceTexture = NewScopedRenderTarget2D(lumSize.Width, lumSize.Height, PixelFormat.R16_Float, 1);
 
-                luminanceEffect.SetInput(input);
+                luminanceEffect.SetInput(currentInput);
                 luminanceEffect.SetOutput(luminanceTexture);
                 luminanceEffect.Draw();
 
@@ -112,14 +148,14 @@ namespace SiliconStudio.Paradox.Effects.Images
             // TODO: Add Glare pass
             if (bloom.Enabled)
             {
-                var brightTexture = NewScopedRenderTarget2D(input.Width, input.Height, input.Format, 1);
+                var brightTexture = NewScopedRenderTarget2D(currentInput.Width, currentInput.Height, currentInput.Format, 1);
 
-                brightFilter.SetInput(input);
+                brightFilter.SetInput(currentInput);
                 brightFilter.SetOutput(brightTexture);
                 brightFilter.Draw();
 
                 bloom.SetInput(brightTexture);
-                bloom.SetOutput(input);
+                bloom.SetOutput(currentInput);
                 bloom.Draw();
             }
 
@@ -132,7 +168,7 @@ namespace SiliconStudio.Paradox.Effects.Images
 
             // Color transform group pass (tonemap, color grading, gamma correction)
             var lastEffect = colorTransformGroup.Enabled ? (ImageEffect)colorTransformGroup: Scaler;
-            lastEffect.SetInput(input);
+            lastEffect.SetInput(currentInput);
             lastEffect.SetOutput(outputForLastEffectBeforeAntiAliasing);
             lastEffect.Draw();
 
