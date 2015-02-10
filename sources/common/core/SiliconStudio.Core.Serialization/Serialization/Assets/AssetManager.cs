ea3e3aba6f46ce210886f17aa1403a7a50097807
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-02-10 19:23:03 +0900

    [Core] Added non-generic Load and LoadAsync methods to the AssetManager

--- sources/common/core/SiliconStudio.Core.Serialization/Serialization/Assets/AssetManager.cs
+++ sources/common/core/SiliconStudio.Core.Serialization/Serialization/Assets/AssetManager.cs
@@ -75,6 +75,11 @@ namespace SiliconStudio.Core.Serialization.Assets
 
         public T Load<T>(string url, AssetManagerLoaderSettings settings = null) where T : class
         {
+            return (T)Load(typeof(T), url, settings);
+        }
+
+        public object Load(Type type, string url, AssetManagerLoaderSettings settings = null)
+        {
             if (settings == null)
                 settings = AssetManagerLoaderSettings.Default;
 
@@ -84,7 +89,7 @@ namespace SiliconStudio.Core.Serialization.Assets
             {
                 using (var profile = Profiler.Begin(AssetProfilingKeys.AssetLoad, url))
                 {
-                    return (T)DeserializeObject(url, typeof(T), settings);
+                    return DeserializeObject(url, type, settings);
                 }
             }
         }
@@ -92,6 +97,11 @@ namespace SiliconStudio.Core.Serialization.Assets
         public Task<T> LoadAsync<T>(string url, AssetManagerLoaderSettings settings = null) where T : class
         {
             return Task.Factory.StartNew(() => Load<T>(url, settings));
+        }
+
+        public Task<object> LoadAsync(Type type, string url, AssetManagerLoaderSettings settings = null)
+        {
+            return Task.Factory.StartNew(() => Load(type, url, settings));
         }
 
         public bool TryGetAssetUrl(object obj, out string url)
