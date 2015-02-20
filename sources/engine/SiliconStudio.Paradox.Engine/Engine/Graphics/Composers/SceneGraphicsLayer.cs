2c44cc562fceac0129ebf75dd4d747275c05c9ea
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-02-20 18:27:32 +0900

    Fixed compilation

--- sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/Composers/SceneGraphicsLayer.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/Composers/SceneGraphicsLayer.cs
@@ -2,6 +2,7 @@
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
 using System.Collections;
+using System.ComponentModel;
 
 using SiliconStudio.Core;
 using SiliconStudio.Core.Annotations;
