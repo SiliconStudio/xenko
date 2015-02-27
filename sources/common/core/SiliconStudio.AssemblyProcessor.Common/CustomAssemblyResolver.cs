93e213851fe185a4d8a99dcf9a1ef4b31e48b7e2
Virgile Bello <virgile@siliconstudio.co.jp>
2015-02-27 22:21:08 +0900

    [AssemblyProcessor] Use Roslyn to generate serialization assembly.

--- sources/common/core/SiliconStudio.AssemblyProcessor.Common/CustomAssemblyResolver.cs
+++ sources/common/core/SiliconStudio.AssemblyProcessor.Common/CustomAssemblyResolver.cs
@@ -1,17 +1,50 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System.Collections.Generic;
+using System.IO;
 using Mono.Cecil;
 
 namespace SiliconStudio.AssemblyProcessor
 {
-    class CustomAssemblyResolver : DefaultAssemblyResolver
+    /// <summary>
+    /// Allow to register assemblies manually, with their in-memory representation if necessary.
+    /// </summary>
+    public class CustomAssemblyResolver : DefaultAssemblyResolver
     {
+        /// <summary>
+        /// Assemblies stored as byte arrays.
+        /// </summary>
+        private readonly Dictionary<AssemblyDefinition, byte[]> assemblyData = new Dictionary<AssemblyDefinition, byte[]>();
+
         /// <summary>
         /// Registers the specified assembly.
         /// </summary>
         /// <param name="assembly">The assembly to register.</param>
         public void Register(AssemblyDefinition assembly)
         {
+            this.RegisterAssembly(assembly);
+        }
+
+        /// <summary>
+        /// Gets the assembly data (if it exists).
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns></returns>
+        public byte[] GetAssemblyData(AssemblyDefinition assembly)
+        {
+            byte[] data;
+            assemblyData.TryGetValue(assembly, out data);
+            return data;
+        }
+
+        /// <summary>
+        /// Registers the specified assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to register.</param>
+        public void Register(AssemblyDefinition assembly, byte[] peData)
+        {
+            assemblyData[assembly] = peData;
             this.RegisterAssembly(assembly);
         }
     }
