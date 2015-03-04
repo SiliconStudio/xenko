b464eb61cc224c233f91efd89e39b04331728b3e
Pierre Rahier <pierre@siliconstudio.co.jp>
2015-03-04 18:03:50 +0900

    [Engine] Change default LightSpot values to have a nice gizmo spot cone after creation of new spotlight in the scene.

--- sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/Lights/LightSpot.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/Lights/LightSpot.cs
@@ -16,26 +16,26 @@ namespace SiliconStudio.Paradox.Effects.Lights
     {
         public LightSpot()
         {
-            BeamAngle = 0;
-            FieldAngle = 0;
-            DecayStart = 100.0f;
+            BeamAngle = 30;
+            FieldAngle = 60;
+            DecayStart = 20;
         }
 
         /// <summary>
         /// Gets or sets the decay start.
         /// </summary>
         /// <value>The decay start.</value>
-        [DefaultValue(100.0f)]
+        [DefaultValue(20.0f)]
         public float DecayStart { get; set; }
 
         /// <summary>Gets or sets the beam angle of the spot light.</summary>
         /// <value>The beam angle of the spot (in degrees between 0 and 90).</value>
-        [DefaultValue(0)]
+        [DefaultValue(30.0f)]
         public float BeamAngle { get; set; }
 
         /// <summary>Gets or sets the spot field angle of the spot light.</summary>
         /// <value>The spot field angle of the spot (in degrees between 0 and 90).</value>
-        [DefaultValue(0)]
+        [DefaultValue(60.0f)]
         public float FieldAngle { get; set; }
     }
 }
