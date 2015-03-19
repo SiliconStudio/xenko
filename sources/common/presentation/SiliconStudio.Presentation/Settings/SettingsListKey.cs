0128795f6d5a045804b2f7d4443e0490c9d75537
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-03-19 17:26:31 +0900

    [Scene] Reworked SceneInternalSettings saving/loading

--- sources/common/presentation/SiliconStudio.Presentation/Settings/SettingsListKey.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Settings/SettingsListKey.cs
@@ -6,14 +6,55 @@ using System.Collections.Generic;
 using System.Linq;
 
 using SiliconStudio.Core.IO;
+using SiliconStudio.Presentation.Extensions;
 
 namespace SiliconStudio.Presentation.Settings
 {
     /// <summary>
+    /// This is the base class to represent a <see cref="SettingsKey"/> containing a list of values.
+    /// </summary>
+    public abstract class SettingsListKey : SettingsKey
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsListKey"/> class.
+        /// </summary>
+        /// <param name="name">The name of the settings key. Must be unique amongst an application.</param>
+        /// <param name="defaultValue">The default values for this settings key.</param>
+        protected SettingsListKey(UFile name, object defaultValue)
+            : base(name, defaultValue)
+        {
+        }
+
+        /// <summary>
+        /// Gets the non-generic list of values of this settings key in the current profile.
+        /// </summary>
+        /// <param name="searchInParentProfile">If true, the settings service will look in the parent profile of the given profile if the settings key is not defined into it.</param>
+        /// <param name="profile">The profile in which to look for the value. If <c>null</c>, it will look in the <see cref="SettingsService.CurrentProfile"/>.</param>
+        /// <param name="createInCurrentProfile">If true, the list will be created in the current profile, from the value of its parent profile.</param>
+        /// <returns>The non-generic list of values of this settings key.</returns>
+        /// <exception cref="KeyNotFoundException">No value can be found in the given profile matching this settings key.</exception>
+        public IList GetNonGenericList(bool searchInParentProfile = true, SettingsProfile profile = null, bool createInCurrentProfile = false)
+        {
+            profile = profile ?? SettingsService.CurrentProfile;
+            object value;
+            if (profile.GetValue(Name, out value, searchInParentProfile, createInCurrentProfile))
+            {
+                var list = value as IList;
+                if (list != null)
+                {
+                    return list;
+                }
+            }
+
+            throw new KeyNotFoundException("Settings key not found");
+        }
+    }
+
+    /// <summary>
     /// This class represents a <see cref="SettingsKey"/> containing a list of values of the given type <see cref="T"/>.
     /// </summary>
     /// <typeparam name="T">The type of items in the list contained in this key.</typeparam>
-    public class SettingsListKey<T> : SettingsKey
+    public class SettingsListKey<T> : SettingsListKey
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SettingsListKey{T}"/> class.
@@ -21,7 +62,7 @@ namespace SiliconStudio.Presentation.Settings
         /// <param name="name">The name of the settings key. Must be unique amongst an application.</param>
         /// <param name="defaultValue">The default values for this settings key.</param>
         public SettingsListKey(UFile name, IEnumerable<T> defaultValue)
-            : base(name, defaultValue.ToList())
+            : base(name, defaultValue.SafeArgument("defaultValue").ToList())
         {
             if (defaultValue == null) throw new ArgumentNullException("defaultValue");
         }
@@ -40,21 +81,15 @@ namespace SiliconStudio.Presentation.Settings
         /// <param name="searchInParentProfile">If true, the settings service will look in the parent profile of the given profile if the settings key is not defined into it.</param>
         /// <param name="profile">The profile in which to look for the value. If <c>null</c>, it will look in the <see cref="SettingsService.CurrentProfile"/>.</param>
         /// <param name="createInCurrentProfile">If true, the list will be created in the current profile, from the value of its parent profile.</param>
-        /// <returns>The value of this settings key.</returns>
+        /// <returns>The list of values of this settings key.</returns>
         /// <exception cref="KeyNotFoundException">No value can be found in the given profile matching this settings key.</exception>
         public IList<T> GetList(bool searchInParentProfile = true, SettingsProfile profile = null, bool createInCurrentProfile = false)
         {
-            profile = profile ?? SettingsService.CurrentProfile;
-            object value;
-            if (profile.GetValue(Name, out value, searchInParentProfile, createInCurrentProfile))
+            var list = GetNonGenericList(searchInParentProfile, profile, createInCurrentProfile) as IList<T>;
+            if (list != null)
             {
-                var list = value as IList<T>;
-                if (list != null)
-                {
-                    return list;
-                }
+                return list;
             }
-
             throw new KeyNotFoundException("Settings key not found");
         }
 
