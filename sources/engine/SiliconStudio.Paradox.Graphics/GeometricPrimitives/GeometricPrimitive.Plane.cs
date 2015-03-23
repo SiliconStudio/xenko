a9f392575962110fadeadded92ccbbf0a2d34ebb
Pierre Rahier <pierre@siliconstudio.co.jp>
2015-03-23 20:15:36 +0900

    [Graphics] Fix problem of normals on GeometricPrimitive.Plane.

--- sources/engine/SiliconStudio.Paradox.Graphics/GeometricPrimitives/GeometricPrimitive.Plane.cs
+++ sources/engine/SiliconStudio.Paradox.Graphics/GeometricPrimitives/GeometricPrimitive.Plane.cs
@@ -73,7 +73,7 @@ namespace SiliconStudio.Paradox.Graphics.GeometricPrimitives
 
                 var lineWidth = tessellationX + 1;
                 var lineHeight = tessellationY + 1;
-                var vertices = new VertexPositionNormalTexture[lineWidth * lineHeight];
+                var vertices = new VertexPositionNormalTexture[lineWidth * lineHeight * (generateBackFace? 2: 1)];
                 var indices = new int[tessellationX * tessellationY * 6 * (generateBackFace? 2: 1)];
 
                 var deltaX = sizeX / tessellationX;
@@ -117,13 +117,23 @@ namespace SiliconStudio.Paradox.Graphics.GeometricPrimitives
                 }
                 if(generateBackFace)
                 {
+                    normal = -Vector3.UnitZ;
+                    for (int y = 0; y < (tessellationY + 1); y++)
+                    {
+                        for (int x = 0; x < (tessellationX + 1); x++)
+                        {
+                            var position = new Vector3(-sizeX + deltaX * x, sizeY - deltaY * y, 0);
+                            var texCoord = new Vector2(uv.X * x / tessellationX, uv.Y * y / tessellationY);
+                            vertices[vertexCount++] = new VertexPositionNormalTexture(position, normal, texCoord);
+                        }
+                    }
                     // Create indices
                     for (int y = 0; y < tessellationY; y++)
                     {
                         for (int x = 0; x < tessellationX; x++)
                         {
                             // Six indices (two triangles) per face.
-                            int vbase = lineWidth * y + x;
+                            int vbase = lineWidth * (y + tessellationY + 1) + x;
                             indices[indexCount++] = (vbase + 1);
                             indices[indexCount++] = (vbase + lineWidth);
                             indices[indexCount++] = (vbase + 1 + lineWidth);
