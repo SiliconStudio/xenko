86bed0d3bf309859802a0461b0646f92779232f6
Virgile Bello <virgile@siliconstudio.co.jp>
2015-03-23 17:44:42 +0900

    [Engine] Remove unecessary namespaces.

--- sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/DynamicEffectCompiler.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/DynamicEffectCompiler.cs
@@ -1,14 +1,10 @@
 // Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Markup;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Collections;
 using SiliconStudio.Core.Extensions;
 using SiliconStudio.Paradox.Graphics;
-using SiliconStudio.Paradox.Shaders;
 using SiliconStudio.Paradox.Shaders.Compiler;
 
 namespace SiliconStudio.Paradox.Effects
