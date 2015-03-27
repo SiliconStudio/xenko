51f37cc62e65e6e448c8a12e0dfaf7d5c7d86a96
Virgile Bello <virgile@siliconstudio.co.jp>
2015-03-27 20:40:24 +0900

    [Scripts] Allow errors to happen when deserializing Yaml scripts. If assembly could not compile, Yaml stream will be kept as is when saved again. PDX-1523

---
+++ sources/engine/SiliconStudio.Paradox.Assets/Serializers/UnloadableScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using SharpYaml.Events;
+using SiliconStudio.Core;
+
+namespace SiliconStudio.Paradox.Assets.Serializers
+{
+    /// <summary>
+    /// Represents a Script that could not be loaded properly (usually due to missing/broken assemblies).
+    /// Yaml representation is kept as is, so that it can be properly saved again.
+    /// </summary>
+    class UnloadableScript : Script
+    {
+        [DataMemberIgnore]
+        public List<ParsingEvent> ParsingEvents { get; private set; }
+
+        public UnloadableScript(List<ParsingEvent> parsingEvents)
+        {
+            ParsingEvents = parsingEvents;
+        }
+    }
+}
