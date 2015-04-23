23b3df3ebca771657686be014d997e21e50b3dc3
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-04-23 18:28:13 +0900

    [Editor] Removed shaders from the list of available compute node types in materials

--- sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/Materials/ComputeColors/ComputeShaderClassColor.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Engine/Graphics/Materials/ComputeColors/ComputeShaderClassColor.cs
@@ -10,7 +10,8 @@ namespace SiliconStudio.Paradox.Assets.Materials.ComputeColors
     /// </summary>
     [DataContract("ComputeShaderClassColor")]
     [Display("Shader")]
-    public class ComputeShaderClassColor : ComputeShaderClassBase<IComputeColor>, IComputeColor 
+    // TODO: This class has been made abstract to be removed from the editor - unabstract it to re-enable it!
+    public abstract class ComputeShaderClassColor : ComputeShaderClassBase<IComputeColor>, IComputeColor 
     {
     }
 }
