0213de84c8e357f95044fda6a9214d592b49a746
Virgile Bello <virgile@siliconstudio.co.jp>
2015-05-13 19:28:50 +0900

    [Doc] Make index.aspx as default.

--- doc/paradox_doc_build.bat
+++ doc/paradox_doc_build.bat
@@ -3,3 +3,4 @@ setlocal
 set SHARPDOC=..\sources\common\deps\SharpDoc\Bin\SharpDoc.exe
 REM set SHARPDOC=..\externals\SharpDoc\Build\Tools\Bin\SharpDoc.exe
 %SHARPDOC% -c paradox_doc_config.xml -w=https://pj.siliconstudio.co.jp/confluence/display/PDXDOC/ -wL=%APPDATA%\ParadoxDoc\auth.conf
+copy Style\paradox\web.config paradox_doc_output
