1828167fc9e2b70a877585c29cae569ad6c1fe42
Benlitz <dev@benlitz.net>
2015-05-16 16:49:46 +0900

    [Presentation] Added AsyncCommand<T> class and updated usages

--- sources/common/presentation/SiliconStudio.Presentation/Commands/AsyncCommand.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Commands/AsyncCommand.cs
@@ -10,6 +10,7 @@ namespace SiliconStudio.Presentation.Commands
     public class AsyncCommand : CommandBase
     {
         private readonly Action<object> action;
+        private readonly Func<bool> canExecute;
 
         public AsyncCommand(IViewModelServiceProvider serviceProvider, Action<object> action)
             : base(serviceProvider)
@@ -23,14 +24,73 @@ namespace SiliconStudio.Presentation.Commands
             this.action = x => action();
         }
 
+        public AsyncCommand(IViewModelServiceProvider serviceProvider, Action<object> action, Func<bool> canExecute)
+            : base(serviceProvider)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public AsyncCommand(IViewModelServiceProvider serviceProvider, Action action, Func<bool> canExecute)
+            : base(serviceProvider)
+        {
+            this.action = x => action();
+            this.canExecute = canExecute;
+        }
+
         public override void Execute(object parameter)
         {
             Task.Run(() => action(parameter));
         }
 
-        public async Task ExecuteAwaitable(object parameter = null)
+        public Task ExecuteAwaitable(object parameter = null)
         {
-            await Task.Run(() => action(parameter));
+            return Task.Run(() => action(parameter));
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            var result = base.CanExecute(parameter);
+            return result && canExecute != null ? canExecute() : result;
+        }
+    }
+
+    public class AsyncCommand<T> : CommandBase
+    {
+        private readonly Action<T> action;
+        private readonly Func<bool> canExecute;
+
+        public AsyncCommand(IViewModelServiceProvider serviceProvider, Action<T> action)
+            : base(serviceProvider)
+        {
+            this.action = action;
+        }
+
+        public AsyncCommand(IViewModelServiceProvider serviceProvider, Action<T> action, Func<bool> canExecute)
+            : base(serviceProvider)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if ((typeof(T).IsValueType || parameter != null) && !(parameter is T))
+                throw new ArgumentException(@"Unexpected parameter type in the command.", "parameter");
+
+            action((T)parameter);
+        }
+
+
+        public Task ExecuteAwaitable(object parameter)
+        {
+            return Task.Run(() => Execute(parameter));
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            var result = base.CanExecute(parameter);
+            return result && canExecute != null ? canExecute() : result;
         }
     }
 }
