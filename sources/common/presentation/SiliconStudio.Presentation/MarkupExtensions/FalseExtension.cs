9ff1d783226bb40d62626a74668e46251a19b465
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-06-02 14:50:20 +0900

    [Presentation] Replaced the Bool markup extension by a True and a False extensions

---
+++ sources/common/presentation/SiliconStudio.Presentation/MarkupExtensions/FalseExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace SiliconStudio.Presentation.MarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(bool))]
+    public class FalseExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return false;
+        }
+    }
+}
