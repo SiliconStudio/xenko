2e17464bdd6e3e1717769014506cc81ceeabb5dd
Virgile Bello <virgile@siliconstudio.co.jp>
2015-06-05 18:54:24 +0900

    [Assets] Missing file.

---
+++ sources/engine/SiliconStudio.Paradox.Assets/GameUserSettings.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core.Settings;
+using SiliconStudio.Paradox.Engine.Design;
+
+namespace SiliconStudio.Paradox.Assets
+{
+    public static class GameUserSettings
+    {
+        public static class Effect
+        {
+            public static SettingsValueKey<EffectCompilationMode> EffectCompilationMode = new SettingsValueKey<EffectCompilationMode>("Package/Game/Effect/EffectCompilationMode", Engine.Design.EffectCompilationMode.Local);
+            public static SettingsValueKey<bool> RecordEffectRequested = new SettingsValueKey<bool>("Package/Game/Effect/RecordEffectRequested", false);
+        }
+    }
+}
