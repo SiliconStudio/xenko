5d1f52414e5418e76b51148b7ba6324d148629d8
Virgile Bello <virgile@siliconstudio.co.jp>
2015-06-08 14:12:39 +0900

    [Engine] Added comments on EffectCompilationMode.

--- sources/engine/SiliconStudio.Paradox.Engine/Engine/Design/EffectCompilationMode.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Engine/Design/EffectCompilationMode.cs
@@ -9,9 +9,24 @@ namespace SiliconStudio.Paradox.Engine.Design
     [Flags]
     public enum EffectCompilationMode
     {
+        /// <summary>
+        /// Effects can't be compiled. <see cref="Shaders.Compiler.NullEffectCompiler"/> will be used.
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Effects can only be compiled in process (if possible). <see cref="Shaders.Compiler.EffectCompiler"/> will be used.
+        /// </summary>
         Local = 1,
+
+        /// <summary>
+        /// Effects can only be compiled over network. <see cref="Shaders.Compiler.RemoteEffectCompiler"/> will be used.
+        /// </summary>
         Remote = 2,
+
+        /// <summary>
+        /// Effects can be compiled either in process (if possible) or over network otherwise.
+        /// </summary>
         LocalOrRemote = Local | Remote,
     }
 }
