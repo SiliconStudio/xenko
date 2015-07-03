290a4a06cc1957e66630a20c9f2faf7c9ed373e3
Virgile Bello <virgile@siliconstudio.co.jp>
2015-07-03 11:03:18 +0900

    [Assets] Fix paradox package upgrader start version.

--- sources/engine/SiliconStudio.Paradox.Assets/ParadoxPackageUpgrader.cs
+++ sources/engine/SiliconStudio.Paradox.Assets/ParadoxPackageUpgrader.cs
@@ -9,7 +9,7 @@ using SiliconStudio.Core.IO;
 
 namespace SiliconStudio.Paradox.Assets
 {
-    [PackageUpgrader("Paradox", "1.0.0-beta1", "1.1.4-beta")]
+    [PackageUpgrader("Paradox", "1.0.0-beta01", "1.1.4-beta")]
     public class ParadoxPackageUpgrader : PackageUpgrader
     {
         public override bool Upgrade(PackageSession session, ILogger log, Package dependentPackage, PackageDependency dependency, Package dependencyPackage, IList<PackageLoadingAssetFile> assetFiles)
