7065476ded8e138590d7097dbe5cf1a93c11733c
Pierre Rahier <pierre@siliconstudio.co.jp>
2015-07-14 18:31:57 +0900

    [Paradox.Assets] Hides the MaterialAsset ParameterCollection from the editor.

--- sources/engine/SiliconStudio.Paradox.Assets/Materials/MaterialAsset.cs
+++ sources/engine/SiliconStudio.Paradox.Assets/Materials/MaterialAsset.cs
@@ -8,9 +8,11 @@ using SiliconStudio.Assets;
 using SiliconStudio.Assets.Compiler;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Annotations;
+using SiliconStudio.Core.Diagnostics;
 using SiliconStudio.Core.Mathematics;
 using SiliconStudio.Core.Reflection;
 using SiliconStudio.Core.Serialization;
+using SiliconStudio.Core.Yaml;
 using SiliconStudio.Paradox.Rendering;
 using SiliconStudio.Paradox.Rendering.Materials;
 using SiliconStudio.Paradox.Rendering.Materials.ComputeColors;
@@ -25,6 +27,8 @@ namespace SiliconStudio.Paradox.Assets.Materials
     [ThumbnailCompiler(PreviewerCompilerNames.MaterialThumbnailCompilerQualifiedName, true, Priority = -5000)]
     [AssetCompiler(typeof(MaterialAssetCompiler))]
     [ObjectFactory(typeof(MaterialFactory))]
+    [AssetFormatVersion(1)]
+    [AssetUpgrader(0, 1, typeof(RemoveParametersUpgrader))]
     [Display(115, "Material", "A material")]
     public sealed class MaterialAsset : Asset, IMaterialDescriptor, IAssetCompileTimeDependencies
     {
@@ -72,7 +76,7 @@ namespace SiliconStudio.Paradox.Assets.Materials
         /// Gets or sets the parameters.
         /// </summary>
         /// <value>The parameters.</value>
-        [DataMember(30)]
+        [DataMemberIgnore]
         public ParameterCollection Parameters { get; set; }
 
         public IEnumerable<AssetReference<MaterialAsset>> FindMaterialReferences()
@@ -127,6 +131,15 @@ namespace SiliconStudio.Paradox.Assets.Materials
             foreach (var materialReference in FindMaterialReferences())
             {
                 yield return materialReference;
+            }
+        }
+
+
+        public class RemoveParametersUpgrader : AssetUpgraderBase
+        {
+            protected override void UpgradeAsset(int currentVersion, int targetVersion, ILogger log, dynamic asset)
+            {
+                asset.Parameters = DynamicYamlEmpty.Default;
             }
         }
     }
