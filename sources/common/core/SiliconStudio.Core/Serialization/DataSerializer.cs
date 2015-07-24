53ff3e99f834d6a786b6c9dc3f588409fe43fff2
Virgile Bello <virgile@siliconstudio.co.jp>
2015-07-24 11:46:03 +0900

    [Build] Bump Paradox version to 1.2.1

--- sources/common/core/SiliconStudio.Core/Serialization/DataSerializer.cs
+++ sources/common/core/SiliconStudio.Core/Serialization/DataSerializer.cs
@@ -13,7 +13,7 @@ namespace SiliconStudio.Core.Serialization
     public abstract class DataSerializer
     {
         // Binary format version, needs to be bumped in case of big changes in serialization formats (i.e. primitive types).
-        public const int BinaryFormatVersion = 9;
+        public const int BinaryFormatVersion = 10;
 
         public ObjectId SerializationTypeId;
 
