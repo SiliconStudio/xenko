71997c443e5b0a652664a2bc164cf2f6905a384f
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-08-06 09:20:22 +0900

    [Assets] Unload native library at the end of the compiler asset App

--- sources/assets/SiliconStudio.Assets.CompilerApp/Program.cs
+++ sources/assets/SiliconStudio.Assets.CompilerApp/Program.cs
@@ -1,13 +1,24 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core;
+
 namespace SiliconStudio.Assets.CompilerApp
 {
     class Program
     {
         private static int Main(string[] args)
         {
-            var packageBuilder = new PackageBuilderApp();
-            return packageBuilder.Run(args);
+            try
+            {
+                var packageBuilder = new PackageBuilderApp();
+                return packageBuilder.Run(args);
+            }
+            finally
+            {
+                // Free all native library loaded from the process
+                NativeLibrary.UnLoadAll();
+            }
         }
     }
 }
