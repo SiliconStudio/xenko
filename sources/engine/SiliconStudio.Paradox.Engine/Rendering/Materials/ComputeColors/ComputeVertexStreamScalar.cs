dd1903d98f3acb2034cb4dde0d6bf68bfe57bf8f
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-08-12 16:03:22 +0900

    [Engine] Add support for custom color vertex stream in materials

---
+++ sources/engine/SiliconStudio.Paradox.Engine/Rendering/Materials/ComputeColors/ComputeVertexStreamScalar.cs
@@ -0,0 +1,40 @@
+// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+using System.ComponentModel;
+
+using SiliconStudio.Core;
+using SiliconStudio.Paradox.Rendering.Materials.Processor.Visitors;
+
+namespace SiliconStudio.Paradox.Rendering.Materials.ComputeColors
+{
+    /// <summary>
+    /// A compute scalar producing a scalar from a stream.
+    /// </summary>
+    [DataContract("ComputeVertexStreamScalar")]
+    [Display("Vertex Stream")]
+    public class ComputeVertexStreamScalar : ComputeVertexStreamBase, IComputeScalar
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComputeVertexStreamScalar"/> class.
+        /// </summary>
+        public ComputeVertexStreamScalar()
+        {
+            Stream = new ColorVertexStreamDefinition();
+            Channel = ColorChannel.R;
+        }
+
+        /// <summary>
+        /// Gets or sets the channel.
+        /// </summary>
+        /// <value>The channel.</value>
+        /// <userdoc>Selects the RGBA channel to sample from the texture.</userdoc>
+        [DataMember(20)]
+        [DefaultValue(ColorChannel.R)]
+        public ColorChannel Channel { get; set; }
+
+        protected override string GetColorChannelAsString()
+        {
+            return MaterialUtility.GetAsShaderString(Channel);
+        }
+    }
+}
---
+++ sources/engine/SiliconStudio.Paradox.Engine/Rendering/Materials/ComputeColors/IVertexStreamDefinition.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+namespace SiliconStudio.Paradox.Rendering.Materials.ComputeColors
+{
+    /// <summary>
+    /// Definition of a stream. e.g COLOR0, COLOR1...etc.
+    /// </summary>
+    public interface IVertexStreamDefinition
+    {
+        /// <summary>
+        /// Gets the name of the semantic.
+        /// </summary>
+        /// <returns>A string with the name of the stream semantic.</returns>
+        string GetSemanticName();
+    }
+}
