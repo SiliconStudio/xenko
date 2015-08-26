9a456916e5329ae983ecd81afa717cd87d652e6f
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-08-26 14:17:11 +0900

    [Graphics]  Add ColorSpace enum

---
+++ sources/engine/SiliconStudio.Paradox/Graphics/ColorSpace.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core;
+
+namespace SiliconStudio.Paradox.Graphics
+{
+    /// <summary>
+    /// The colorspace used for textures, materials, lighting...
+    /// </summary>
+    [DataContract("ColorSpace")]
+    public enum ColorSpace
+    {
+        /// <summary>
+        /// Use a linear colorspace.
+        /// </summary>
+        Linear,
+
+        /// <summary>
+        /// Use a gamma colorspace.
+        /// </summary>
+        Gamma,
+    }
+}
