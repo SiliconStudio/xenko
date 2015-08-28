2a58d19c9efbebce46d19aa065e5cf30c3a54de9
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-08-28 17:54:31 +0900

    [GameStudio] Preview is now able to render in both LDR/HDR mode depending on the GameSettingsAsset.RenderingMode and the type of assets. Gamma and Linear is also simplified by creating either a Srgb or non-Srgb BackBuffer

---
+++ sources/engine/SiliconStudio.Paradox.Assets/ParadoxAssetDependenciesCompiler.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Assets;
+using SiliconStudio.Assets.Compiler;
+
+namespace SiliconStudio.Paradox.Assets
+{
+    public abstract class ParadoxAssetDependenciesCompiler : AssetDependenciesCompiler
+    {
+        protected void AddGameSettings(AssetItem assetItem, AssetItem originalItem)
+        {
+            // Copy game settings from the original package to the micro-package of the asset being compiled.
+            if (originalItem.Location != GameSettingsAsset.GameSettingsLocation)
+            {
+                var currentPackage = originalItem.Package.Session.CurrentPackage;
+                if (currentPackage != null)
+                {
+                    var gameSettingsAssetItem = currentPackage.Assets.Find(GameSettingsAsset.GameSettingsLocation);
+                    if (gameSettingsAssetItem != null)
+                    {
+                        var gameSettingsAssetCopy = (GameSettingsAsset)AssetCloner.Clone((GameSettingsAsset)gameSettingsAssetItem.Asset);
+                        var newGameSettingsAssetItem = new AssetItem(GameSettingsAsset.GameSettingsLocation, gameSettingsAssetCopy)
+                        {
+                            SourceFolder = gameSettingsAssetItem.FullPath.GetParent()
+                        };
+                        assetItem.Package.Assets.Add(newGameSettingsAssetItem);
+                    }
+                }
+            }            
+        }
+    }
+}
