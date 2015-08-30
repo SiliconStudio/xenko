85c17f82420414d7bb59f82c5e2bd55670b58572
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-08-31 00:23:03 +0900

    [Solution] More fixes related to VS2015 only systems

--- sources/tools/SiliconStudio.TextureConverter.Wrappers/build-deps.bat
+++ sources/tools/SiliconStudio.TextureConverter.Wrappers/build-deps.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
 msbuild SiliconStudio.TextureConverter.Wrappers.sln /Property:Configuration=Debug;Platform="Mixed Platforms"
 msbuild SiliconStudio.TextureConverter.Wrappers.sln /Property:Configuration=Release;Platform="Mixed Platforms"
