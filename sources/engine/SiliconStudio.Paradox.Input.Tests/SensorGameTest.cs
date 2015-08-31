5144e5b43ef4694b8104133757bd3d7ff4200293
Pierre Rahier <pierre@siliconstudio.co.jp>
2015-08-31 15:09:24 +0900

    [Input.Test] Tests for sensors (WIP)

--- sources/engine/SiliconStudio.Paradox.Input.Tests/SensorGameTest.cs
+++ sources/engine/SiliconStudio.Paradox.Input.Tests/SensorGameTest.cs
@@ -1,13 +1,20 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
+using System;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Paradox.Engine;
 using SiliconStudio.Paradox.Games;
 using SiliconStudio.Paradox.Graphics;
 using SiliconStudio.Paradox.Graphics.Regression;
+using SiliconStudio.Paradox.Rendering;
+using SiliconStudio.Paradox.Rendering.Sprites;
+using SiliconStudio.Paradox.UI;
+using SiliconStudio.Paradox.UI.Controls;
+using SiliconStudio.Paradox.UI.Panels;
 
 namespace SiliconStudio.Paradox.Input.Tests
 {
@@ -26,12 +33,44 @@ namespace SiliconStudio.Paradox.Input.Tests
         private Vector3 currentGravity;
         private Vector3 currentYawPitchRoww;
 
+        private enum DebugScenes
+        {
+            Orientation,
+            UserAccel,
+            Gravity,
+            RawAccel,
+            Gyroscope,
+            Compass
+        }
+        private DebugScenes currentScene;
+
+        private TextBlock currentText;
+        private Entity entity;
+        private SpriteComponent spriteComponent;
+        private ModelComponent modelComponent;
+        private Model teapot;
+
+        public SensorGameTest()
+        {
+            CurrentVersion = 1;
+            AutoLoadDefaultSettings = true;
+            GraphicsDeviceManager.PreferredGraphicsProfile = new [] { GraphicsProfile.Level_11_0, };
+        }
+
         protected override async Task LoadContent()
         {
             await base.LoadContent();
 
-            font = Asset.Load<SpriteFont>("Arial");
+            font = Asset.Load<SpriteFont>("Font");
+            teapot = Asset.Load<Model>("Teapot");
             batch = new SpriteBatch(GraphicsDevice);
+
+            BuildUI();
+
+            spriteComponent = new SpriteComponent { SpriteProvider = new SpriteFromSheet { Sheet = Asset.Load<SpriteSheet>("SpriteSheet")} };
+            modelComponent = new ModelComponent();
+            entity = new Entity { spriteComponent, modelComponent };
+            SceneSystem.SceneInstance.Scene.Entities.Add(entity);
 
             if (Input.Accelerometer.IsSupported)
                 Input.Accelerometer.IsEnabled = true;
@@ -50,6 +89,87 @@ namespace SiliconStudio.Paradox.Input.Tests
 
             if (Input.Orientation.IsSupported)
                 Input.Orientation.IsEnabled = true;
+
+            ChangeScene(0);
+        }
+
+        private void BuildUI()
+        {
+            var bufferSize = new Vector2(GraphicsDevice.Presenter.BackBuffer.Width, GraphicsDevice.Presenter.BackBuffer.Height);
+            var ui = new UIComponent { VirtualResolution = new Vector3(bufferSize, 500) };
+            SceneSystem.SceneInstance.Scene.Entities.Add(new Entity { ui });
+            
+            currentText = new TextBlock { Font = font, TextColor = Color.White, VerticalAlignment = VerticalAlignment.Bottom, HorizontalAlignment = HorizontalAlignment.Center};
+
+            var buttonBack = new Button { Content = new TextBlock { Font = font, Text = "Previous" }, VerticalAlignment = VerticalAlignment.Bottom, HorizontalAlignment = HorizontalAlignment.Left};
+            var buttonNext = new Button { Content = new TextBlock { Font = font, Text = "Next" }, VerticalAlignment = VerticalAlignment.Bottom, HorizontalAlignment = HorizontalAlignment.Right};
+
+            currentText.SetGridColumn(1);
+            buttonNext.SetGridColumn(2);
+
+            buttonBack.Click += (o, _) => ChangeScene(-1);
+            buttonNext.Click += (o, _) => ChangeScene(+1);
+
+            ui.RootElement = new UniformGrid { Columns = 3, Children = { buttonBack, currentText, buttonNext } };
+        }
+
+        private void ChangeScene(int i)
+        {
+            var sceneCount = Enum.GetNames(typeof(DebugScenes)).Length;
+            currentScene = (DebugScenes)((i + (int)currentScene + sceneCount) % sceneCount);
+
+            currentText.Text = currentScene.ToString();
+
+            spriteComponent.Enabled = false;
+            modelComponent.Enabled = false;
+
+            switch (currentScene)
+            {
+                case DebugScenes.Orientation:
+                    modelComponent.Enabled = true;
+                    modelComponent.Model = teapot;
+                    break;
+                case DebugScenes.UserAccel:
+                    break;
+                case DebugScenes.Gravity:
+                    break;
+                case DebugScenes.RawAccel:
+                    break;
+                case DebugScenes.Gyroscope:
+                    break;
+                case DebugScenes.Compass:
+                    spriteComponent.Enabled = true;
+                    spriteComponent.Color = Color.Red;
+                    spriteComponent.CurrentFrame = 0;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            switch (currentScene)
+            {
+                case DebugScenes.Orientation:
+                    entity.Transform.Rotation = -Input.Orientation.Quaternion;
+                    break;
+                case DebugScenes.UserAccel:
+                    break;
+                case DebugScenes.Gravity:
+                    break;
+                case DebugScenes.RawAccel:
+                    break;
+                case DebugScenes.Gyroscope:
+                    break;
+                case DebugScenes.Compass:
+                    entity.Transform.RotationEulerXYZ = new Vector3(0, 0, Input.Compass.Heading);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -67,7 +187,6 @@ namespace SiliconStudio.Paradox.Input.Tests
                 currentYawPitchRoww = new Vector3(Input.Orientation.Yaw, Input.Orientation.Pitch, Input.Orientation.Roll);
             }
 
-            GraphicsDevice.Clear(GraphicsDevice.BackBuffer, new Color4());
             GraphicsDevice.Clear(GraphicsDevice.DepthStencilBuffer, DepthStencilClearOptions.DepthBuffer);
 
             var targetSize = new Vector2(GraphicsDevice.BackBuffer.Width, GraphicsDevice.BackBuffer.Height);
