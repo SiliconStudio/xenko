5b85a7c62c7d7e3e470e6efd81ef395cab1d031f
Virgile Bello <virgile@siliconstudio.co.jp>
2015-08-31 20:20:43 +0900

    [Assets] Missing files for PackageSaveParameters.

---
+++ sources/assets/SiliconStudio.Assets/PackageSaveSourceFileOperations.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SiliconStudio.Assets
+{
+    public enum PackageSaveSourceFileOperations
+    {
+        None = 0,
+        ReversibleOnly = 1,
+        All = 2,
+    }
+}
