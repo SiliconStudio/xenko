d40918fb61cec4286d84992f3db1627d635dcd2b
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2015-09-02 12:52:50 +0900

    [Materials] Reenable shader compute nodes [PDX-1843]

--- sources/engine/SiliconStudio.Paradox.Engine/Rendering/Materials/ComputeColors/ComputeShaderClassScalar.cs
+++ sources/engine/SiliconStudio.Paradox.Engine/Rendering/Materials/ComputeColors/ComputeShaderClassScalar.cs
@@ -11,7 +11,7 @@ namespace SiliconStudio.Paradox.Rendering.Materials.ComputeColors
     [DataContract("ComputeShaderClassScalar")]
     [Display("Shader")]
     // TODO: This class has been made abstract to be removed from the editor - unabstract it to re-enable it!
-    public abstract class ComputeShaderClassScalar : ComputeShaderClassBase<IComputeScalar>, IComputeScalar
+    public class ComputeShaderClassScalar : ComputeShaderClassBase<IComputeScalar>, IComputeScalar
     {
     }
 }
