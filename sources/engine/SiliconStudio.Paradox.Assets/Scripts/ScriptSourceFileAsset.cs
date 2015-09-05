ab818d2e8c509e44741bf54c471ec6049b21ecf8
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-09-05 19:04:42 +0900

    [ScriptAsset] added preliminary discovery code

--- sources/engine/SiliconStudio.Paradox.Assets/Scripts/ScriptSourceFileAsset.cs
+++ sources/engine/SiliconStudio.Paradox.Assets/Scripts/ScriptSourceFileAsset.cs
@@ -10,29 +10,45 @@ using SiliconStudio.Paradox.Engine;
 
 namespace SiliconStudio.Paradox.Assets.Scripts
 {
+    public interface IScriptTypesProvider
+    {
+        IReadOnlyCollection<Type> GetSourcePathTypes(string path);
+    }
+
     [DataContract("EffectCompositorAsset")]
     [AssetDescription(".cs")]
-    [Display(95, "Script Source Code", "A script source code file")]
+    [Display(95, "Script Source Code", "A C# source code file")]
     public sealed class ScriptSourceFileAsset : SourceCodeAsset
     {
         public ScriptSourceFileAsset()
         {
-            Debug.WriteLine("x");
         }
 
         public IReadOnlyCollection<Type> GetContainedScriptTypes()
         {
-            // TODO: properly return the list of script types.
-            return new List<Type> { typeof(TestScript) };
+            lock(providerLocker)
+            {
+                if(provider != null)
+                {
+                    return provider.GetSourcePathTypes(AbsoluteSourceLocation);
+                }
+                else
+                {
+                    return null;
+                }
+            }
         }
-    }
 
-    // TODO: delete this
-    public class TestScript : AsyncScript
-    {
-        public override Task Execute()
+        private static object providerLocker = new object();
+
+        private static IScriptTypesProvider provider;
+
+        public static void SetProvider(IScriptTypesProvider p)
         {
-            throw new NotImplementedException();
+            lock(providerLocker)
+            {
+                provider = p;
+            }
         }
     }
 }
