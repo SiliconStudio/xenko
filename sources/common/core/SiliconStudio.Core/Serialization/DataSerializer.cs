f37147822fa10b3885931776722d3885fcfe692d
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-09-18 17:12:27 +0900

    [Build] Bump version to 1.3.2-beta

--- sources/common/core/SiliconStudio.Core/Serialization/DataSerializer.cs
+++ sources/common/core/SiliconStudio.Core/Serialization/DataSerializer.cs
@@ -13,7 +13,7 @@ namespace SiliconStudio.Core.Serialization
     public abstract class DataSerializer
     {
         // Binary format version, needs to be bumped in case of big changes in serialization formats (i.e. primitive types).
-        public const int BinaryFormatVersion = 13;
+        public const int BinaryFormatVersion = 14;
 
         public ObjectId SerializationTypeId;
 
