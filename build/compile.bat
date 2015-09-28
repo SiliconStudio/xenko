05d935528fbfb75eda040c971382d98cc3ad76cf
Emmanuel Stapf <emmanuel.stapf@siliconstudio.co.jp>
2015-09-28 16:50:16 +0900

    [CoreCLR] Added batch script that compiles the Direct3D target.

---
+++ build/compile.bat
@@ -0,0 +1,3 @@
+rem "\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /verbosity:normal /p:Configuration=Release /p:Platform="Any CPU" Paradox.OpenGLES.sln
+"\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /verbosity:normal /p:Configuration=Debug /p:Platform="Mixed Platforms" Paradox.Direct3D.sln
+
