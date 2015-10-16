cc00d27d5be2d9c29c8c1c5a558e7c3431ec9555
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-10-16 11:35:57 +0900

    [TemplateScripts] Added the required code to actually instantiate template scripts

---
+++ sources/assets/SiliconStudio.Assets/Templates/TemplateAssetDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core;
+
+namespace SiliconStudio.Assets.Templates
+{
+    [DataContract]
+    public class TemplateAssetDescription : TemplateDescription
+    {
+
+    }
+}
