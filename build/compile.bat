659c52fcc6f2b967e4f98d4bbd36b51345f1f260
Emmanuel Stapf <emmanuel.stapf@siliconstudio.co.jp>
2015-10-22 15:32:01 +0900

    [CoreCLR] Updated batch file to compile the major solution and to stop at the first solution that fails. Useful to ensure that a simple modification works for all our combinations of platforms.

--- build/compile.bat
+++ build/compile.bat
@@ -1,3 +1,29 @@
-rem "\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /verbosity:normal /p:Configuration=Release /p:Platform="Any CPU" Paradox.OpenGLES.sln
-"\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m /verbosity:normal /p:Configuration=Debug /p:Platform="Mixed Platforms" Paradox.Direct3D.sln
+@echo off
 
+set XXMSBUILD="\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set _option=/m /verbosity:normal /p:Configuration=Debug /p:Platform="Mixed Platforms"
+
+set Project=Paradox.sln
+%XXMSBUILD%  %_option% %Project%
+if %ERRORLEVEL% != 0 goto error
+
+set Project=Paradox.Direct3D.sln
+%XXMSBUILD%  %_option% %Project%
+if %ERRORLEVEL% != 0 goto error
+
+set Project=Paradox.Direct3D.SDL.sln
+%XXMSBUILD%  %_option% %Project%
+if %ERRORLEVEL% != 0 goto error
+
+set Project=Paradox.Direct3D.CoreCLR.sln
+%XXMSBUILD%  %_option% %Project%
+if %ERRORLEVEL% != 0 goto error
+
+goto exit
+
+:error
+echo "Error while compiling project: " %Project%
+
+:exit
+
+@echo on
