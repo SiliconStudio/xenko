b0805fd46007d4d62220102e98ecac70830d88d3
Emmanuel Stapf <manu-silicon@users.noreply.github.com>
2015-10-26 10:01:17 +0900

    [Warnings] Clarify why we have implemented an empty `add' and `remove' event entry eventhough it might look wrong at first sight.

--- sources/engine/SiliconStudio.Xenko.Graphics/GraphicsDeviceServiceLocal.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics/GraphicsDeviceServiceLocal.cs
@@ -34,28 +34,12 @@ namespace SiliconStudio.Xenko.Graphics
             GraphicsDevice = graphicsDevice;
         }
 
-        public event EventHandler<EventArgs> DeviceCreated {
-            add { }
-            remove { }
-        }
-
-        public event EventHandler<EventArgs> DeviceDisposing
-        {
-            add { }
-            remove { }
-        }
-
-        public event EventHandler<EventArgs> DeviceReset
-        {
-            add { }
-            remove { }
-        }
-
-        public event EventHandler<EventArgs> DeviceResetting
-        {
-            add { }
-            remove { }
-        }
+			// We provide an empty `add' and `remove' to avoid a warning about unused events that we have
+			// to implement as they are part of the IGraphicsDeviceService definition.
+        public event EventHandler<EventArgs> DeviceCreated { add { } remove { } }
+        public event EventHandler<EventArgs> DeviceDisposing { add { } remove { } }
+        public event EventHandler<EventArgs> DeviceReset { add { } remove { } }
+        public event EventHandler<EventArgs> DeviceResetting { add { } remove { } }
 
         public GraphicsDevice GraphicsDevice { get; private set; }
     }
