83de5572f8ab26bb9491a578367f201b3396bf30
Pierre Rahier <pierre@siliconstudio.co.jp>
2015-10-28 14:30:49 +0900

    [Graphics.Tests] Fix and reactivate TestImageEffect.

--- sources/engine/SiliconStudio.Xenko.Graphics.Tests/TestImageEffect.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics.Tests/TestImageEffect.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
-using System.Diagnostics;
-using System.IO;
+
 using System.Threading.Tasks;
 
 using NUnit.Framework;
-
+using SiliconStudio.Core.Diagnostics;
 using SiliconStudio.Xenko.Rendering;
 using SiliconStudio.Xenko.Rendering.Images;
 using SiliconStudio.Xenko.Games;
@@ -19,13 +18,14 @@ namespace SiliconStudio.Xenko.Graphics.Tests
         private RenderContext drawEffectContext;
 
         private Texture hdrTexture;
-
         private Texture hdrRenderTexture;
+
         private PostProcessingEffects postProcessingEffects;
 
         public TestImageEffect()
         {
             CurrentVersion = 1;
+            GraphicsDeviceManager.PreferredGraphicsProfile = new[] { GraphicsProfile.Level_10_0 };
             GraphicsDeviceManager.PreferredBackBufferWidth = 760;
             GraphicsDeviceManager.PreferredBackBufferHeight = 1016;
         }
@@ -34,42 +34,44 @@ namespace SiliconStudio.Xenko.Graphics.Tests
         {
             base.RegisterTests();
 
-            FrameGameSystem.Draw(DrawCustomEffect).TakeScreenshot();
+            FrameGameSystem.TakeScreenshot();
         }
 
         protected override async Task LoadContent()
         {
             await base.LoadContent();
 
-            hdrTexture = await Asset.LoadAsync<Texture>("Atrium");
-            hdrRenderTexture = Texture.New2D(GraphicsDevice, hdrTexture.Width, hdrTexture.Height, 1, hdrTexture.Format, TextureFlags.ShaderResource | TextureFlags.RenderTarget);
+            hdrTexture = Asset.Load<Texture>("HdrTexture");
+            hdrRenderTexture = Texture.New2D(GraphicsDevice, hdrTexture.Width, hdrTexture.Height, 1, hdrTexture.Description.Format, TextureFlags.ShaderResource | TextureFlags.RenderTarget);
             drawEffectContext = RenderContext.GetShared(Services);
             postProcessingEffects = new PostProcessingEffects(drawEffectContext);
             postProcessingEffects.BrightFilter.Threshold = 100.0f;
             postProcessingEffects.Bloom.DownScale = 2;
+            postProcessingEffects.Bloom.Enabled = true;
+            postProcessingEffects.Bloom.ShowOnlyBloom = true;
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            base.Draw(gameTime);
+            if (!ScreenShotAutomationEnabled)
+                AdjustEffectParameters();
 
-            if(!ScreenShotAutomationEnabled)
-                DrawCustomEffect();
+            DrawCustomEffect();
+
+            base.Draw(gameTime);
         }
 
-        private void DrawCustomEffect()
+        private void AdjustEffectParameters()
         {
-            GraphicsDevice.Copy(hdrTexture, hdrRenderTexture);
-
             if (Input.IsKeyDown(Keys.Left))
             {
                 postProcessingEffects.BrightFilter.Threshold -= 10.0f;
-                Trace.WriteLine(string.Format("BrightFilter Threshold: {0}", postProcessingEffects.BrightFilter.Threshold));
+                Log.Info("BrightFilter Threshold: {0}", postProcessingEffects.BrightFilter.Threshold);
             }
             else if (Input.IsKeyDown(Keys.Right))
             {
                 postProcessingEffects.BrightFilter.Threshold += 10.0f;
-                Trace.WriteLine(string.Format("BrightFilter Threshold: {0}", postProcessingEffects.BrightFilter.Threshold));
+                Log.Info("BrightFilter Threshold: {0}", postProcessingEffects.BrightFilter.Threshold);
             }
 
             postProcessingEffects.Bloom.Enabled = !Input.IsKeyDown(Keys.Space);
@@ -77,14 +79,17 @@ namespace SiliconStudio.Xenko.Graphics.Tests
             if (Input.IsKeyDown(Keys.Down))
             {
                 postProcessingEffects.Bloom.Amount += -0.01f;
-                Trace.WriteLine(string.Format("Bloom Amount: {0}", postProcessingEffects.Bloom.Amount));
+                Log.Info("Bloom Amount: {0}", postProcessingEffects.Bloom.Amount);
             }
             else if (Input.IsKeyDown(Keys.Up))
             {
                 postProcessingEffects.Bloom.Amount += +0.01f;
-                Trace.WriteLine(string.Format("Bloom Amount: {0}", postProcessingEffects.Bloom.Amount));
+                Log.Info("Bloom Amount: {0}", postProcessingEffects.Bloom.Amount);
             }
-
+        }
+        private void DrawCustomEffect()
+        {
+            GraphicsDevice.CopyRegion(hdrTexture, 0, null, hdrRenderTexture, 0);
 
             postProcessingEffects.SetInput(hdrRenderTexture);
             postProcessingEffects.SetOutput(GraphicsDevice.BackBuffer);
