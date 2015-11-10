5c0fe50eb458d52de4ae08d2c4330941fd1a5163
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2015-11-10 12:32:35 +0900

    [Assets] Add tests for asset inheritance with overrides

--- sources/assets/SiliconStudio.Assets.Tests/TestAssetInheritance.cs
+++ sources/assets/SiliconStudio.Assets.Tests/TestAssetInheritance.cs
@@ -3,16 +3,55 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
+using SiliconStudio.Core.Reflection;
 
 namespace SiliconStudio.Assets.Tests
 {
     [TestFixture]
     public class TestAssetInheritance
     {
+
         [Test]
-        public void TestCreateChildAssetWithParts()
+        public void TestWithOverrides()
         {
+            // Create a derivative asset and check overrides
+            var assets = new List<TestAssetWithParts>();
+            var assetItems = new List<AssetItem>();
+
+            var objDesc = TypeDescriptorFactory.Default.Find(typeof(TestAssetWithParts));
+            var memberDesc = objDesc.Members.First(t => t.Name == "Name");
+
+            assets.Add(new TestAssetWithParts()
+            {
+                Parts =
+                {
+                        new AssetPart(Guid.NewGuid()),
+                        new AssetPart(Guid.NewGuid())
+                }
+            });
+            assetItems.Add(new AssetItem("asset-0", assets[0]));
+
+            // Set a sealed on Name property
+            assets[0].SetOverride(memberDesc, OverrideType.Sealed);
+
+            var childAsset = (TestAssetWithParts)assetItems[0].CreateChildAsset();
+
+            // Check that child asset has a base
+            Assert.NotNull(childAsset.Base);
+
+            // Check that derived asset is using base
+            Assert.AreEqual(OverrideType.Base, childAsset.GetOverride(memberDesc));
+
+            // Check that property Name on base asset is sealed
+            Assert.AreEqual(OverrideType.Sealed, childAsset.Base.Asset.GetOverride(memberDesc));
+        }
+
+        [Test]
+        public void TestWithParts()
+        {
+            // Create a derivative asset with asset parts
             var project = new Package();
             var assets = new List<TestAssetWithParts>();
             var assetItems = new List<AssetItem>();
