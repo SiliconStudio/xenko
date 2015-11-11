ff1e236123c8501a8f6bb44c992c880ab16e4f35
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2015-11-11 18:47:49 +0900

    [Particles] Copied most of the test code to a clean branch.
    Purpose of the task is to test low-level memory access patterns and the ParticlePool design.

---
+++ sources/engine/SiliconStudio.Xenko.Particles/Particle.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
+using SiliconStudio.Core;
+
+namespace SiliconStudio.Xenko.Particles
+{
+    public struct  Particle
+    {
+        public readonly IntPtr Pointer;
+
+        public Particle(IntPtr pointer)
+        {
+            Pointer = pointer;
+        }
+
+        #region Accessors
+
+
+        /// <summary>
+        /// Gets the particle's field value.
+        /// </summary>
+        /// <typeparam name="T">The field type.</typeparam>
+        /// <param name="accessor">The field accessor</param>
+        /// <returns>The field value.</returns>
+        public T Get<T>(ParticleFieldAccessor accessor) where T : struct
+        {
+        //    return ParticleUtilities.ToStruct<T>(Pointer + accessor);
+              return Utilities.Read<T>(Pointer + accessor);
+        }
+
+        /// <summary>
+        /// Gets the particle's field value.
+        /// </summary>
+        /// <typeparam name="T">The field type.</typeparam>
+        /// <param name="accessor">The field accessor</param>
+        /// <returns>The field value.</returns>
+        public T Get<T>(ParticleFieldAccessor<T> accessor) where T : struct
+        {
+        //    return ParticleUtilities.ToStruct<T>(Pointer + accessor);
+            return Utilities.Read<T>(Pointer + accessor);
+        }
+
+        /// <summary>
+        /// Sets the particle's field to a value.
+        /// </summary>
+        /// <typeparam name="T">The field type.</typeparam>
+        /// <param name="accessor">The field accessor</param>
+        /// <param name="value">The value to set</param>
+        public void Set<T>(ParticleFieldAccessor<T> accessor, ref T value) where T : struct
+        {
+        //    ParticleUtilities.ToPtr(value, Pointer + accessor);
+            Utilities.Write(Pointer + accessor, ref value);
+        }
+
+        /// <summary>
+        /// Sets the particle's field to a value.
+        /// </summary>
+        /// <typeparam name="T">The field type.</typeparam>
+        /// <param name="accessor">The field accessor</param>
+        /// <param name="value">The value to set</param>
+        public void Set<T>(ParticleFieldAccessor<T> accessor, T value) where T : struct
+        {
+        //    ParticleUtilities.ToPtr(value, Pointer + accessor);
+            Utilities.Write(Pointer + accessor, ref value);
+        }
+
+        #endregion
+
+        public IntPtr this[ParticleFieldAccessor accessor]
+        {
+            get { return Pointer + accessor; }
+        }
+
+        public static implicit operator IntPtr(Particle particle) => particle.Pointer;
+    }
+}
---
+++ sources/engine/SiliconStudio.Xenko.Particles/ParticleFieldAccessor.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+namespace SiliconStudio.Xenko.Particles
+{
+    public struct ParticleFieldAccessor
+    {
+        // Not sure about this class. I copied it from the old implementation, but I think it can be removed.
+        // Maybe change ParticleFieldDescription and use it directly?
+        private readonly int offset;
+
+        internal ParticleFieldAccessor(ParticleField field)
+        {
+            offset = field.Offset;
+        }
+
+        public ParticleFieldAccessor(int offset)
+        {
+            this.offset = offset;
+        }
+
+        public bool IsValid() => (offset != -1);
+
+        public static implicit operator int(ParticleFieldAccessor accessor) => accessor.offset;
+    }
+
+    public struct ParticleFieldAccessor<T>
+    {
+        private readonly int offset;
+
+        internal ParticleFieldAccessor(ParticleField field)
+        {
+            offset = field.Offset;
+        }
+
+        public ParticleFieldAccessor(int offset)
+        {
+            this.offset = offset;
+        }
+
+        public bool IsValid() => (offset != -1);
+
+        public static implicit operator ParticleFieldAccessor(ParticleFieldAccessor<T> accessor) => new ParticleFieldAccessor(accessor.offset);
+
+        public static implicit operator int(ParticleFieldAccessor<T> accessor) => accessor.offset;
+    }
+}
