e6a56e2b33de7b35f4506e93ae58342f8c45119b
Virgile Bello <virgile@siliconstudio.co.jp>
2015-11-12 10:51:21 +0900

    [Reflection] AssemblyRegistry.GetTypeFromAlias also supports DataContractAliasAttribute.

--- sources/common/core/SiliconStudio.AssemblyProcessor.Common/DataContractAliasProcessor.cs
+++ sources/common/core/SiliconStudio.AssemblyProcessor.Common/DataContractAliasProcessor.cs
@@ -12,7 +12,7 @@ namespace SiliconStudio.AssemblyProcessor
         {
             foreach (var type in context.Assembly.EnumerateTypes())
             {
-                var dataContractAttribute = type.CustomAttributes.FirstOrDefault(x => x.AttributeType.FullName == "SiliconStudio.Core.DataContractAttribute");
+                var dataContractAttribute = type.CustomAttributes.FirstOrDefault(x => x.AttributeType.FullName == "SiliconStudio.Core.DataContractAttribute" || x.AttributeType.FullName == "SiliconStudio.Core.DataAliasAttribute");
 
                 // Only process if ctor with 1 argument
                 if (dataContractAttribute == null || !dataContractAttribute.HasConstructorArguments || dataContractAttribute.ConstructorArguments.Count != 1)
