e98b07f85c6554590597b1b98132359ef72e0118
Manu <manu-silicon@users.noreply.github.com>
2015-11-13 13:52:22 +0900

    [Android] Fixed typo on #ifdef.

--- sources/engine/SiliconStudio.Xenko.Engine/Starter/AndroidXenkoActivity.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Starter/AndroidXenkoActivity.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-#if !SILICONSTUDIO_PLATFORM_ANDROID
+#if SILICONSTUDIO_PLATFORM_ANDROID
 using System;
 using Android.App;
 using Android.Graphics;
