f96d21a10f3adadb080d474836afed2fb5909ffa
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2015-11-16 19:10:38 +0900

    [ActionStack] Unit tests for TransactionalActionStack

---
+++ sources/common/presentation/SiliconStudio.ActionStack.Tests/TransactionalActionStackTestContainer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SiliconStudio.ActionStack.Tests
+{
+    class TransactionalActionStackTestContainer : ActionStackTestContainer
+    {
+        public TransactionalActionStackTestContainer(int capacity)
+            : base(new TransactionalActionStack(capacity))
+        {
+            RegisterEvents();
+        }
+
+        public new TransactionalActionStack Stack => (TransactionalActionStack)base.Stack;
+
+        public List<Tuple<object, EventArgs>> TransactionStarted = new List<Tuple<object, EventArgs>>();
+        public List<Tuple<object, ActionItemsEventArgs<IActionItem>>> TransactionEnded = new List<Tuple<object, ActionItemsEventArgs<IActionItem>>>();
+        public List<Tuple<object, ActionItemsEventArgs<IActionItem>>> TransactionCancelled = new List<Tuple<object, ActionItemsEventArgs<IActionItem>>>();
+        public List<Tuple<object, ActionItemsEventArgs<IActionItem>>> TransactionDiscarded = new List<Tuple<object, ActionItemsEventArgs<IActionItem>>>();
+
+        private void RegisterEvents()
+        {
+            Stack.TransactionStarted += (sender, e) => TransactionStarted.Add(Tuple.Create(sender, e));
+            Stack.TransactionEnded += (sender, e) => TransactionEnded.Add(Tuple.Create(sender, e));
+            Stack.TransactionCancelled += (sender, e) => TransactionCancelled.Add(Tuple.Create(sender, e));
+            Stack.TransactionDiscarded += (sender, e) => TransactionDiscarded.Add(Tuple.Create(sender, e));
+        }
+
+        public void CheckTransactionCount(int begun, int ended, int cancelled, int discarded)
+        {
+            Assert.AreEqual(begun, TransactionStarted.Count);
+            Assert.AreEqual(ended, TransactionEnded.Count);
+            Assert.AreEqual(cancelled, TransactionCancelled.Count);
+            Assert.AreEqual(discarded, TransactionDiscarded.Count);
+        }
+    }
+}
