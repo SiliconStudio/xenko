e730f59d3b34dc9457a6018089a0cf701d8b3d92
Manu <manu-silicon@users.noreply.github.com>
2015-11-16 10:49:29 +0900

    [General] Removed reference to SILICONSTUDIO_RUNTIME_CORECLR since this branch is just for SDL, not CoreCLR.

--- sources/engine/SiliconStudio.Xenko/Graphics/StandardImageHelper.Windows.cs
+++ sources/engine/SiliconStudio.Xenko/Graphics/StandardImageHelper.Windows.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
-#if SILICONSTUDIO_PLATFORM_WINDOWS_DESKTOP || SILICONSTUDIO_RUNTIME_CORECLR
+#if SILICONSTUDIO_PLATFORM_WINDOWS_DESKTOP
 using System;
 #if !SILICONSTUDIO_UI_SDL_ONLY
 using System.Drawing;
