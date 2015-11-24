da85f4516ae32f9577308d7db101989280a744da
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2015-11-24 14:25:46 +0900

    [Particles] ParticleSystemComponentRenderer now uses ParticleBatch (a copy of the Shader3DBatch, content will change later)

---
+++ sources/engine/SiliconStudio.Xenko.Particles/Shaders.Bytecodes/ParticleBatch.Extensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Xenko.Shaders;
+
+namespace SiliconStudio.Xenko.Particles
+{
+    public partial class ParticleBatch
+    {
+        private static EffectBytecode bytecode = null;
+        private static EffectBytecode bytecodeSRgb = null;
+
+        internal static EffectBytecode Bytecode
+        {
+            get
+            {
+                return bytecode ?? (bytecode = EffectBytecode.FromBytesSafe(binaryBytecode));
+            }
+        }
+
+        internal static EffectBytecode BytecodeSRgb
+        {
+            get
+            {
+                return bytecodeSRgb ?? (bytecodeSRgb = EffectBytecode.FromBytesSafe(binaryBytecodeSRgb));
+            }
+        }
+    }
+}
