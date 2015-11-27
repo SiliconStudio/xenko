d95f6bda2d2b6cda113b59bf0ed8cf7da20c69bc
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-11-27 15:53:03 +0900

    [Xenko.Native] small cosmetics fixes

--- sources/engine/SiliconStudio.Xenko.Native/SiliconStudio.Xenko.Native.h
+++ sources/engine/SiliconStudio.Xenko.Native/SiliconStudio.Xenko.Native.h
@@ -1,6 +1,10 @@
+// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
 #pragma once
 
 #include <NativeMath.h>
+#include <NativeMemory.h>
 
 #pragma pack(push, 4)
 typedef struct Vector2
--- sources/engine/SiliconStudio.Xenko.Native/SpriteBatchNative.c
+++ sources/engine/SiliconStudio.Xenko.Native/SpriteBatchNative.c
@@ -1,3 +1,6 @@
+// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
 #include "SiliconStudio.Xenko.Native.h"
 
 void UpdateBufferValuesFromElementInfoNative(SpriteDrawInfo* drawInfo, VertexPositionColorTextureSwizzle* vertexPointer, void* indexPointer, int vertexStartOffset)
