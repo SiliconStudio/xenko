970c551c44160075075c1e9b161a192c373c0a2e
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2015-12-07 19:38:04 +0900

    [OpenGL] Add support for BGRA sRGB textures

--- sources/engine/SiliconStudio.Xenko.Graphics/OpenGL/OpenGLConvertExtensions.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics/OpenGL/OpenGLConvertExtensions.cs
@@ -389,6 +389,13 @@ namespace SiliconStudio.Xenko.Graphics
                     type = PixelType.UnsignedByte;
                     pixelSize = 4;
                     break;
+                case PixelFormat.B8G8R8A8_UNorm_SRgb:
+                    // TODO: Check on iOS/Android and OpenGL 3
+                    internalFormat = graphicsDevice.currentVersionMajor < 3 ? SrgbAlpha : Srgb8Alpha8;
+                    format = graphicsDevice.currentVersionMajor < 3 ? (PixelFormatGl)SrgbAlpha : PixelFormatGl.Bgra;
+                    type = PixelType.UnsignedByte;
+                    pixelSize = 4;
+                    break;
                 case PixelFormat.R16_Float:
                     internalFormat = R16f;
                     format = PixelFormatGl.Red;
