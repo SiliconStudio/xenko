4c5c02430beeac2a5af695633dca6e0c90cdc078
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2015-12-09 15:11:31 +0900

    [Presentation] Removed unused observable collections.

--- sources/common/presentation/SiliconStudio.Presentation/Collections/IObservableList.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Collections/IObservableList.cs
@@ -2,6 +2,10 @@
 
 namespace SiliconStudio.Presentation.Collections
 {
+    /// <summary>
+    /// This interface regroups the <see cref="IList{T}"/> interface and the <see cref="IObservableCollection{T}"/> interface.
+    /// </summary>
+    /// <typeparam name="T">The type of item contained in the list.</typeparam>
     public interface IObservableList<T> : IList<T>, IObservableCollection<T>
     {
         void AddRange(IEnumerable<T> items);
