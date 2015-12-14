9a47a39b0b0e7f6fd2d183ef04d43b8ae7b0816a
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2015-12-14 15:53:26 +0900

    [Particles] Bug fix - ParticleSystem was using the local transform rather than the world transform

--- sources/engine/SiliconStudio.Xenko.Particles/Components/ParticleSystemProcessor.cs
+++ sources/engine/SiliconStudio.Xenko.Particles/Components/ParticleSystemProcessor.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections.Generic;
 using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
 using SiliconStudio.Xenko.Engine;
 using SiliconStudio.Xenko.Games;
 using SiliconStudio.Xenko.Rendering;
@@ -48,9 +49,38 @@ namespace SiliconStudio.Xenko.Particles.Components
 
                     // We must update the TRS location of the particle system prior to updating the system itself.
                     // Particles only handle uniform scale.
-                    particleSystem.Translation = transformComponent.Position;
-                    particleSystem.Rotation = transformComponent.Rotation;
-                    particleSystem.UniformScale = transformComponent.Scale.X;   // This is incorrect for non-uniform scale vectors.
+
+                    if (transformComponent.Parent == null)
+                    {
+                        // The transform doesn't have a parent. Local transform IS world transform
+
+                        particleSystem.Translation = transformComponent.Position;   // This is the local position!
+
+                        particleSystem.UniformScale = transformComponent.Scale.X;   // This is the local scale!
+
+                        particleSystem.Rotation = transformComponent.Rotation;      // This is the local rotation!
+                    }
+                    else
+                    {
+                        // The transform has a parent - do not use local transform values, instead check the world matrix
+
+                        // Position
+                        particleSystem.Translation = new Vector3(transformComponent.WorldMatrix.M41, transformComponent.WorldMatrix.M42, transformComponent.WorldMatrix.M43);
+
+                        // Scale
+                        var uniformScaleX = new Vector3(transformComponent.WorldMatrix.M11, transformComponent.WorldMatrix.M12, transformComponent.WorldMatrix.M13);
+                        particleSystem.UniformScale = uniformScaleX.Length();
+
+                        // Rotation
+                        // TODO Maybe implement Quaternion.RotationMatrix which also handles cases when the matrix has scaling?
+                        var invScl = (particleSystem.UniformScale > 0) ? 1f / particleSystem.UniformScale : 1f;
+                        var rotMatrix = new Matrix(
+                            transformComponent.WorldMatrix.M11 * invScl, transformComponent.WorldMatrix.M12 * invScl, transformComponent.WorldMatrix.M13 * invScl, 0,
+                            transformComponent.WorldMatrix.M21 * invScl, transformComponent.WorldMatrix.M22 * invScl, transformComponent.WorldMatrix.M23 * invScl, 0,
+                            transformComponent.WorldMatrix.M31 * invScl, transformComponent.WorldMatrix.M32 * invScl, transformComponent.WorldMatrix.M33 * invScl, 0,
+                            0, 0, 0, 1);
+                        Quaternion.RotationMatrix(ref rotMatrix, out particleSystem.Rotation);
+                    }
 
                     particleSystem.Update(0.016f * speed);
 
