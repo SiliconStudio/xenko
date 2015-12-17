73c0242427f3162158d460d0adbc9785b64ca1c0
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2015-12-17 14:21:10 +0900

    [Particles] (Temp) Moving vertex building to the ParticleVertexBuffer, first commit

--- sources/engine/SiliconStudio.Xenko.Particles/VertexLayouts/ParticleVertexBuffer.cs
+++ sources/engine/SiliconStudio.Xenko.Particles/VertexLayouts/ParticleVertexBuffer.cs
@@ -14,10 +14,23 @@ namespace SiliconStudio.Xenko.Particles.VertexLayouts
     {
         public VertexDeclaration VertexDeclaration { get; private set; }
 
+        protected const int OffsetPosition  = 0;
+        protected const int OffsetUv        = 12 + OffsetPosition;
+        protected const int OffsetColor     = 8  + OffsetUv;
+        protected const int OffsetLifetime  = 4  + OffsetColor;
+        protected const int OffsetRandom    = 4  + OffsetLifetime;
+        protected const int OffsetMax       = 4  + OffsetRandom;
+
+        private int verticesPerParticle = 4;
+        private int verticesPerQuad = 4;
+        private int indicesPerQuad = 6;
+
         private readonly int vertexStructSize;
         private readonly int indexStructSize;
 
         private DeviceResourceContext ResourceContext;
+
+        private readonly Dictionary<AttributeDescription, AttributeAccessor> availableAttributes;
 
         public ParticleVertexBuffer()
         {
@@ -32,6 +45,37 @@ namespace SiliconStudio.Xenko.Particles.VertexLayouts
                     );
 
             indexStructSize = sizeof(short);
+
+            availableAttributes = new Dictionary<AttributeDescription, AttributeAccessor>();
+            var totalOffset = 0;
+            foreach (var vertexElement in VertexDeclaration.VertexElements)
+            {
+                var attrDesc = new AttributeDescription(vertexElement.SemanticName);
+                var stride = vertexElement.Format.SizeInBytes();
+                var attrAccs = new AttributeAccessor { Offset = totalOffset, Stride = stride };
+                totalOffset += stride;
+
+                availableAttributes.Add(attrDesc, attrAccs);
+            }
+        }
+
+        private bool bufferIsDirty = true;
+        private int requiredQuads = 0;
+        private int livingQuads = 0;
+
+        public void SetRequiredQuads(int quadsPerParticle, int livingParticles, int totalParticles)
+        {
+            verticesPerParticle = quadsPerParticle * verticesPerQuad;
+            var minQuads = quadsPerParticle * livingParticles;
+            var maxQuads = quadsPerParticle * totalParticles;
+
+            livingQuads = minQuads;
+
+            if (minQuads > requiredQuads || maxQuads <= requiredQuads / 2)
+            {
+                requiredQuads = maxQuads;
+                bufferIsDirty = true;
+            }
         }
 
         private unsafe void InitBuffer(GraphicsDevice device, Effect effect, int vertexCount, int indexCount)
@@ -39,15 +83,13 @@ namespace SiliconStudio.Xenko.Particles.VertexLayouts
             //    ResourceContext = device.GetOrCreateSharedData(GraphicsDeviceSharedDataType.PerContext, "ResourceKey",
             //        d => new DeviceResourceContext(device, effect, VertexDeclaration, vertexCount, indexStructSize, indexCount));
 
-            indexCount = (indexCount / 6) * 6;
-
             ResourceContext = new DeviceResourceContext(device, effect, VertexDeclaration, vertexCount, indexStructSize, indexCount);
 
             var mappedIndices = device.MapSubresource(ResourceContext.IndexBuffer, 0, MapMode.WriteDiscard, false, 0, indexCount * indexStructSize);
             var indexPointer = mappedIndices.DataBox.DataPointer;
 
             var k = 0;
-            for (var i = 0; i < indexCount; k += 4)
+            for (var i = 0; i < indexCount; k += verticesPerQuad)
             {
                 *(short*)(indexPointer + indexStructSize * i++) = (short)(k + 0);
                 *(short*)(indexPointer + indexStructSize * i++) = (short)(k + 1);
@@ -61,30 +103,84 @@ namespace SiliconStudio.Xenko.Particles.VertexLayouts
         }
 
         private MappedResource mappedVertices;
-        private bool hasStarted = false;
+        private IntPtr vertexBuffer = IntPtr.Zero;
+        private IntPtr vertexBufferOrigin = IntPtr.Zero;
 
-        public IntPtr StartBuffer(GraphicsDevice device, Effect effect)
+        internal IntPtr StartBuffer(GraphicsDevice device, Effect effect)
         {
-            if (!hasStarted)
+            if (bufferIsDirty && requiredQuads > 0)
             {
-                InitBuffer(device, effect, 1024 * 4, 1024 * 6);
-                hasStarted = true;
+                InitBuffer(device, effect, requiredQuads * verticesPerQuad, requiredQuads * indicesPerQuad);
+                bufferIsDirty = false;
             }
 
+            vertexBuffer = IntPtr.Zero;
+            vertexBufferOrigin = IntPtr.Zero;
+
+            if (bufferIsDirty)
+                return IntPtr.Zero;
+
             mappedVertices = device.MapSubresource(ResourceContext.VertexBuffer, 0, MapMode.WriteDiscard, false, 0, ResourceContext.VertexCount * vertexStructSize);
+
+            vertexBuffer        = mappedVertices.DataBox.DataPointer;
+            vertexBufferOrigin  = mappedVertices.DataBox.DataPointer;
 
             return mappedVertices.DataBox.DataPointer;
         }
 
-        public void FlushBuffer(GraphicsDevice device, int indexCount)
+        internal void RestartBuffer()
         {
+            vertexBuffer = vertexBufferOrigin;
+        }
+
+        internal void FlushBuffer(GraphicsDevice device)
+        {
+            if (bufferIsDirty)
+                return;
+
+            vertexBuffer = IntPtr.Zero;
+            vertexBufferOrigin = IntPtr.Zero;
+
             device.UnmapSubresource(mappedVertices);
 
             device.SetVertexArrayObject(ResourceContext.VertexArrayObject);
 
-            device.DrawIndexed(PrimitiveType.TriangleList, indexCount, ResourceContext.IndexBufferPosition);
+            device.DrawIndexed(PrimitiveType.TriangleList, livingQuads * indicesPerQuad, ResourceContext.IndexBufferPosition);
         }
-       
+
+        internal void NextVertex()
+        {
+            vertexBuffer += VertexDeclaration.VertexStride;
+        }
+
+        internal void NextParticle()
+        {
+            vertexBuffer += VertexDeclaration.VertexStride * verticesPerParticle;
+        }
+
+        internal AttributeAccessor GetAccessor(AttributeDescription desc) 
+        {            
+            AttributeAccessor accessor;
+            if (!availableAttributes.TryGetValue(desc, out accessor))
+            {
+                return new AttributeAccessor { Offset = 0, Stride = 0 };
+            }
+            
+            return accessor;
+        }
+
+        internal void SetAttribute(AttributeAccessor accessor, IntPtr ptrRef) 
+        {
+            Utilities.CopyMemory(vertexBuffer + accessor.Offset, ptrRef, accessor.Stride);
+        }
+
+        internal void SetAttributePerParticle(AttributeAccessor accessor, IntPtr ptrRef)
+        {
+            for (var i = 0; i < verticesPerParticle; i++)
+            {
+                Utilities.CopyMemory(vertexBuffer + accessor.Offset + i * VertexDeclaration.VertexStride, ptrRef, accessor.Stride);
+            }
+        }
 
         /// <summary>
         /// Use a ResourceContext per GraphicsDevice (DeviceContext)
@@ -147,4 +243,21 @@ namespace SiliconStudio.Xenko.Particles.VertexLayouts
 
     }
 
+    public struct AttributeAccessor
+    {
+        public int Offset;
+        public int Stride;
+    }
+
+    public struct AttributeDescription
+    {
+        private readonly int hashCode;
+        public override int GetHashCode() => hashCode;
+
+        public AttributeDescription(string name)
+        {
+            hashCode = name.GetHashCode();
+        }
+
+    }
 }
