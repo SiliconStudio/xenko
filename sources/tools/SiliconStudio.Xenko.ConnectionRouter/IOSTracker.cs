1deae79d97760a9b9c268b35d629d2c18dba6207
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-12-18 18:34:06 +0900

    [ConnectionRouter] Added iproxy as attached child process

--- sources/tools/SiliconStudio.Xenko.ConnectionRouter/IOSTracker.cs
+++ sources/tools/SiliconStudio.Xenko.ConnectionRouter/IOSTracker.cs
@@ -78,6 +78,7 @@ namespace SiliconStudio.Xenko.ConnectionRouter
                     }
             };
             process.Start();
+            new AttachedChildProcessJob(process);
 
             Log.Info("Device connected: {0}; successfully mapped port {1}:{2}", device.Name, testedLocalPort, RouterClient.DefaultListenPort);
 
