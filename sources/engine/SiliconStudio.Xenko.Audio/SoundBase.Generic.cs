e1348ea216820f5b9ac9286886194a2565d7edbf
Manu <manu-silicon@users.noreply.github.com>
2016-01-05 13:02:48 +0900

    [Audio] Enable SDL exclusive implementation for Audio
    To compile the code to only include the SDL support for Audio, one has
    to now define SILICONSTUDIO_XENKO_SOUND_SDL (this is not automatically
    done for now to not break exisitng build, especially Windows builds).
    Add SDL specific implementation for SoundEffectInstance and
    DynamicSoundEffectInstance.

--- sources/engine/SiliconStudio.Xenko.Audio/SoundBase.Generic.cs
+++ sources/engine/SiliconStudio.Xenko.Audio/SoundBase.Generic.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-#if !SILICONSTUDIO_PLATFORM_WINDOWS
+#if !SILICONSTUDIO_PLATFORM_WINDOWS || SILICONSTUDIO_XENKO_SOUND_SDL
+
+using System;
+
 namespace SiliconStudio.Xenko.Audio
 {
     public partial class SoundBase
--- sources/engine/SiliconStudio.Xenko.Audio/SoundBase.Windows.cs
+++ sources/engine/SiliconStudio.Xenko.Audio/SoundBase.Windows.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-#if SILICONSTUDIO_PLATFORM_WINDOWS
+#if SILICONSTUDIO_PLATFORM_WINDOWS && !SILICONSTUDIO_XENKO_SOUND_SDL
 
 using System;
 using SharpDX.XAudio2;
