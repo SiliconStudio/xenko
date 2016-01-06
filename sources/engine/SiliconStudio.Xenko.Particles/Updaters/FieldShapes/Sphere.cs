0699598f6fd93186be586e872f7adf2398c174fd
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-01-06 14:32:09 +0900

    [Particles] Sphere, Box and Cylinder added as bounding shapes for particles' force fields.

--- sources/engine/SiliconStudio.Xenko.Particles/Updaters/FieldShapes/Sphere.cs
+++ sources/engine/SiliconStudio.Xenko.Particles/Updaters/FieldShapes/Sphere.cs
@@ -8,15 +8,15 @@ using SiliconStudio.Xenko.Particles.DebugDraw;
 
 namespace SiliconStudio.Xenko.Particles.Updaters.FieldShapes
 {
-    [DataContract("FieldShapeCube")]
-    public class Cube : FieldShape
+    [DataContract("FieldShapeSphere")]
+    public class Sphere : FieldShape
     {
         public override DebugDrawShape GetDebugDrawShape(out Vector3 pos, out Quaternion rot, out Vector3 scl)
         {
             pos = new Vector3(0, 0, 0);
             rot = new Quaternion(0, 0, 0, 1);
-            scl = new Vector3(halfSideX * 2, halfSideY * 2, halfSideZ * 2);
-            return DebugDrawShape.Cube;
+            scl = new Vector3(radius * 2, radius * 2, radius * 2);
+            return DebugDrawShape.Sphere;
         }
 
         [DataMemberIgnore]
@@ -33,40 +33,17 @@ namespace SiliconStudio.Xenko.Particles.Updaters.FieldShapes
 
         [DataMemberIgnore]
         private Vector3 mainAxis;
-
-
+        
         /// <summary>
-        /// The maximum distance from the origin along the X axis. The X side is twice as big.
+        /// The maximum distance from the central point.
         /// </summary>
         /// <userdoc>
-        /// The maximum distance from the origin along the X axis. The X side is twice as big.
-        /// </userdoc>
-        [DataMember(10)]
-        [Display("Half X")]
-        public float HalfSideX { get {return halfSideX; } set { halfSideX = (value > MathUtil.ZeroTolerance) ? value : MathUtil.ZeroTolerance; } }
-        private float halfSideX = 1f;
-
-        /// <summary>
-        /// The maximum distance from the origin along the Y axis. The Y side is twice as big.
-        /// </summary>
-        /// <userdoc>
-        /// The maximum distance from the origin along the Y axis. The Y side is twice as big.
+        /// The maximum distance from the central point.
         /// </userdoc>
         [DataMember(20)]
-        [Display("Half Y")]
-        public float HalfSideY { get { return halfSideY; } set { halfSideY = (value > MathUtil.ZeroTolerance) ? value : MathUtil.ZeroTolerance; } }
-        private float halfSideY = 1f;
-
-        /// <summary>
-        /// The maximum distance from the origin along the Z axis. The Z side is twice as big.
-        /// </summary>
-        /// <userdoc>
-        /// The maximum distance from the origin along the Z axis. The Z side is twice as big.
-        /// </userdoc>
-        [DataMember(30)]
-        [Display("Half Z")]
-        public float HalfSideZ { get { return halfSideZ; } set { halfSideZ = (value > MathUtil.ZeroTolerance) ? value : MathUtil.ZeroTolerance; } }
-        private float halfSideZ = 1f;
+        [Display("Radius")]
+        public float Radius { get { return radius; } set { radius = (value > MathUtil.ZeroTolerance) ? value : MathUtil.ZeroTolerance; } }
+        private float radius = 1f;
 
         public override void PreUpdateField(Vector3 fieldPosition, Quaternion fieldRotation, Vector3 fieldSize)
         {
@@ -77,11 +54,11 @@ namespace SiliconStudio.Xenko.Particles.Updaters.FieldShapes
 
             mainAxis = new Vector3(0, 1, 0);
             fieldRotation.Rotate(ref mainAxis);
-    }
+        }
 
-    public override float GetDistanceToCenter(
-            Vector3 particlePosition, Vector3 particleVelocity,
-            out Vector3 alongAxis, out Vector3 aroundAxis, out Vector3 awayAxis)
+        public override float GetDistanceToCenter(
+                Vector3 particlePosition, Vector3 particleVelocity,
+                out Vector3 alongAxis, out Vector3 aroundAxis, out Vector3 awayAxis)
         {
             // Along - following the main axis
             alongAxis = mainAxis;
@@ -98,10 +75,9 @@ namespace SiliconStudio.Xenko.Particles.Updaters.FieldShapes
             inverseRotation.Rotate(ref particlePosition);
             particlePosition /= fieldSize;
 
-            // Start of code for Cube
-            var maxDist = Math.Max(Math.Abs(particlePosition.X) / halfSideX, Math.Abs(particlePosition.Y) / halfSideY);
-            maxDist = Math.Max(maxDist, Math.Abs(particlePosition.Z) / halfSideZ);
-            // End of code for Cube
+            // Start of code for Sphere
+            var maxDist = particlePosition.Length() / radius;
+            // End of code for Sphere
 
             return maxDist;
         }
