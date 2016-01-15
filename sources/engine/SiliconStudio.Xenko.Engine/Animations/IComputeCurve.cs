cd885f5ca93216e6acce777ca4b5c2d4f7a28a5a
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-01-15 14:48:12 +0900

    [IComputeCurve] Initial concept with constant and binary operator (add, subtract) curves implemented

---
+++ sources/engine/SiliconStudio.Xenko.Engine/Animations/IComputeCurve.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core.Annotations;
+
+namespace SiliconStudio.Xenko.Animations
+{
+    /// <summary>
+    /// Base interface for curve based compute value nodes.
+    /// </summary>
+    [InlineProperty]
+    public interface IComputeCurve<out T> where T : struct
+    {
+        /// <summary>
+        /// Samples the compute curve at the specified location, usually in the [0 .. 1] range
+        /// </summary>
+        /// <param name="location">Location to sample at</param>
+        /// <returns>Sampled value</returns>
+        T SampleAt(float location);
+    }
+}
