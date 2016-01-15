fddc19b27c02a526f4cc32282e4dc46577831fd4
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2016-01-15 10:52:17 +0900

    [Engine] Cleanup code for EntityComponentAttributes. Fix issue in EntityComponentAttributes not getting the AllowMultipleComponent correctly.

--- sources/engine/SiliconStudio.Xenko.Engine/Engine/EntityComponent.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/EntityComponent.cs
@@ -2,66 +2,15 @@
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Serialization;
 
 namespace SiliconStudio.Xenko.Engine
 {
     /// <summary>
-    /// Allows a component of the same type to be added multiple time to the same entity (default is <c>false</c>)
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public class AllowMultipleComponentAttribute : Attribute
-    {
-    }
-
-    public struct EntityComponentAttributes
-    {
-        private static readonly Dictionary<Type, EntityComponentAttributes> ComponentAttributes = new Dictionary<Type, EntityComponentAttributes>();
-        public EntityComponentAttributes(bool allowMultipleComponent)
-        {
-            AllowMultipleComponent = allowMultipleComponent;
-        }
-
-        public readonly bool AllowMultipleComponent ;
-
-        public static EntityComponentAttributes Get<T>() where T : EntityComponent
-        {
-            return GetInternal(typeof(T));
-        }
-
-        public static EntityComponentAttributes Get(Type type)
-        {
-            if (type == null) throw new ArgumentNullException(nameof(type));
-            if (!typeof(EntityComponent).IsAssignableFrom(type)) throw new ArgumentException("The type must be of EntityComponent", "type");
-            return GetInternal(type);
-        }
-
-        private static EntityComponentAttributes GetInternal(Type type)
-        {
-            lock (ComponentAttributes)
-            {
-                EntityComponentAttributes attributes;
-                if (!ComponentAttributes.TryGetValue(type, out attributes))
-                {
-                    attributes = new EntityComponentAttributes(type.GetTypeInfo().GetCustomAttribute<AllowMultipleComponentAttribute>() != null);
-                    ComponentAttributes.Add(type, attributes);
-                    return attributes;
-                }
-            }
-            return new EntityComponentAttributes();
-        }
-    }
-
-    /// <summary>
     /// Base class for <see cref="Entity"/> components.
     /// </summary>
-    [DataSerializer(typeof(EntityComponent.Serializer))]
+    [DataSerializer(typeof(Serializer))]
     [DataContract]
     public abstract class EntityComponent
     {
