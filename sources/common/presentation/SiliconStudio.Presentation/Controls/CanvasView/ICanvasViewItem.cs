2a777f880ec3f02f27fbe8bafda240553e8ddab2
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-01-21 18:07:04 +0900

    [Presentation] WIP defining items for the Canvas View (XK-2694).

---
+++ sources/common/presentation/SiliconStudio.Presentation/Controls/CanvasView/ICanvasViewItem.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+namespace SiliconStudio.Presentation.Controls
+{
+    public interface ICanvasViewItem
+    {
+        void Attach(CanvasView view);
+
+        void Detach(CanvasView view);
+
+        void Render(CanvasRenderer renderer);
+    }
+}
