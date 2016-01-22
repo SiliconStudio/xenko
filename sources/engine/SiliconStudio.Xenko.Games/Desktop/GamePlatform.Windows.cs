5b8c86caacf8910cb3a4d7800dc0927ff11d4703
Manu <emmanuel.stapf@siliconstudio.co.jp>
2016-01-22 16:02:00 +0900

    [Games] Only initialize COM on Windows

--- sources/engine/SiliconStudio.Xenko.Games/Desktop/GamePlatform.Windows.cs
+++ sources/engine/SiliconStudio.Xenko.Games/Desktop/GamePlatform.Windows.cs
@@ -32,7 +32,7 @@ namespace SiliconStudio.Xenko.Games
         public GamePlatformWindows(GameBase game) : base(game)
         {
             IsBlockingRun = true;
-#if SILICONSTUDIO_RUNTIME_CORECLR
+#if SILICONSTUDIO_PLATFORM_WINDOWS_DESKTOP && SILICONSTUDIO_RUNTIME_CORECLR
                 // This is required by the Audio subsystem of SharpDX.
             Win32Native.CoInitialize(IntPtr.Zero);
 #endif
