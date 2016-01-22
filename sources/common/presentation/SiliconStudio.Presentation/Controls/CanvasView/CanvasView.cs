30a9c9c8314229dab4bf9f84dc948ae24a531604
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-01-22 17:34:56 +0900

    [Presentation] CanvasView now handles only one model at a time. Removed debug test code. Also added a simple line primitive in CanvasRenderer. (XK-2694)

--- sources/common/presentation/SiliconStudio.Presentation/Controls/CanvasView/CanvasView.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Controls/CanvasView/CanvasView.cs
@@ -1,22 +1,38 @@
 ﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-#define DEBUG_CanvasView
+#region Copyright and license
+// Some parts of this file were inspired by OxyPlot (https://github.com/oxyplot/oxyplot)
+/*
+The MIT license (MTI)
+https://opensource.org/licenses/MIT
+
+Copyright (c) 2014 OxyPlot contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion
 
 using System;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-
-#if DEBUG_CanvasView
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using System.Windows.Media;
 using System.Windows.Threading;
-#endif
 
 namespace SiliconStudio.Presentation.Controls
 {
@@ -29,16 +45,11 @@ namespace SiliconStudio.Presentation.Controls
         private const string CanvasPartName = "PART_Canvas";
         
         /// <summary>
-        /// Identifies the <see cref="Items"/> dependency property.
+        /// Identifies the <see cref="Model"/> dependency property.
         /// </summary>
-        public static readonly DependencyProperty ItemsProperty =
-            DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<ICanvasViewItem>), typeof(CanvasView), new PropertyMetadata(null, OnItemsPropertyChanged));
+        public static readonly DependencyProperty ModelProperty =
+            DependencyProperty.Register(nameof(Model), typeof(ICanvasViewItem), typeof(CanvasView), new PropertyMetadata(null, OnModelPropertyChanged));
 
-
-        /// <summary>
-        /// The canvas.
-        /// </summary>
-        private Canvas canvas;
         /// <summary>
         /// The renderer.
         /// </summary>
@@ -49,176 +60,61 @@ namespace SiliconStudio.Presentation.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CanvasView), new FrameworkPropertyMetadata(typeof(CanvasView)));
         }
 
-        public ObservableCollection<ICanvasViewItem> Items
+        public ICanvasViewItem Model
         {
-            get { return (ObservableCollection<ICanvasViewItem>)GetValue(ItemsProperty); }
-            set { SetValue(ItemsProperty, value); }
+            get { return (ICanvasViewItem)GetValue(ModelProperty); }
+            set { SetValue(ModelProperty, value); }
         }
 
-        private static void OnItemsPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        private static void OnModelPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
             var view = (CanvasView)sender;
 
-            var items = e.OldValue as ObservableCollection<ICanvasViewItem>;
-            if (items != null)
-            {
-                items.CollectionChanged -= view.ItemsCollectionChanged;
-                view.DetachItems(items);
-            }
+            var model = e.OldValue as ICanvasViewItem;
+            model?.Detach(view);
 
-            items = e.NewValue as ObservableCollection<ICanvasViewItem>;
-            if (items != null)
-            {
-                items.CollectionChanged += view.ItemsCollectionChanged;
-                view.AttachItems(items);
-            }
-        }
+            model = e.NewValue as ICanvasViewItem;
+            model?.Attach(view);
 
-        private void AttachItems(IEnumerable<ICanvasViewItem> items)
-        {
-            foreach (var item in items)
-            {
-                item.Attach(this);
-            }
-        }
-
-        private void DetachItems(IEnumerable<ICanvasViewItem> items)
-        {
-            foreach (var item in items)
-            {
-                item.Detach(this);
-            }
-        }
-
-        private void ItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            switch (e.Action)
-            {
-                case NotifyCollectionChangedAction.Move:
-                    return;
-
-                case NotifyCollectionChangedAction.Reset:
-                    var items = sender as ObservableCollection<ICanvasViewItem>;
-                    if (items != null)
-                    {
-                        DetachItems(items);
-                        AttachItems(items);
-                    }
-                    return;
-
-                case NotifyCollectionChangedAction.Add:
-                case NotifyCollectionChangedAction.Remove:
-                case NotifyCollectionChangedAction.Replace:
-                    if (e.OldItems != null)
-                    {
-                        DetachItems(e.OldItems.Cast<ICanvasViewItem>());
-                    }
-            
-                    if (e.NewItems != null)
-                    {
-                        AttachItems(e.NewItems.Cast<ICanvasViewItem>());
-                    }
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            view.InvalidateCanvas();
         }
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
 
-            this.canvas = GetTemplateChild(CanvasPartName) as Canvas;
+            var canvas = GetTemplateChild(CanvasPartName) as Canvas;
             if (canvas == null)
                 throw new InvalidOperationException($"A part named '{CanvasPartName}' must be present in the ControlTemplate, and must be of type '{typeof(Canvas).FullName}'.");
 
-            this.canvas.UpdateLayout();
-            this.renderer = new CanvasRenderer(this.canvas);
+            canvas.UpdateLayout();
+            this.renderer = new CanvasRenderer(canvas);
+
+            InvalidateCanvas();
         }
 
-#if DEBUG && DEBUG_CanvasView
-        private const int UpdateInterval = 1000/60;
-
-        private readonly Timer timer;
-        private readonly Stopwatch watch = new Stopwatch();
-        private readonly Func<double, double, double, double> function;
-
-        private readonly object syncRoot = new object();
-
-        private IList<Point>[] pointLists;
-
-        private readonly Color[] colors =
+        public void InvalidateCanvas()
         {
-            Colors.Aqua, Colors.Bisque, Colors.Brown, Colors.Green, Colors.HotPink, Colors.Khaki, Colors.Maroon, Colors.Navy, Colors.Orange,
-        };
+            if (renderer == null || Model == null)
+                return;
 
-        public CanvasView()
-        {
-            this.timer = new Timer(OnTimerElapsed);
-            this.function = (t, x, a) => Math.Cos(t*a)*(x == 0 ? 1 : Math.Sin(x*a)/x);
-
-            Loaded += OnLoaded;
-        }
-
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            this.timer.Change(Timeout.Infinite, Timeout.Infinite);
-            pointLists = new IList<Point>[20];
-            for (var i = 0; i < pointLists.Length; ++i)
+            BeginInvoke(() =>
             {
-                pointLists[i] = new List<Point>();
-            }
-            this.watch.Start();
-            this.timer.Change(1000, UpdateInterval);
-        }
-
-        private void OnTimerElapsed(object state)
-        {
-            lock (this.syncRoot)
-            {
-                var t = this.watch.ElapsedMilliseconds*0.001;
-                for (var i = 0; i < pointLists.Length; ++i)
-                {
-                    var points = pointLists[i];
-                    var a = 0.5 + i*0.05;
-                    points.Clear();
-                    for (double x = -5; x <= 5; x += 0.1)
-                    {
-                        points.Add(new Point(100*x + 500, 314*function(t, x, a) + 250));
-                    }
-                }
-            }
-
-            BeginInvoke(Render);
-        }
-
-        private void Render()
-        {
-            lock (this.syncRoot)
-            {
-                if (renderer == null)
-                    return;
                 renderer.Clear();
-                for (var i = 0; i < pointLists.Length; ++i)
-                {
-                    //renderer.DrawPolygon(pointLists[i], colors[i%colors.Length], colors[(i+2) % colors.Length], 1, PenLineJoin.Bevel);
-                    renderer.DrawPolyline(pointLists[i], colors[i%colors.Length], 1, PenLineJoin.Bevel);
-                }
-            }
+                Model.Render(renderer);
+            });
         }
 
         private void BeginInvoke(Action action)
         {
-            if (!this.Dispatcher.CheckAccess())
+            if (!Dispatcher.CheckAccess())
             {
-                this.Dispatcher.BeginInvoke(DispatcherPriority.Background, action);
+                Dispatcher.InvokeAsync(action, DispatcherPriority.Background);
             }
             else
             {
                 action();
             }
         }
-#endif
     }
 }
