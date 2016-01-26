26308f22c8de50ff5c92b0d62efb322f1ea052ec
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-01-26 15:57:06 +0900

    [Presentation] OnMouseBehavior uses Point instead of Vector2.

---
+++ sources/common/presentation/SiliconStudio.Presentation/Extensions/MathExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using SiliconStudio.Core.Mathematics;
+
+namespace SiliconStudio.Presentation.Extensions
+{
+    using WindowsPoint = System.Windows.Point;
+
+    public static class MathExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="WindowsPoint"/> to a <see cref="Vector2"/>.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2 ToVector2(this WindowsPoint point)
+        {
+            return new Vector2((float)point.X, (float)point.Y);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="Vector2"/> to a <see cref="WindowsPoint"/>.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static WindowsPoint ToWindowsPoint(this Vector2 vector)
+        {
+            return new WindowsPoint(vector.X, vector.Y);
+        }
+    }
+}
