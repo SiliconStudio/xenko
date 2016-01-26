f97d6e2b766f5045e9becfd17d4ddc32ca7487f8
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-01-26 17:10:37 +0900

    [Particles] Some basic graphics tests are working on Android (WIP)

--- sources/engine/SiliconStudio.Xenko.Particles.Tests/GameTest.cs
+++ sources/engine/SiliconStudio.Xenko.Particles.Tests/GameTest.cs
@@ -27,43 +27,6 @@ namespace SiliconStudio.Xenko.Particles.Tests
     /// </summary>
     class GameTest : GameTestBase
     {
-        private SpriteFont font;
-
-        private SpriteBatch batch;
-        private Vector3 currentAcceleration;
-        private float currentHeading;
-        private Vector3 currentRotationRate;
-        private Vector3 currentUserAcceleration;
-        private Vector3 currentGravity;
-        private Vector3 currentYawPitchRoww;
-
-        private enum DebugScenes
-        {
-            Orientation,
-            UserAccel,
-            Gravity,
-            RawAccel,
-            Gyroscope,
-            Compass
-        }
-        private Dictionary<DebugScenes, Color> sceneToColor = new Dictionary<DebugScenes, Color>
-        {
-            { DebugScenes.UserAccel, Color.Blue},
-            { DebugScenes.Gravity, Color.Green },
-            { DebugScenes.RawAccel, Color.Yellow },
-            { DebugScenes.Compass, Color.Red}
-        };
-        private DebugScenes currentScene;
-
-        private TextBlock currentText;
-        private Entity entity;
-        private Entity entity2;
-        private Entity entity3;
-        private SpriteComponent spriteComponent;
-//        private ModelComponent modelComponent;
-//        private ModelComponent modelComponent2;
-//        private ModelComponent modelComponent3;
-//        private Model teapot;
 
         public GameTest()
         {
@@ -76,32 +39,11 @@ namespace SiliconStudio.Xenko.Particles.Tests
         {
             await base.LoadContent();
 
-            font = Asset.Load<SpriteFont>("Font");
+            // TODO My custom code here
+//            font = Asset.Load<SpriteFont>("Font");
 //            teapot = Asset.Load<Model>("Teapot");
-            batch = new SpriteBatch(GraphicsDevice);
-
-            foreach (var entity1 in SceneSystem.SceneInstance.Scene.Entities)
-            {
-                foreach (var component in entity1.Components)
-                {
-                    if (component is ParticleSystemComponent)
-                    {
-                        System.Console.WriteLine("PArticle System found!");
-                    }
-                }
-            }
-
-//            spriteComponent = new SpriteComponent { SpriteProvider = new SpriteFromSheet { Sheet = Asset.Load<SpriteSheet>("SpriteSheet") } };
-//            modelComponent = new ModelComponent { Model = teapot };
-//            modelComponent2 = new ModelComponent { Model = teapot };
-//            modelComponent3 = new ModelComponent { Model = teapot };
-//            entity = new Entity { spriteComponent, modelComponent };
-//            entity2 = new Entity { modelComponent2 };
-//            entity3 = new Entity { modelComponent3 };
-//            SceneSystem.SceneInstance.Scene.Entities.Add(entity);
-//            SceneSystem.SceneInstance.Scene.Entities.Add(entity2);
-//            SceneSystem.SceneInstance.Scene.Entities.Add(entity3);
-            
+//            batch = new SpriteBatch(GraphicsDevice);
+        
         }
 
 
@@ -114,15 +56,12 @@ namespace SiliconStudio.Xenko.Particles.Tests
         {
             base.Draw(gameTime);
 
+            // TODO My custom code here
+
             // update the values only once every x frames in order to be able to read them.
             if ((gameTime.FrameCount % 20) == 0)
             {
-                currentAcceleration = Input.Accelerometer.Acceleration;
-                currentHeading = Input.Compass.Heading;
-                currentRotationRate = Input.Gyroscope.RotationRate;
-                currentUserAcceleration = Input.UserAcceleration.Acceleration;
-                currentGravity = Input.Gravity.Vector;
-                currentYawPitchRoww = new Vector3(Input.Orientation.Yaw, Input.Orientation.Pitch, Input.Orientation.Roll);
+                // TODO Do something based on frame count? Maybe take a screenshot
             }
 
           //  GraphicsDevice.Clear(GraphicsDevice.DepthStencilBuffer, DepthStencilClearOptions.DepthBuffer);
