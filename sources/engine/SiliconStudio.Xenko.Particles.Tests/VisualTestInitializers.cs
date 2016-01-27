d5333fa4b149efa19d42cc201898fda8b75eaa4d
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-01-27 17:30:48 +0900

    [Particles] Visual tests updated to test _diff images

--- sources/engine/SiliconStudio.Xenko.Particles.Tests/VisualTestInitializers.cs
+++ sources/engine/SiliconStudio.Xenko.Particles.Tests/VisualTestInitializers.cs
@@ -1,26 +1,13 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using System.Threading.Tasks;
 using NUnit.Framework;
-using SiliconStudio.Core;
-using SiliconStudio.Core.Serialization.Assets;
-using SiliconStudio.Xenko.Engine;
 
 namespace SiliconStudio.Xenko.Particles.Tests
 {
     class VisualTestInitializers : GameTest
     {
-        public VisualTestInitializers() : base("VisualTestInitializers") { }
-
-        protected override async Task LoadContent()
-        {
-            await base.LoadContent();
-
-            var assetManager = Services.GetSafeServiceAs<AssetManager>();
-
-            SceneSystem.SceneInstance = new SceneInstance(Services, assetManager.Load<Scene>("VisualTestInitializers"));
-        }
+        public VisualTestInitializers() : base("VisualTestInitializers") { }      
 
         [Test]
         public void RunVisualTests()
