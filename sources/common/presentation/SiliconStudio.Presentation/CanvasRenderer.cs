b2959c02e5beb1de2e39680231a888839347003c
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-02-10 15:35:46 +0900

    [Presentation] Added extensions for CanvasRenderer.

--- sources/common/presentation/SiliconStudio.Presentation/CanvasRenderer.cs
+++ sources/common/presentation/SiliconStudio.Presentation/CanvasRenderer.cs
@@ -61,24 +61,6 @@ namespace SiliconStudio.Presentation
         }
 
         /// <summary>
-        /// Draws a circle in the canvas.
-        /// </summary>
-        /// <param name="point"></param>
-        /// <param name="radius">The radius of the circle.</param>
-        /// <param name="fillColor">The color of the shape's interior.</param>
-        /// <param name="strokeColor">The color of the shape's outline.</param>
-        /// <param name="thickness">The wifdth of the shape's outline.</param>
-        /// <param name="lineJoin">The type of join that is used at the vertices of the shape.</param>
-        /// <param name="dashArray">The pattern of dashes and gaps that is used to outline the shape.</param>
-        /// <param name="dashOffset">The distance within the dash pattern where a dash begins.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void DrawCircle(Point point, double radius, Color fillColor, Color strokeColor,
-            double thickness = 1.0, PenLineJoin lineJoin = PenLineJoin.Miter, ICollection<double> dashArray = null, double dashOffset = 0)
-        {
-            DrawEllipse(point, new Size(radius, radius), fillColor, strokeColor, thickness, lineJoin, dashArray, dashOffset);
-        }
-
-        /// <summary>
         /// Draws an ellipse in the canvas.
         /// </summary>
         /// <param name="point"></param>
---
+++ sources/common/presentation/SiliconStudio.Presentation/Extensions/CanvasRendererExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SiliconStudio.Presentation.Extensions
+{
+    public static class CanvasRendererExtensions
+    {
+        /// <summary>
+        /// Draws a circle in the canvas.
+        /// </summary>
+        /// <param name="renderer">The renderer.</param>
+        /// <param name="point"></param>
+        /// <param name="radius">The radius of the circle.</param>
+        /// <param name="fillColor">The color of the shape's interior.</param>
+        /// <param name="strokeColor">The color of the shape's outline.</param>
+        /// <param name="thickness">The wifdth of the shape's outline.</param>
+        /// <param name="lineJoin">The type of join that is used at the vertices of the shape.</param>
+        /// <param name="dashArray">The pattern of dashes and gaps that is used to outline the shape.</param>
+        /// <param name="dashOffset">The distance within the dash pattern where a dash begins.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DrawCircle(this CanvasRenderer renderer, Point point, double radius, Color fillColor, Color strokeColor,
+            double thickness = 1.0, PenLineJoin lineJoin = PenLineJoin.Miter, ICollection<double> dashArray = null, double dashOffset = 0)
+        {
+            renderer.DrawEllipse(point, new Size(radius, radius), fillColor, strokeColor, thickness, lineJoin, dashArray, dashOffset);
+        }
+    }
+}
