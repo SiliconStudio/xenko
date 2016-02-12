1985f57d7e5ea915472eabb8e58644f7e34931ee
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-02-12 19:39:06 +0900

    [Xenko.Assets] Fixed OpenGL detection during asset build time (fixed compilation.. removed debugger :( )

--- sources/engine/SiliconStudio.Xenko.Shaders.Compiler/EffectCompiler.cs
+++ sources/engine/SiliconStudio.Xenko.Shaders.Compiler/EffectCompiler.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -184,8 +183,6 @@ namespace SiliconStudio.Xenko.Shaders.Compiler
             // -------------------------------------------------------
 
             var bytecode = new EffectBytecode { Reflection = parsingResult.Reflection, HashSources = parsingResult.HashSources };
-
-            Debugger.Launch();
 
             // Select the correct backend compiler
             IShaderCompiler compiler;
