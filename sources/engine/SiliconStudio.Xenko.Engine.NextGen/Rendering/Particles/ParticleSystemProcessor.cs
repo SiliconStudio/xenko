ed0a5c4fb98c25977f213eb926e1cbe71e468bfb
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-02-17 18:32:39 +0900

    [GraphicsRefactor] Particle renderer WIP

---
+++ sources/engine/SiliconStudio.Xenko.Engine.NextGen/Rendering/Particles/ParticleSystemProcessor.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using SiliconStudio.Core;
+using SiliconStudio.Xenko.Engine;
+using SiliconStudio.Xenko.Particles.Components;
+using SiliconStudio.Xenko.Rendering;
+
+namespace SiliconStudio.Xenko.Rendering.Particles
+{
+    /// <summary>
+    /// The processor in charge of updating and drawing the entities having sprite components.
+    /// </summary>
+    internal class NextGenSpriteProcessor : EntityProcessor<ParticleSystemComponent>
+    {
+        private NextGenRenderSystem renderSystem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NextGenSpriteProcessor"/> class.
+        /// </summary>
+        public NextGenSpriteProcessor()
+            : base(typeof(TransformComponent))
+        {
+            ParticleSystems = new List<RenderParticleSystem>();
+        }
+
+        public List<RenderParticleSystem> ParticleSystems { get; private set; }
+
+        protected internal override void OnSystemAdd()
+        {
+            renderSystem = Services.GetSafeServiceAs<NextGenRenderSystem>();
+        }
+
+        public override void Draw(RenderContext gameTime)
+        {
+            ParticleSystems.Clear();
+            foreach (var particleSystemStateKeyPair in ComponentDatas)
+            {
+                if (particleSystemStateKeyPair.Value.ParticleSystemComponent.Enabled)
+                {
+                    ParticleSystems.Add(particleSystemStateKeyPair.Value);
+                }
+            }
+        }
+
+        protected override void OnEntityComponentAdding(Entity entity, ParticleSystemComponent spriteComponent, RenderParticleSystem data)
+        {
+            foreach (var particleEmitter in spriteComponent.ParticleSystem.Emitters)
+            {
+                
+            }
+            renderSystem.RenderObjects.Add(data);
+        }
+
+        protected override void OnEntityComponentRemoved(Entity entity, ParticleSystemComponent spriteComponent, RenderParticleSystem data)
+        {
+            renderSystem.RenderObjects.Remove(data);
+        }
+
+        protected override RenderParticleSystem GenerateComponentData(Entity entity, ParticleSystemComponent particleSystemComponent)
+        {
+            return new RenderParticleSystem
+            {
+                ParticleSystemComponent = particleSystemComponent,
+                TransformComponent = entity.Transform,
+            };
+        }
+
+        protected override bool IsAssociatedDataValid(Entity entity, ParticleSystemComponent spriteComponent, RenderParticleSystem associatedData)
+        {
+            return
+                spriteComponent == associatedData.ParticleSystemComponent &&
+                entity.Transform == associatedData.TransformComponent;
+        }
+    }
+}
---
+++ sources/engine/SiliconStudio.Xenko.Engine.NextGen/Rendering/Particles/ParticleSystemRenderFeature.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Graphics;
+
+namespace SiliconStudio.Xenko.Rendering.Particles
+{
+    public class ParticleSystemRenderFeature : RootEffectRenderFeature
+    {
+        public override bool SupportsRenderObject(RenderObject renderObject)
+        {
+            return renderObject is RenderParticleSystem;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Extract()
+        {
+            base.Extract();
+        }
+
+        public override void Prepare(RenderContext context)
+        {
+            base.Prepare(context);
+        }
+
+        public override void PrepareEffectPermutations()
+        {
+            base.PrepareEffectPermutations();
+        }
+
+        public override void Draw(RenderDrawContext context, RenderView renderView, RenderViewStage renderViewStage, int startIndex, int endIndex)
+        {
+            base.Draw(context, renderView, renderViewStage, startIndex, endIndex);
+
+            Matrix viewInverse;
+            Matrix.Invert(ref renderView.View, out viewInverse);
+
+            for (var index = startIndex; index <= endIndex; index++)
+            {
+                var renderNodeReference = renderViewStage.RenderNodes[index].RenderNode;
+                var renderNode = GetRenderNode(renderNodeReference);
+
+                var renderParticleSystem = (RenderParticleSystem)renderNode.RenderObject;
+
+                // Get effect
+                var renderEffect = renderNode.RenderEffect;
+
+                // TODO GRAPHICS REFACTOR: Extract data
+                var particleSystemComponent = renderParticleSystem.ParticleSystemComponent;
+                var particleSystem = particleSystemComponent.ParticleSystem;
+
+                particleSystemComponent.ParticleSystem.Draw(context, ref renderView.View, ref renderView.Projection, ref viewInverse, particleSystemComponent.Color);
+            }
+        }
+        
+    }
+}
