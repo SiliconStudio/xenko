33f5db6b4362887da704dc52d63f25d7e4e2877d
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-02-19 15:40:10 +0900

    [Engine] Events WIP

---
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/Events/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+namespace SiliconStudio.Xenko.Engine.Events
+{
+    public class Class1
+    {
+         
+    }
+}
