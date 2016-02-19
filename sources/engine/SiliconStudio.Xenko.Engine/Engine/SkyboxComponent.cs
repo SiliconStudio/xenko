bf7c26b7ce854f030e48480fde1fad458433a66e
Virgile Bello <virgile@siliconstudio.co.jp>
2016-02-19 16:38:13 +0900

    [GraphicsRefactor] Moved files from Engine.NextGen to Engine

--- sources/engine/SiliconStudio.Xenko.Engine/Engine/SkyboxComponent.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/SkyboxComponent.cs
@@ -14,8 +14,8 @@ namespace SiliconStudio.Xenko.Engine
     /// </summary>
     [DataContract("SkyboxComponent")]
     [Display("Skybox", Expand = ExpandRule.Once)]  // More important than lights, as usually the Skybox is associated with a light
-    [DefaultEntityComponentRenderer(typeof(SkyboxComponentRenderer), -100)]
-    [DefaultEntityComponentProcessor(typeof(SkyboxProcessor))]
+    //[DefaultEntityComponentRenderer(typeof(SkyboxComponentRenderer), -100)]
+    //[DefaultEntityComponentProcessor(typeof(SkyboxProcessor))]
     [ComponentOrder(11500)]
     public sealed class SkyboxComponent : ActivableEntityComponent
     {
