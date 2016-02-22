8cc0f48c3cba515052ffbe9cf173f3dfb3f7dbf6
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-02-22 11:10:58 +0900

    Added Dataflow package

---
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/Events/EventKey.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace SiliconStudio.Xenko.Engine.Events
+{
+    public class EventKey<T>
+    {
+        private readonly BroadcastBlock<T> broadcastBlock = new BroadcastBlock<T>(null);
+
+        internal IDisposable Connect(ITargetBlock<T> target)
+        {
+            return broadcastBlock.LinkTo(target);
+        }
+
+        public void Broadcast(T data)
+        {
+            broadcastBlock.Post(data);
+        }
+    }
+}
---
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/Events/EventReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace SiliconStudio.Xenko.Engine.Events
+{
+    public class EventReceiver<T>
+    {
+        private readonly BufferBlock<T> block;
+
+        public EventReceiver(EventKey<T> key, bool buffered = false)
+        {
+            if (buffered)
+            {
+                block = new BufferBlock<T>(new DataflowBlockOptions
+                {
+                    BoundedCapacity = 1
+                });
+            }
+            else
+            {
+                block = new BufferBlock<T>();
+            }
+
+            key.Connect(block);
+        }
+
+        public async Task<T> Receive()
+        {
+            return await block.ReceiveAsync();
+        }
+    }
+}
