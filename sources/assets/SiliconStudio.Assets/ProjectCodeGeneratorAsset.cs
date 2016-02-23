9ef97d7370c51ece8214883d39221f559e88427f
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-02-23 20:09:28 +0900

    [Xenko.Assets] Made effects work properly as asset, only renaming case is missing

---
+++ sources/assets/SiliconStudio.Assets/ProjectCodeGeneratorAsset.cs
@@ -0,0 +1,14 @@
+﻿using SiliconStudio.Core;
+
+namespace SiliconStudio.Assets
+{
+    [DataContract("ProjectCodeGeneratorAsset")]
+    public abstract class ProjectCodeGeneratorAsset : ProjectSourceCodeAsset
+    {
+        [Display(Browsable = false)]
+        public abstract string Generator { get; set; }
+
+        [Display(Browsable = false)]
+        public string GeneratedAbsolutePath { get; set; }
+    }
+}
