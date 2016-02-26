9ff4a7c3514c594cd722d7dae5aaedae655face6
Virgile Bello <virgile@siliconstudio.co.jp>
2016-02-26 12:11:45 +0900

    [GraphicsRefactor] Make sure Effect Compiler never uses CompilerParameters if going async

---
+++ sources/engine/SiliconStudio.Xenko.Shaders/Compiler/EffectCompilerParameters.cs
@@ -0,0 +1,17 @@
+﻿using SiliconStudio.Core;
+
+namespace SiliconStudio.Xenko.Shaders.Compiler
+{
+    [DataContract]
+    public struct EffectCompilerParameters
+    {
+        /// <summary>
+        /// Gets or sets the priority (in case this compile is scheduled in a custom async pool)
+        /// </summary>
+        /// <value>
+        /// The priority.
+        /// </value>
+        [DataMemberIgnore]
+        public int TaskPriority { get; set; }
+    }
+}
