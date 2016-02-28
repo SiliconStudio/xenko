514771f84eb68da101e4924a7b1d58ff04766e45
Virgile Bello <virgile@siliconstudio.co.jp>
2016-02-28 12:22:12 +0900

    [GraphicsRefactor] EffectInstance: Separated parameter updating in two new classes: EffectParameterUpdater and EffectParameterUpdaterLayout

---
+++ sources/engine/SiliconStudio.Xenko.Graphics/EffectParameterUpdaterLayout.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using SiliconStudio.Xenko.Graphics;
+using SiliconStudio.Xenko.Shaders;
+
+namespace SiliconStudio.Xenko.Rendering
+{
+    /// <summary>
+    /// Defines a layout used by <see cref="EffectParameterUpdater"/> to update several <see cref="ResourceGroup"/> from a <see cref="NextGenParameterCollection"/>.
+    /// </summary>
+    public class EffectParameterUpdaterLayout
+    {
+        internal ResourceGroupLayout[] ResourceGroupLayouts;
+
+        internal DescriptorSetLayoutBuilder[] Layouts;
+        internal NextGenParameterCollectionLayout ParameterCollectionLayout = new NextGenParameterCollectionLayout();
+
+        public EffectParameterUpdaterLayout(GraphicsDevice graphicsDevice, Effect effect, DescriptorSetLayoutBuilder[] layouts)
+        {
+            Layouts = layouts;
+
+            // Process constant buffers
+            ResourceGroupLayouts = new ResourceGroupLayout[layouts.Length];
+            for (int layoutIndex = 0; layoutIndex < layouts.Length; layoutIndex++)
+            {
+                var layout = layouts[layoutIndex];
+
+                ParameterCollectionLayout.ProcessResources(layout);
+
+                string cbufferName = null;
+
+                for (int entryIndex = 0; entryIndex < layout.Entries.Count; ++entryIndex)
+                {
+                    var layoutEntry = layout.Entries[entryIndex];
+                    if (layoutEntry.Class == EffectParameterClass.ConstantBuffer)
+                    {
+                        var constantBuffer = effect.Bytecode.Reflection.ConstantBuffers.First(x => x.Name == layoutEntry.Key.Name);
+                        ParameterCollectionLayout.ProcessConstantBuffer(constantBuffer);
+
+                        // For now we assume first cbuffer will be the main one
+                        if (cbufferName == null)
+                            cbufferName = layoutEntry.Key.Name;
+                    }
+                }
+
+                ResourceGroupLayouts[layoutIndex] = ResourceGroupLayout.New(graphicsDevice, layout, effect.Bytecode, cbufferName);
+            }
+        }
+    }
+}
