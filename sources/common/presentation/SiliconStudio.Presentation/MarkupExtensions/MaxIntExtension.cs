071aadbf287c5879b51bbdb776ecb48064408648
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-03-01 16:16:13 +0900

    [Presentation] Moved min/max int/double markup extension classes into dedicated files.

--- sources/common/presentation/SiliconStudio.Presentation/MarkupExtensions/MaxIntExtension.cs
+++ sources/common/presentation/SiliconStudio.Presentation/MarkupExtensions/MaxIntExtension.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 using System;
 using System.Windows.Markup;
@@ -6,18 +6,11 @@ using System.Windows.Markup;
 namespace SiliconStudio.Presentation.MarkupExtensions
 {
     [MarkupExtensionReturnType(typeof(int))]
-    public class IntExtension : MarkupExtension
+    public sealed class MaxIntExtension : MarkupExtension
     {
-        public int Value { get; set; }
-
-        public IntExtension(object value)
-        {
-            Value = Convert.ToInt32(value);
-        }
-
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return Value;
+            return int.MaxValue;
         }
     }
 }
