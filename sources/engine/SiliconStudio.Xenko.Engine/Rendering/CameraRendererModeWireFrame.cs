63eb636248dc8918acd065190e02633aa96f98a1
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-03-01 20:13:36 +0900

    [GraphicsRefactor] Split CameraRenderModes

---
+++ sources/engine/SiliconStudio.Xenko.Engine/Rendering/CameraRendererModeWireFrame.cs
@@ -0,0 +1,40 @@
+using SiliconStudio.Core;
+using SiliconStudio.Xenko.Graphics;
+
+namespace SiliconStudio.Xenko.Rendering
+{
+    [DataContract("CameraRendererModeWireFrame")]
+    [Display("WireFrame")]
+    public class CameraRendererModeWireFrame : CameraRenderModeBase
+    {
+        [DataMemberIgnore]
+        public RenderStage WireFrameRenderStage { get; set; }
+
+        protected override void InitializeCore()
+        {
+            base.InitializeCore();
+
+            // Create optional render stages that don't exist yet
+            if (WireFrameRenderStage == null)
+                WireFrameRenderStage = EntityComponentRendererBase.GetOrCreateRenderStage(RenderSystem, "WireFrame", "WireFrame", new RenderOutputDescription(PixelFormat.R32G32B32A32_Float, PixelFormat.D24_UNorm_S8_UInt));
+        }
+
+        public override void BeforeExtract(RenderContext context)
+        {
+            base.BeforeExtract(context);
+
+            if (RenderSystem.GetPipelinePlugin<MeshPipelinePlugin>(false) != null)
+            {
+                // If MeshPipelinePlugin exists and we have wire frame, let's enable WireFrameRenderFeature
+                RenderSystem.GetPipelinePlugin<WireFrameMeshPipelinePlugin>(true);
+            }
+        }
+
+        protected override void DrawCore(RenderDrawContext context)
+        {
+            var renderFrame = context.RenderContext.Tags.Get(RenderFrame.Current);
+            context.CommandList.Clear(renderFrame.DepthStencil, DepthStencilClearOptions.DepthBuffer);
+            RenderSystem.Draw(context, MainRenderView, WireFrameRenderStage);
+        }
+    }
+}
