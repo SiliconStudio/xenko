e31fd8db41ea09f2aa4a232c429ce912b29e661f
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-03-02 12:10:18 +0900

    [Presentation] Added a method to render a batch of ellipses (or circles) in one go.

--- sources/common/presentation/SiliconStudio.Presentation/Extensions/CanvasRendererExtensions.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Extensions/CanvasRendererExtensions.cs
@@ -33,6 +33,26 @@ namespace SiliconStudio.Presentation.Extensions
         /// </summary>
         /// <param name="renderer">The renderer.</param>
         /// <param name="point"></param>
+        /// <param name="diameter">The diameter of the circle.</param>
+        /// <param name="fillColor">The color of the shape's interior.</param>
+        /// <param name="strokeColor">The color of the shape's outline.</param>
+        /// <param name="thickness">The wifdth of the shape's outline.</param>
+        /// <param name="lineJoin">The type of join that is used at the vertices of the shape.</param>
+        /// <param name="dashArray">The pattern of dashes and gaps that is used to outline the shape.</param>
+        /// <param name="dashOffset">The distance within the dash pattern where a dash begins.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DrawCircle(this CanvasRenderer renderer, Point point, double diameter, Color fillColor, Color strokeColor,
+            double thickness = 1.0, PenLineJoin lineJoin = PenLineJoin.Miter, ICollection<double> dashArray = null, double dashOffset = 0)
+        {
+            renderer.DrawEllipse(point, new Size(diameter, diameter), fillColor, strokeColor, thickness, lineJoin, dashArray, dashOffset);
+        }
+
+        /// <summary>
+        /// Draws a collection of circles in the canvas, where all have the same visual appearance (stroke, fill, etc.).
+        /// This performs better than calling DrawCircle multiple times.
+        /// </summary>
+        /// <param name="renderer">The renderer.</param>
+        /// <param name="points"></param>
         /// <param name="radius">The radius of the circle.</param>
         /// <param name="fillColor">The color of the shape's interior.</param>
         /// <param name="strokeColor">The color of the shape's outline.</param>
@@ -41,10 +61,10 @@ namespace SiliconStudio.Presentation.Extensions
         /// <param name="dashArray">The pattern of dashes and gaps that is used to outline the shape.</param>
         /// <param name="dashOffset">The distance within the dash pattern where a dash begins.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void DrawCircle(this CanvasRenderer renderer, Point point, double radius, Color fillColor, Color strokeColor,
+        public static void DrawCircles(this CanvasRenderer renderer, IList<Point> points, double radius, Color fillColor, Color strokeColor,
             double thickness = 1.0, PenLineJoin lineJoin = PenLineJoin.Miter, ICollection<double> dashArray = null, double dashOffset = 0)
         {
-            renderer.DrawEllipse(point, new Size(radius, radius), fillColor, strokeColor, thickness, lineJoin, dashArray, dashOffset);
+            renderer.DrawEllipses(points, radius, radius, fillColor, strokeColor, thickness, lineJoin, dashArray, dashOffset);
         }
 
         /// <summary>
