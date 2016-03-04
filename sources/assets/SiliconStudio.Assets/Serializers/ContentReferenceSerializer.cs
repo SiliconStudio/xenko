12ac03b1d0916244f1cd131da59ed35306f555c9
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2016-03-04 13:11:57 +0900

    [Core] Move ShadowObject to SiliconStudio.Core.Serialization. Take into account AttachedReference with ShadowObject.Id. Modify serialization of attached reference to serialize the referenceId as well (GUID of the attached reference object)

--- sources/assets/SiliconStudio.Assets/Serializers/ContentReferenceSerializer.cs
+++ sources/assets/SiliconStudio.Assets/Serializers/ContentReferenceSerializer.cs
@@ -6,6 +6,7 @@ using SharpYaml.Events;
 using SharpYaml.Serialization;
 using SiliconStudio.Core;
 using SiliconStudio.Core.IO;
+using SiliconStudio.Core.Reflection;
 using SiliconStudio.Core.Serialization;
 using SiliconStudio.Core.Yaml;
 
@@ -36,12 +37,25 @@ namespace SiliconStudio.Assets.Serializers
         {
             Guid guid;
             UFile location;
-            if (!AssetReference.TryParse(fromScalar.Value, out guid, out location))
+            Guid referenceId;
+            if (!AssetReference.TryParse(fromScalar.Value, out referenceId, out guid, out location))
             {
                 throw new YamlException(fromScalar.Start, fromScalar.End, "Unable to decode asset reference [{0}]. Expecting format GUID:LOCATION".ToFormat(fromScalar.Value));
             }
 
-            return AttachedReferenceManager.CreateSerializableVersion(context.Descriptor.Type, guid, location);
+            var instance = AttachedReferenceManager.CreateSerializableVersion(context.Descriptor.Type, guid, location);
+
+            // If the referenceId is empty, force its creation, else attach it to the reference
+            if (referenceId == Guid.Empty)
+            {
+
+                IdentifiableHelper.GetId(instance);
+            }
+            else
+            {
+                IdentifiableHelper.SetId(instance, referenceId);
+            }
+            return instance;
         }
         public override string ConvertTo(ref ObjectContext objectContext)
         {
@@ -49,7 +63,8 @@ namespace SiliconStudio.Assets.Serializers
             if (attachedReference == null)
                 throw new YamlException(string.Format("Unable to extract asset reference from object [{0}]", objectContext.Instance));
 
-            return string.Format("{0}:{1}", attachedReference.Id, attachedReference.Url);
+            var referenceId = IdentifiableHelper.GetId(objectContext.Instance);
+            return $"{referenceId}/{attachedReference.Id}:{attachedReference.Url}";
         }
     }
 }
