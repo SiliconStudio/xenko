e5c15344f5c4f7737c34736d07f19e2ada3e2867
Virgile Bello <virgile@siliconstudio.co.jp>
2016-03-09 12:21:27 +0900

    [GraphicsRefactor] Skinning: Allow values higher than MaxBones

--- sources/engine/SiliconStudio.Xenko.Engine/Rendering/SkinningRenderFeature.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Rendering/SkinningRenderFeature.cs
@@ -72,10 +72,8 @@ namespace SiliconStudio.Xenko.Rendering
                         renderEffect.EffectValidator.ValidateParameter(MaterialKeys.HasSkinningNormal, renderMesh.Mesh.Parameters.Get(MaterialKeys.HasSkinningNormal));
                         renderEffect.EffectValidator.ValidateParameter(MaterialKeys.HasSkinningTangent, renderMesh.Mesh.Parameters.Get(MaterialKeys.HasSkinningTangent));
 
-                        var skinningBones = renderMesh.Mesh.Skinning.Bones.Length;
-                        if (skinningBones > MaxBones)
-                            throw new NotImplementedException("Too many bones");
-                        renderEffect.EffectValidator.ValidateParameter(MaterialKeys.SkinningMaxBones, MaxBones);
+                        var skinningBones = Math.Min(MaxBones, renderMesh.Mesh.Skinning.Bones.Length);
+                        renderEffect.EffectValidator.ValidateParameter(MaterialKeys.SkinningMaxBones, skinningBones);
                     }
                 }
             }
