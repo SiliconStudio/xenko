0c3814718b3d76d371e0f4caede3b1f3afdc9c60
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-03-09 14:28:52 +0900

    [Quantum] Quantum objects are not IDirtiable anymore

--- sources/common/presentation/SiliconStudio.Quantum/Commands/SyncNodeCommand.cs
+++ sources/common/presentation/SiliconStudio.Quantum/Commands/SyncNodeCommand.cs
@@ -7,12 +7,12 @@ namespace SiliconStudio.Quantum.Commands
 {
     public abstract class SyncNodeCommand : NodeCommandBase
     {
-        public sealed override Task<IActionItem> Execute(IContent content, object index, object parameter, IEnumerable<IDirtiable> dirtiables)
+        public sealed override Task<IActionItem> Execute(IContent content, object index, object parameter)
         {
-            var result = ExecuteSync(content, index, parameter, dirtiables);
+            var result = ExecuteSync(content, index, parameter);
             return Task.FromResult(result);
         }
 
-        protected abstract IActionItem ExecuteSync(IContent content, object index, object parameter, IEnumerable<IDirtiable> dirtiables);
+        protected abstract IActionItem ExecuteSync(IContent content, object index, object parameter);
     }
 }
