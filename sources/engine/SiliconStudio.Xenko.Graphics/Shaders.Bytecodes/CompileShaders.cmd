96a3e8dac5c3bfb60d138d909c2f8a7f7e58731b
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-03-10 12:08:26 +0900

    [Graphics] Regenerate shader bytecodes (OpenGL/ES)

--- sources/engine/SiliconStudio.Xenko.Graphics/Shaders.Bytecodes/CompileShaders.cmd
+++ sources/engine/SiliconStudio.Xenko.Graphics/Shaders.Bytecodes/CompileShaders.cmd
@@ -2,6 +2,6 @@
 setlocal
 set XenkoSdkDir=%~dp0..\..\..\..\
 set XenkoSdkBinDir=%XenkoSdkDir%Bin\Windows-Direct3D11\
-%XenkoSdkBinDir%SiliconStudio.Assets.CompilerApp.exe --profile=Windows --graphics-platform=Direct3D11 --platform=Windows --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.xkpkg
-%XenkoSdkBinDir%SiliconStudio.Assets.CompilerApp.exe --profile=Windows-OpenGL --graphics-platform=OpenGL --platform=Windows --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.xkpkg
-%XenkoSdkBinDir%SiliconStudio.Assets.CompilerApp.exe --profile=Windows-OpenGLES --graphics-platform=OpenGLES --platform=Windows --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.xkpkg
+%XenkoSdkBinDir%SiliconStudio.Assets.CompilerApp.exe --profile=Windows --platform=Windows --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.xkpkg
+%XenkoSdkBinDir%SiliconStudio.Assets.CompilerApp.exe --profile=Windows-OpenGL --platform=Windows --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.xkpkg
+%XenkoSdkBinDir%SiliconStudio.Assets.CompilerApp.exe --profile=Windows-OpenGLES --platform=Windows --output-path=%~dp0obj\app_data --build-path=%~dp0obj\build_app_data --package-file=Graphics.xkpkg
