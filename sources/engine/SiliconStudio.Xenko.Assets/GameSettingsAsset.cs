251663e168cde4c123f7dd90e64087fb0d2b30c9
Manu <manu-silicon@users.noreply.github.com>
2016-03-10 15:16:33 +0900

    Enable Linux even when not compiling against CoreCLR

--- sources/engine/SiliconStudio.Xenko.Assets/GameSettingsAsset.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/GameSettingsAsset.cs
@@ -128,11 +128,9 @@ namespace SiliconStudio.Xenko.Assets
                 case PlatformType.Windows10:
                     configPlatform = ConfigPlatforms.Windows10;
                     break;
-#if SILICONSTUDIO_RUNTIME_CORECLR
                 case PlatformType.Linux:
                     configPlatform = ConfigPlatforms.Linux;
                     break;
-#endif
                 default:
                     throw new ArgumentOutOfRangeException(nameof(platform), platform, null);
             }
