2c9f32b35f82c122ccb1ce650acff52e89b24886
Virgile Bello <virgile@siliconstudio.co.jp>
2016-03-10 14:51:38 +0900

    [OpenGL] Better detection of comparison sampler states

--- sources/engine/SiliconStudio.Xenko.Shaders.Compiler/OpenGL/ShaderCompiler.cs
+++ sources/engine/SiliconStudio.Xenko.Shaders.Compiler/OpenGL/ShaderCompiler.cs
@@ -218,8 +218,11 @@ namespace SiliconStudio.Xenko.Shaders.Compiler.OpenGL
                 foreach (var variable in glslShader.Declarations.OfType<Variable>().Where(x => (x.Qualifiers.Contains(StorageQualifier.Uniform))))
                 {
                     // Check if we have a variable that starts or ends with this name (in case of samplers)
-                    if (variable.Type == SamplerType.Sampler1D || variable.Type == SamplerType.Sampler2D || variable.Type == SamplerType.Sampler3D ||
-                        variable.Type.Name.Text.Equals(SamplerType.SamplerCube.Name.Text, StringComparison.OrdinalIgnoreCase))
+                    // TODO: Have real AST support for all the list in Keywords.glsl
+                    if (variable.Type.Name.Text.Contains("sampler1D")
+                        || variable.Type.Name.Text.Contains("sampler2D")
+                        || variable.Type.Name.Text.Contains("sampler3D")
+                        || variable.Type.Name.Text.Contains("samplerCube"))
                     {
                         // TODO: Make more robust
                         var textureBindingIndex = reflection.ResourceBindings.IndexOf(x => variable.Name.ToString().StartsWith(x.Param.RawName));
