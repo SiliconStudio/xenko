279b1782524fbe2924b2d51d1f329d1c68102be1
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-03-11 20:16:45 +0900

    [Engine] Fixed un-removed enum

--- sources/engine/SiliconStudio.Xenko.Engine/Engine/Events/EventReceiver.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/Events/EventReceiver.cs
@@ -9,25 +9,6 @@ using System.Threading.Tasks.Dataflow;
 
 namespace SiliconStudio.Xenko.Engine.Events
 {
-    [Flags]
-    public enum EventReceiverOptions
-    {
-        /// <summary>
-        /// If no flags are present only the most recent event will be buffered
-        /// </summary>
-        None,
-        /// <summary>
-        /// If this flag is present the events will be buffered into a queue,
-        /// receivers might decide to consume at any pace they wish as long as they consume them at some point
-        /// </summary>
-        Buffered = 1 << 0,
-        /// <summary>
-        /// If this flag is present at the end of each frame the events queue will be cleared,
-        /// this will ensure that consumers will have always up-to-date events (from previous/current frame)
-        /// </summary>
-        ClearEveryFrame = 1 << 1
-    }
-
     /// <summary>
     /// Creates an event receiver that is used to receive T type events from an EventKey
     /// </summary>
