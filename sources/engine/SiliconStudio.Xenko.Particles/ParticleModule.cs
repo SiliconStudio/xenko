250f3ebee9a261accaae67e5bb59f2393209e0e0
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-03-15 11:20:47 +0900

    [Particles] Proper disposing of resources when restarting/moving/stopping a particle system.

--- sources/engine/SiliconStudio.Xenko.Particles/ParticleModule.cs
+++ sources/engine/SiliconStudio.Xenko.Particles/ParticleModule.cs
@@ -31,7 +31,7 @@ namespace SiliconStudio.Xenko.Particles
         /// <summary>
         /// Resets the current state to the module's initial state
         /// </summary>
-        internal virtual void RestartSimulation() { }
+        internal virtual void ResetSimulation() { }
 
         /// <summary>
         /// Attepmts to get a debug shape (shape type and location matrix) for the current module in order to display its boundaries better
