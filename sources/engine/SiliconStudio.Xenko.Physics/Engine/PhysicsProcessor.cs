956da089cb7f00d2c40ce52a084c2393878fadec
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-03-16 16:36:07 +0900

    [Physics] Code clean ups

--- sources/engine/SiliconStudio.Xenko.Physics/Engine/PhysicsProcessor.cs
+++ sources/engine/SiliconStudio.Xenko.Physics/Engine/PhysicsProcessor.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) 2014-2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using System;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Mathematics;
 using SiliconStudio.Xenko.Engine;
 using SiliconStudio.Xenko.Games;
 using System.Collections.Generic;
 using SiliconStudio.Core.Diagnostics;
-using SiliconStudio.Xenko.Graphics;
 using SiliconStudio.Xenko.Physics.Engine;
 using SiliconStudio.Xenko.Rendering;
 using SiliconStudio.Xenko.Rendering.Composers;
