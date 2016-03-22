87fd1742834f9fbf77e58e4843d06b7b888d97a2
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-03-22 15:45:35 +0900

    [Quantum] Remove custom node factories - there is now a single node factory at a time per NodeContainer

--- sources/common/presentation/SiliconStudio.Quantum/NodeContainer.cs
+++ sources/common/presentation/SiliconStudio.Quantum/NodeContainer.cs
@@ -10,72 +10,16 @@ using SiliconStudio.Quantum.References;
 
 namespace SiliconStudio.Quantum
 {
-    public interface INodeContainer
-    {
-        /// <summary>
-        /// Gets or set the visitor to use to create nodes. Default value is a <see cref="DefaultNodeBuilder"/> constructed with default parameters.
-        /// </summary>
-        INodeBuilder NodeBuilder { get; set; }
-
-        /// <summary>
-        /// Overrides the default factory to use to create <see cref="IGraphNode"/> instances.
-        /// </summary>
-        /// <param name="nodeFactory">The new factory to use to create <see cref="IGraphNode"/> instances.</param>
-        /// <seealso cref="RestoreDefaultFactory"/>
-        void OverrideDefaultFactory(NodeFactoryDelegate nodeFactory);
-
-        /// <summary>
-        /// Clears any override made to the default factory with <see cref="OverrideDefaultFactory"/>.
-        /// This method will restore the default factory of this class.
-        /// </summary>
-        /// <seealso cref="OverrideDefaultFactory"/>
-        void RestoreDefaultFactory();
-
-        /// <summary>
-        /// Registers a custom factory to create <see cref="IGraphNode"/> instances. This method returns an
-        /// identifier that can be passed to the <see cref="GetOrCreateNode"/> method to create custom nodes when needed.
-        /// </summary>
-        /// <param name="nodeFactory">The factory to register.</param>
-        /// <returns>An identifier to the registered factory.</returns>
-        /// <seealso cref="UnregisterFactory"/>
-        NodeFactoryId RegisterFactory(NodeFactoryDelegate nodeFactory);
-
-        /// <summary>
-        /// Unregisters a previously registered factory to create <see cref="IGraphNode"/> instances.
-        /// </summary>
-        /// <param name="factoryId">The identifier of the factory to unregister.</param>
-        /// <seealso cref="RegisterFactory"/>
-        void UnregisterFactory(NodeFactoryId factoryId);
-
-        /// <summary>
-        /// Gets the node associated to a data object, if it exists, otherwise creates a new node for the object and its member recursively.
-        /// </summary>
-        /// <param name="rootObject">The data object.</param>
-        /// <param name="nodeFactoryId">An identifier to the node factory to use to create nodes.</param>
-        /// <returns>The <see cref="IGraphNode"/> associated to the given object.</returns>
-        IGraphNode GetOrCreateNode(object rootObject, NodeFactoryId nodeFactoryId = default(NodeFactoryId));
-
-        /// <summary>
-        /// Gets the <see cref="IGraphNode"/> associated to a data object, if it exists.
-        /// </summary>
-        /// <param name="rootObject">The data object.</param>
-        /// <returns>The <see cref="IGraphNode"/> associated to the given object if available, or <c>null</c> otherwise.</returns>
-        /// <remarks>Calling this method will update references of the returned node and its children, recursively.</remarks>
-        IGraphNode GetNode(object rootObject);
-    }
-
     /// <summary>
     /// A container used to store nodes and resolve references between them.
     /// </summary>
     public class NodeContainer : INodeContainer
     {
         private readonly Dictionary<Guid, IGraphNode> nodesByGuid = new Dictionary<Guid, IGraphNode>();
-        private readonly Dictionary<IGraphNode, NodeFactoryDelegate> factoriesByNode = new Dictionary<IGraphNode, NodeFactoryDelegate>();
-        private readonly Dictionary<NodeFactoryId, NodeFactoryDelegate> nodeFactories = new Dictionary<NodeFactoryId, NodeFactoryDelegate>();
         private readonly IGuidContainer guidContainer;
         private readonly object lockObject = new object();
         private readonly ThreadLocal<HashSet<IGraphNode>> processedNodes = new ThreadLocal<HashSet<IGraphNode>>();
-        private NodeFactoryDelegate defaultNodeFactory;
+        private NodeFactoryDelegate defaultNodeFactory = DefaultNodeFactory;
 
         /// <summary>
         /// Creates a new instance of <see cref="NodeContainer"/> class.
@@ -83,8 +27,6 @@ namespace SiliconStudio.Quantum
         public NodeContainer()
             : this(new GuidContainer())
         {
-            NodeBuilder = CreateDefaultNodeBuilder();
-            OverrideDefaultFactory(DefaultNodeFactory);
         }
 
         /// <summary>
@@ -97,7 +39,6 @@ namespace SiliconStudio.Quantum
             if (guidContainer == null) throw new ArgumentNullException(nameof(guidContainer));
             this.guidContainer = guidContainer;
             NodeBuilder = CreateDefaultNodeBuilder();
-            OverrideDefaultFactory(DefaultNodeFactory);
         }
 
         /// <inheritdoc/>
@@ -114,46 +55,25 @@ namespace SiliconStudio.Quantum
         public IEnumerable<Guid> Guids => nodesByGuid.Keys;
 
         /// <inheritdoc/>
-        public void OverrideDefaultFactory(NodeFactoryDelegate nodeFactory)
+        public void OverrideNodeFactory(NodeFactoryDelegate nodeFactory)
         {
             lock (lockObject)
             {
-                nodeFactories[new NodeFactoryId(Guid.Empty)] = nodeFactory;
                 defaultNodeFactory = nodeFactory;
             }
         }
 
         /// <inheritdoc/>
-        public void RestoreDefaultFactory()
+        public void RestoreDefaultNodeFactory()
         {
             lock (lockObject)
             {
-                OverrideDefaultFactory(DefaultNodeFactory);
+                OverrideNodeFactory(DefaultNodeFactory);
             }
         }
 
         /// <inheritdoc/>
-        public NodeFactoryId RegisterFactory(NodeFactoryDelegate nodeFactory)
-        {
-            lock (lockObject)
-            {
-                var id = new NodeFactoryId(Guid.NewGuid());
-                nodeFactories.Add(id, nodeFactory);
-                return id;
-            }
-        }
-
-        /// <inheritdoc/>
-        public void UnregisterFactory(NodeFactoryId factoryId)
-        {
-            lock (lockObject)
-            {
-                nodeFactories.Remove(factoryId);
-            }
-        }
-
-        /// <inheritdoc/>
-        public IGraphNode GetOrCreateNode(object rootObject, NodeFactoryId nodeFactoryId = default(NodeFactoryId))
+        public IGraphNode GetOrCreateNode(object rootObject)
         {
             if (rootObject == null)
                 return null;
@@ -163,11 +83,7 @@ namespace SiliconStudio.Quantum
                 if (!processedNodes.IsValueCreated)
                     processedNodes.Value = new HashSet<IGraphNode>();
 
-                NodeFactoryDelegate nodeFactory;
-                nodeFactories.TryGetValue(nodeFactoryId, out nodeFactory);
-                if (nodeFactory == null)
-                    nodeFactory = defaultNodeFactory;
-                var node = GetOrCreateNodeInternal(rootObject, nodeFactory);
+                var node = GetOrCreateNodeInternal(rootObject, defaultNodeFactory);
 
                 processedNodes.Value.Clear();
                 return node;
@@ -272,8 +188,6 @@ namespace SiliconStudio.Quantum
 
                 if (result != null)
                 {
-                    // Register the factory used to create this node.
-                    factoriesByNode.Add(result, nodeFactory);
                     // Register reference objects
                     nodesByGuid.Add(result.Guid, result);
                     // Create or update nodes of referenced objects
@@ -296,16 +210,11 @@ namespace SiliconStudio.Quantum
 
                 processedNodes.Value.Add(node);
 
-                NodeFactoryDelegate nodeFactory;
-                factoriesByNode.TryGetValue(node, out nodeFactory);
-                if (nodeFactory == null)
-                    nodeFactory = defaultNodeFactory;
-
                 // If the node was holding a reference, refresh the reference
                 if (node.Content.IsReference)
                 {
                     node.Content.Reference.Refresh(node.Content.Value);
-                    UpdateOrCreateReferenceTarget(node.Content.Reference, node, nodeFactory);
+                    UpdateOrCreateReferenceTarget(node.Content.Reference, node);
                 }
                 else
                 {
@@ -313,13 +222,13 @@ namespace SiliconStudio.Quantum
                     foreach (var child in node.Children.SelectDeep(x => x.Children).Where(x => x.Content.IsReference))
                     {
                         child.Content.Reference.Refresh(child.Content.Value);
-                        UpdateOrCreateReferenceTarget(child.Content.Reference, child, nodeFactory);
+                        UpdateOrCreateReferenceTarget(child.Content.Reference, child);
                     }
                 }
             }
         }
 
-        private void UpdateOrCreateReferenceTarget(IReference reference, IGraphNode node, NodeFactoryDelegate nodeFactory, Stack<object> indices = null)
+        private void UpdateOrCreateReferenceTarget(IReference reference, IGraphNode node, Stack<object> indices = null)
         {
             if (reference == null) throw new ArgumentNullException(nameof(reference));
             if (node == null) throw new ArgumentNullException(nameof(node));
@@ -336,7 +245,7 @@ namespace SiliconStudio.Quantum
                 foreach (var itemReference in referenceEnumerable)
                 {
                     indices.Push(itemReference.Index);
-                    UpdateOrCreateReferenceTarget(itemReference, node, nodeFactory, indices);
+                    UpdateOrCreateReferenceTarget(itemReference, node, indices);
                     indices.Pop();
                 }
             }
@@ -350,7 +259,7 @@ namespace SiliconStudio.Quantum
                 if (singleReference.TargetNode == null && reference.ObjectValue != null)
                 {
                     // This call will recursively update the references.
-                    var target = singleReference.SetTarget(this, nodeFactory);
+                    var target = singleReference.SetTarget(this, defaultNodeFactory);
                     if (target != null)
                     {
                         var structContent = target.Content as BoxedContent;
