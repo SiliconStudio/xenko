5e57e3e254d8f7ccb2c8d0f64ad23c45806c0345
Alexandre Mutel <alexandre@siliconstudio.co.jp>
2016-03-31 16:54:54 +0900

    [Engine] Add more unit tests for TestMaterialGenerator

--- sources/engine/SiliconStudio.Xenko.Assets.Tests/TestMaterialGenerator.cs
+++ sources/engine/SiliconStudio.Xenko.Assets.Tests/TestMaterialGenerator.cs
@@ -13,10 +13,21 @@ using SiliconStudio.Xenko.Shaders;
 
 namespace SiliconStudio.Xenko.Assets.Tests
 {
+    /// <summary>
+    /// Tests for <see cref="MaterialGenerator"/> and related classes
+    /// </summary>
     [TestFixture]
     public class TestMaterialGenerator
     {
         public static readonly ValueParameterKey<Color4> DiffuseValueCustom1 = ParameterKeys.NewValue<Color4>();
+
+        public static readonly ValueParameterKey<float> BlendValueCustom1 = ParameterKeys.NewValue<float>();
+
+        public static readonly ValueParameterKey<float> BlendValueCustom2 = ParameterKeys.NewValue<float>();
+
+        public static readonly ValueParameterKey<float> MetalnessValueCustom1 = ParameterKeys.NewValue<float>();
+
+        public static readonly ValueParameterKey<float> MetalnessValueCustom2 = ParameterKeys.NewValue<float>();
 
         /// <summary>
         /// Test single material (one shading model, no layers)
@@ -251,7 +262,7 @@ Mixins:
 Compositions:
     layers: !ShaderArraySource
         Values:"
-// The top-layer MaterialDiffuseMapFeature
+// The LayerRoot MaterialDiffuseMapFeature
 + @"
             - !ShaderMixinSource
                 Mixins:
@@ -298,7 +309,7 @@ Compositions:
                                     metalnessMap: !ShaderClassSource
                                         ClassName: ComputeColorConstantFloatLink
                                         GenericArguments: [Material.MetalnessValue]"
-// Performs the actual shading for this layer
+// Performs the actual shading for the Layer1
 + @"
                             - !ShaderMixinSource
                                 Mixins:
@@ -327,8 +338,8 @@ Compositions:
         public void Test2Layers2ShadingModels()
         {
             // - LayerRoot:
-            //   - Layer1: SM0
-            //   - Layer2: SM1
+            //   - Layer0: SM0 B0
+            //   - Layer1: SM1 B1
             var context = new MaterialGeneratorContextExtended();
             var materialDesc = new MaterialDescriptor
             {
@@ -379,7 +390,9 @@ Mixins:
     -   ClassName: MaterialSurfaceArray
 Compositions:
     layers: !ShaderArraySource
-        Values:
+        Values:"
+// Layer0: Starting directly with a blend on attributes (with no previous attributes, so they should be black)
++ @"
             - !ShaderMixinSource
                 Mixins:
                     -   ClassName: MaterialSurfaceStreamsBlend
@@ -412,7 +425,9 @@ Compositions:
                                         Compositions:
                                             computeColorSource: !ShaderClassSource
                                                 ClassName: ComputeColorConstantFloatLink
-                                                GenericArguments: [Material.BlendValue]
+                                                GenericArguments: [Material.BlendValue]"
+// Layer0: Apply the shading of SM0
++ @"
             - !ShaderMixinSource
                 Mixins:
                     -   ClassName: MaterialSurfaceLightingAndShading
@@ -421,7 +436,9 @@ Compositions:
                         Values:
                             - !ShaderClassSource
                                 ClassName: MaterialSurfaceShadingDiffuseLambert
-                                GenericArguments: [false]
+                                GenericArguments: [false]"
+// Layer1: We have here a MaterialSurfaceShadingBlend as we are changing the shading model, so we can only generate a blend of shading models and not attributes
++ @"
             - !ShaderMixinSource
                 Mixins:
                     -   ClassName: MaterialSurfaceSetStreamFromComputeColor
@@ -442,7 +459,9 @@ Compositions:
                                 Compositions:
                                     metalnessMap: !ShaderClassSource
                                         ClassName: ComputeColorConstantFloatLink
-                                        GenericArguments: [Material.MetalnessValue]
+                                        GenericArguments: [Material.MetalnessValue]"
+// Layer1: Apply the shading of SM1
++ @"
                             - !ShaderMixinSource
                                 Mixins:
                                     -   ClassName: MaterialSurfaceLightingAndShading
@@ -463,6 +482,217 @@ Compositions:
             AssertShaderSourceEqual(expected, pixelShaders);
         }
 
+        /// <summary>
+        /// Test material with 2 shading models and 3 layers (without a shading model on the top layer)
+        /// </summary>
+        [Test]
+        public void Test3Layers2ShadingModels()
+        {
+            // This test case is more complex as it shows that the change in shading model is triggering 
+            // a blend of shading (and not a blend of attributes) and the blending factor used for blending 
+            // the 2 shading models is the first one that appears when the shading model changes (Layer1 in the following case)
+
+            // - LayerRoot:
+            //   - Layer0: SM0 B0
+            //   - Layer1: SM1 blending with BlendValueCustom1 -> BlendValueCustom1 will be used as the global blending of (Layer1 + Layer2) over Layer0
+            //   - Layer2: SM1 blending with BlendValueCustom2 -> Same shading model
+            var context = new MaterialGeneratorContextExtended();
+            var materialDesc = new MaterialDescriptor
+            {
+                Layers =
+                {
+                    new MaterialBlendLayer()
+                    {
+                        BlendMap = new ComputeFloat(1.0f),
+                        Material = context.MapTo(new Material(), new MaterialDescriptor()
+                        {
+                            Attributes =
+                            {
+                                Diffuse = new MaterialDiffuseMapFeature(new ComputeColor(Color.Red)),
+                                DiffuseModel = new MaterialDiffuseLambertModelFeature()
+                            },
+                        })
+                    },
+                    new MaterialBlendLayer()
+                    {
+                        BlendMap = new ComputeFloat(0.5f)
+                        {
+                            Key = BlendValueCustom1, // Use custom key in order to see them in the output
+                        },
+                        Material = context.MapTo(new Material(), new MaterialDescriptor()
+                        {
+                            Attributes =
+                            {
+                                Specular= new MaterialMetalnessMapFeature(new ComputeFloat(1.0f)
+                                {
+                                    Key = MetalnessValueCustom1,
+                                }),
+                                SpecularModel = new MaterialSpecularMicrofacetModelFeature()
+                            },
+                        })
+                    },
+                    new MaterialBlendLayer()
+                    {
+                        BlendMap = new ComputeFloat(0.8f)                        {
+                            Key = BlendValueCustom2, // Use custom key in order to see them in the output
+                        },
+                        Material = context.MapTo(new Material(), new MaterialDescriptor()
+                        {
+                            Attributes =
+                            {
+                                Specular= new MaterialMetalnessMapFeature(new ComputeFloat(0.5f)
+                                {
+                                    Key = MetalnessValueCustom2,
+                                }),
+                                SpecularModel = new MaterialSpecularMicrofacetModelFeature()
+                            },
+                        })
+                    }
+                }
+            };
+
+            var result = MaterialGenerator.Generate(materialDesc, context, "diffuse_and_specularx2");
+            Assert.False(result.HasErrors);
+
+            var material = result.Material;
+            Assert.Null(material.Parameters.Get(MaterialKeys.VertexStageSurfaceShaders));
+            Assert.Null(material.Parameters.Get(MaterialKeys.DomainStageSurfaceShaders));
+
+            // Check that the color is correctly store in the shader parameters
+            Assert.AreEqual(new Color4(Color.Red), material.Parameters.Get(MaterialKeys.DiffuseValue));
+
+            var pixelShaders = material.Parameters.Get(MaterialKeys.PixelStageSurfaceShaders);
+
+            var expected = @"!ShaderMixinSource
+Mixins:
+    -   ClassName: MaterialSurfaceArray
+Compositions:
+    layers: !ShaderArraySource
+        Values:"
+// Layer0: Starting directly with a blend on attributes (with no previous attributes, so they should be black)
++ @"
+            - !ShaderMixinSource
+                Mixins:
+                    -   ClassName: MaterialSurfaceStreamsBlend
+                Compositions:
+                    blends: !ShaderArraySource
+                        Values:
+                            - !ShaderClassSource
+                                ClassName: MaterialStreamLinearBlend
+                                GenericArguments: [matDiffuse]
+                            - !ShaderClassSource
+                                ClassName: MaterialStreamLinearBlend
+                                GenericArguments: [matColorBase]
+                    layer: !ShaderMixinSource
+                        Mixins:
+                            -   ClassName: MaterialSurfaceArray
+                        Compositions:
+                            layers: !ShaderArraySource
+                                Values:
+                                    - !ShaderMixinSource
+                                        Mixins:
+                                            -   ClassName: MaterialSurfaceDiffuse
+                                        Compositions:
+                                            diffuseMap: !ShaderClassSource
+                                                ClassName: ComputeColorConstantColorLink
+                                                GenericArguments: [Material.DiffuseValue]
+                                    - !ShaderMixinSource
+                                        Mixins:
+                                            -   ClassName: MaterialSurfaceSetStreamFromComputeColor
+                                                GenericArguments: [matBlend, r]
+                                        Compositions:
+                                            computeColorSource: !ShaderClassSource
+                                                ClassName: ComputeColorConstantFloatLink
+                                                GenericArguments: [Material.BlendValue]"
+// Layer0: Shading of SM0
++ @"
+            - !ShaderMixinSource
+                Mixins:
+                    -   ClassName: MaterialSurfaceLightingAndShading
+                Compositions:
+                    surfaces: !ShaderArraySource
+                        Values:
+                            - !ShaderClassSource
+                                ClassName: MaterialSurfaceShadingDiffuseLambert
+                                GenericArguments: [false]"
+// Layer1: Blending of Layer1 over Layer0. Note that the blending is using BlendValueCustom1 key!!!
++ @"
+            - !ShaderMixinSource
+                Mixins:
+                    -   ClassName: MaterialSurfaceSetStreamFromComputeColor
+                        GenericArguments: [matBlend, r]
+                Compositions:
+                    computeColorSource: !ShaderClassSource
+                        ClassName: ComputeColorConstantFloatLink
+                        GenericArguments: [TestMaterialGenerator.BlendValueCustom1]
+            - !ShaderMixinSource
+                Mixins:
+                    -   ClassName: MaterialSurfaceShadingBlend
+                Compositions:
+                    layers: !ShaderArraySource
+                        Values:"
+// Layer1: Attributes of Layer1
++ @"
+                            - !ShaderMixinSource
+                                Mixins:
+                                    -   ClassName: MaterialSurfaceMetalness
+                                Compositions:
+                                    metalnessMap: !ShaderClassSource
+                                        ClassName: ComputeColorConstantFloatLink
+                                        GenericArguments: [TestMaterialGenerator.MetalnessValueCustom1]"
+// Layer2: Blend stream attributes over Layer1
++ @"
+                            - !ShaderMixinSource
+                                Mixins:
+                                    -   ClassName: MaterialSurfaceStreamsBlend
+                                Compositions:
+                                    blends: !ShaderArraySource
+                                        Values:
+                                            - !ShaderClassSource
+                                                ClassName: MaterialStreamLinearBlend
+                                                GenericArguments: [matSpecular]
+                                    layer: !ShaderMixinSource
+                                        Mixins:
+                                            -   ClassName: MaterialSurfaceArray
+                                        Compositions:
+                                            layers: !ShaderArraySource
+                                                Values:
+                                                    - !ShaderMixinSource
+                                                        Mixins:
+                                                            -   ClassName: MaterialSurfaceMetalness
+                                                        Compositions:
+                                                            metalnessMap: !ShaderClassSource
+                                                                ClassName: ComputeColorConstantFloatLink
+                                                                GenericArguments: [TestMaterialGenerator.MetalnessValueCustom2]
+                                                    - !ShaderMixinSource
+                                                        Mixins:
+                                                            -   ClassName: MaterialSurfaceSetStreamFromComputeColor
+                                                                GenericArguments: [matBlend, r]
+                                                        Compositions:
+                                                            computeColorSource: !ShaderClassSource
+                                                                ClassName: ComputeColorConstantFloatLink
+                                                                GenericArguments: [TestMaterialGenerator.BlendValueCustom2]"
+// Layer2: Shading of Layer2 with SM1. The result of shading will be blend over Layer1 using BlendValueCustom1
++ @"
+                            - !ShaderMixinSource
+                                Mixins:
+                                    -   ClassName: MaterialSurfaceLightingAndShading
+                                Compositions:
+                                    surfaces: !ShaderArraySource
+                                        Values:
+                                            - !ShaderMixinSource
+                                                Mixins:
+                                                    -   ClassName: MaterialSurfaceShadingSpecularMicrofacet
+                                                Compositions:
+                                                    fresnelFunction: !ShaderClassSource
+                                                        ClassName: MaterialSpecularMicrofacetFresnelSchlick
+                                                    geometricShadowingFunction: !ShaderClassSource
+                                                        ClassName: MaterialSpecularMicrofacetVisibilitySmithSchlickGGX
+                                                    normalDistributionFunction: !ShaderClassSource
+                                                        ClassName: MaterialSpecularMicrofacetNormalDistributionGGX";
+
+            AssertShaderSourceEqual(expected, pixelShaders);
+        }
 
         private class MaterialGeneratorContextExtended : MaterialGeneratorContext
         {
