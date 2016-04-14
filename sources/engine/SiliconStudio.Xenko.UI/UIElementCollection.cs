4eedd585c183df701ca4506771fcc3ff640d6d53
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-04-14 16:52:10 +0900

    [UI] Fixed serialization issues with collections (XK-3180).

--- sources/engine/SiliconStudio.Xenko.UI/UIElementCollection.cs
+++ sources/engine/SiliconStudio.Xenko.UI/UIElementCollection.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core;
 using SiliconStudio.Core.Collections;
 
 namespace SiliconStudio.Xenko.UI
@@ -7,6 +9,7 @@ namespace SiliconStudio.Xenko.UI
     /// <summary>
     /// A collection of UIElements.
     /// </summary>
+    [DataContract(nameof(UIElementCollection))]
     public class UIElementCollection : TrackingCollection<UIElement>
     {
     }
