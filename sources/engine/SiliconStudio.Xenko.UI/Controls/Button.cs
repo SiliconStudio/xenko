44e18939536ee456f10d36208faf400b612301b0
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-05-23 16:25:52 +0900

    [UI] Stretch the button image when not sized to the content.

--- sources/engine/SiliconStudio.Xenko.UI/Controls/Button.cs
+++ sources/engine/SiliconStudio.Xenko.UI/Controls/Button.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using System;
 using System.ComponentModel;
 using System.Diagnostics;
 
@@ -19,6 +18,8 @@ namespace SiliconStudio.Xenko.UI.Controls
     [DebuggerDisplay("Button - Name={Name}")]
     public class Button : ButtonBase
     {
+        private StretchType imageStretchType = StretchType.None;
+        private StretchDirection imageStretchDirection = StretchDirection.Both;
         private ISpriteProvider pressedImage;
         private ISpriteProvider notPressedImage;
         private ISpriteProvider mouseOverImage;
@@ -42,6 +43,40 @@ namespace SiliconStudio.Xenko.UI.Controls
                     return;
 
                 base.IsPressed = value;
+                InvalidateMeasure();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value that describes how the button image should be stretched to fill the destination rectangle.
+        /// </summary>
+        /// <remarks>This property has no effect is <see cref="SizeToContent"/> is <c>true</c>.</remarks>
+        [DataMember]
+        [Display(category: LayoutCategory)]
+        [DefaultValue(StretchType.None)]
+        public StretchType ImageStretchType
+        {
+            get { return imageStretchType; }
+            set
+            {
+                imageStretchType = value;
+                InvalidateMeasure();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value that indicates how the button image is scaled.
+        /// </summary>
+        /// <remarks>This property has no effect is <see cref="SizeToContent"/> is <c>true</c>.</remarks>
+        [DataMember]
+        [Display(category: LayoutCategory)]
+        [DefaultValue(StretchDirection.Both)]
+        public StretchDirection ImageStretchDirection
+        {
+            get { return imageStretchDirection; }
+            set
+            {
+                imageStretchDirection = value;
                 InvalidateMeasure();
             }
         }
@@ -130,29 +165,15 @@ namespace SiliconStudio.Xenko.UI.Controls
         {
             return sizeToContent
                 ? base.ArrangeOverride(finalSizeWithoutMargins)
-                : CalculateImageSizeFromAvailable(finalSizeWithoutMargins);
+                : ImageSizeHelper.CalculateImageSizeFromAvailable(ButtonImage, finalSizeWithoutMargins, ImageStretchType, ImageStretchDirection, false);
         }
 
         /// <inheritdoc/>
         protected override Vector3 MeasureOverride(Vector3 availableSizeWithoutMargins)
         {
-            if (sizeToContent)
-            {
-                return base.MeasureOverride(availableSizeWithoutMargins);
-            }
-
-            // Note: copied from ImageElement
-            var desiredSize = CalculateImageSizeFromAvailable(availableSizeWithoutMargins);
-
-            var sprite = ButtonImage;
-            if (sprite == null || !sprite.HasBorders)
-                return desiredSize;
-
-            var borderSum = new Vector2(sprite.BordersInternal.X + sprite.BordersInternal.Z, sprite.BordersInternal.Y + sprite.BordersInternal.W);
-            if (sprite.Orientation == ImageOrientation.Rotated90)
-                Utilities.Swap(ref borderSum.X, ref borderSum.Y);
-
-            return new Vector3(Math.Max(desiredSize.X, borderSum.X), Math.Max(desiredSize.Y, borderSum.Y), desiredSize.Z);
+            return sizeToContent
+                ? base.MeasureOverride(availableSizeWithoutMargins)
+                : ImageSizeHelper.CalculateImageSizeFromAvailable(ButtonImage, availableSizeWithoutMargins, ImageStretchType, ImageStretchDirection, true);
         }
 
         /// <summary>
@@ -163,42 +184,7 @@ namespace SiliconStudio.Xenko.UI.Controls
         {
             InvalidateButtonImage();
         }
-
-        // Note: copied from ImageElement
-        private Vector3 CalculateImageSizeFromAvailable(Vector3 availableSizeWithoutMargins)
-        {
-            var sprite = ButtonImage;
-            if (sprite == null) // no associated image -> no region needed
-                return Vector3.Zero;
-
-            var idealSize = sprite.SizeInPixels;
-            if (idealSize.X <= 0 || idealSize.Y <= 0) // image size null or invalid -> no region needed
-                return Vector3.Zero;
-
-            if (float.IsInfinity(availableSizeWithoutMargins.X) && float.IsInfinity(availableSizeWithoutMargins.Y)) // unconstrained available size -> take the best size for the image: the image size
-                return new Vector3(idealSize, 0);
-
-            // initialize the desired size with maximum available size
-            var desiredSize = availableSizeWithoutMargins;
-
-            // compute the desired image ratios
-            var desiredScale = new Vector2(desiredSize.X / idealSize.X, desiredSize.Y / idealSize.Y);
-
-            // when the size along a given axis is free take the same ratio as the constrained axis.
-            if (float.IsInfinity(desiredScale.X))
-                desiredScale.X = desiredScale.Y;
-            if (float.IsInfinity(desiredScale.Y))
-                desiredScale.Y = desiredScale.X;
-
-            // adjust the scales
-            desiredScale.X = desiredScale.Y = Math.Min(desiredScale.X, desiredScale.Y);
-
-            // update the desired size based on the desired scales
-            desiredSize = new Vector3(idealSize.X * desiredScale.X, idealSize.Y * desiredScale.Y, 0f);
-
-            return desiredSize;
-        }
-
+        
         private void InvalidateButtonImage()
         {
             if (!sizeToContent)
