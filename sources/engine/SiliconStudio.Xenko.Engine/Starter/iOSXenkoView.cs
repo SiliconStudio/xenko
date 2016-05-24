60fbc2818e1570863dd00e2812419ea77a85d00c
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-05-24 17:49:34 +0900

    [Engine] Fixed iOS window resize issue (specially during orientation changes)

--- sources/engine/SiliconStudio.Xenko.Engine/Starter/iOSXenkoView.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Starter/iOSXenkoView.cs
@@ -1,10 +1,15 @@
 // Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 #if SILICONSTUDIO_PLATFORM_IOS
+using System;
 using System.Drawing;
+using System.Reflection;
 using CoreAnimation;
+using CoreGraphics;
 using Foundation;
 using ObjCRuntime;
+using OpenGLES;
+using OpenTK;
 using OpenTK.Graphics.ES30;
 using OpenTK.Platform.iPhoneOS;
 using SiliconStudio.Xenko.Games;
@@ -15,10 +20,14 @@ namespace SiliconStudio.Xenko.Starter
     // note: for more information on iOS application life cycle, 
     // see http://docs.xamarin.com/guides/cross-platform/application_fundamentals/backgrounding/part_1_introduction_to_backgrounding_in_ios
     [Register("iOSXenkoView")]
-    public class iOSXenkoView : iPhoneOSGameView, IAnimatedGameView
+    public sealed class iOSXenkoView : iPhoneOSGameView, IAnimatedGameView
     {
         CADisplayLink displayLink;
         private bool isRunning;
+
+        private uint depthRenderBuffer;
+        private int renderBuffer;
+        private int frameBuffer;
 
         public iOSXenkoView(RectangleF frame)
             : base(frame)
@@ -29,18 +38,33 @@ namespace SiliconStudio.Xenko.Starter
         {
             base.CreateFrameBuffer();
 
-            // TODO: PDX-364: depth format is currently hard coded (need to investigate how it can be transmitted)
-            // Create a depth renderbuffer
-            uint depthRenderBuffer;
-            GL.GenRenderbuffers(1, out depthRenderBuffer);
-            GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, depthRenderBuffer);
+            renderBuffer = Renderbuffer;
+            frameBuffer = Framebuffer;
+        }
 
-            // Allocate storage for the new renderbuffer
-            GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, RenderbufferInternalFormat.Depth24Stencil8, (int)(Size.Width * Layer.ContentsScale), (int)(Size.Height * Layer.ContentsScale));
+        public override void LayoutSubviews()
+        {
+            // ISSUE: reference to a compiler-generated method
+            base.LayoutSubviews();
+            if (GraphicsContext == null)
+                return;
+            var bounds = Bounds;
+            if (Math.Round(bounds.Width) == Size.Width && Math.Round(bounds.Height) == Size.Height)
+                return;
 
-            // Attach the renderbuffer to the framebuffer's depth attachment point
-            GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferSlot.DepthAttachment, RenderbufferTarget.Renderbuffer, depthRenderBuffer);
-            GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferSlot.StencilAttachment, RenderbufferTarget.Renderbuffer, depthRenderBuffer);
+            if (!GraphicsContext.IsCurrent)
+                MakeCurrent();
+
+            Size = new Size((int)bounds.Width, (int)bounds.Height);
+
+            GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, renderBuffer);
+            if (!EAGLContext.RenderBufferStorage((uint)RenderbufferTarget.Renderbuffer, (CAEAGLLayer)Layer))
+            {
+                throw new InvalidOperationException("Error with RenderbufferStorage()!");
+            }
+
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, frameBuffer);
+            GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferSlot.ColorAttachment0, RenderbufferTarget.Renderbuffer, frameBuffer);
         }
 
         [Export("layerClass")]
@@ -56,10 +80,10 @@ namespace SiliconStudio.Xenko.Starter
 
             CreateFrameBuffer();
 
-            var displayLink = UIScreen.MainScreen.CreateDisplayLink(this, new Selector("drawFrame"));
-            displayLink.FrameInterval = 0;
-            displayLink.AddToRunLoop(NSRunLoop.Current, NSRunLoop.NSDefaultRunLoopMode);
-            this.displayLink = displayLink;
+            var link = UIScreen.MainScreen.CreateDisplayLink(this, new Selector("drawFrame"));
+            link.FrameInterval = 0;
+            link.AddToRunLoop(NSRunLoop.Current, NSRunLoop.NSDefaultRunLoopMode);
+            displayLink = link;
 
             isRunning = true;
         }
@@ -80,7 +104,7 @@ namespace SiliconStudio.Xenko.Starter
         [Export("drawFrame")]
         void DrawFrame()
         {
-            OnRenderFrame(new OpenTK.FrameEventArgs());
+            OnRenderFrame(new FrameEventArgs());
         }
     }
 }
