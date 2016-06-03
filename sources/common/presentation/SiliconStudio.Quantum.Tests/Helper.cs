54f37009d1116bf720caf128649a1316bc6019b3
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-06-03 15:48:16 +0900

    [Quantum] Add more helpers in the unit tests of Quantum

--- sources/common/presentation/SiliconStudio.Quantum.Tests/Helper.cs
+++ sources/common/presentation/SiliconStudio.Quantum.Tests/Helper.cs
@@ -11,13 +11,19 @@ namespace SiliconStudio.Quantum.Tests
 {
     public static class Helper
     {
-        public static void TestObjectContentNode(IGraphNode node, object obj, int childCount)
+        /// <summary>
+        /// Tests the validity of a node that is an object that is not a collection
+        /// </summary>
+        /// <param name="node">The node to validate.</param>
+        /// <param name="obj">The value represented by this node.</param>
+        /// <param name="childCount">The number of members expected in the node.</param>
+        public static void TestNonCollectionObjectContentNode(IGraphNode node, object obj, int childCount)
         {
             if (node == null) throw new ArgumentNullException(nameof(node));
             if (obj == null) throw new ArgumentNullException(nameof(obj));
 
             // Check that the content is of the expected type.
-            Assert.AreEqual(typeof(ObjectContent), node.Content.GetType());
+            Assert.IsInstanceOf<ObjectContent>(node.Content);
             // Check that the content is properly referencing its node.
             Assert.AreEqual(node, node.Content.OwnerNode);
             // A node with an ObjectContent should have the same name that the type of its content.
@@ -26,12 +32,57 @@ namespace SiliconStudio.Quantum.Tests
             Assert.IsNull(node.Parent);
             // A node with an ObjectContent should have the related object as value of its content.
             Assert.AreEqual(obj, node.Content.Retrieve());
-            // A node with an ObjectContent should not contain a reference.
+            // A node with an ObjectContent should not contain a reference if it does not represent a collection.
             Assert.AreEqual(false, node.Content.IsReference);
             // Check that we have the expected number of children.
             Assert.AreEqual(childCount, node.Children.Count);
         }
 
+        /// <summary>
+        /// Tests the validity of a node that is an object that is a collection
+        /// </summary>
+        /// <param name="node">The node to validate.</param>
+        /// <param name="obj">The value represented by this node.</param>
+        /// <param name="isReference">Indicate whether the node is expected to contain an enumerable reference to the collection items.</param>
+        public static void TestCollectionObjectContentNode(IGraphNode node, object obj, bool isReference)
+        {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+
+            // Check that the content is of the expected type.
+            Assert.IsInstanceOf<ObjectContent>(node.Content);
+            // Check that the content is properly referencing its node.
+            Assert.AreEqual(node, node.Content.OwnerNode);
+            // A node with an ObjectContent should have the same name that the type of its content.
+            Assert.AreEqual(obj.GetType().Name, node.Name);
+            // A node with an ObjectContent should be a root node.
+            Assert.IsNull(node.Parent);
+            // A node with an ObjectContent should have the related object as value of its content.
+            Assert.AreEqual(obj, node.Content.Retrieve());
+            if (isReference)
+            {
+                // A node with an ObjectContent representing a collection of reference types should contain an enumerable reference.
+                Assert.AreEqual(true, node.Content.IsReference);
+                Assert.IsInstanceOf<ReferenceEnumerable>(node.Content.Reference);
+            }
+            else
+            {
+                // A node with an ObjectContent representing a collection of primitive or struct types should not contain a refernce.
+                Assert.AreEqual(false, node.Content.IsReference);            
+            }
+            // A node with an ObjectContent representing a collection should not have any child.
+            Assert.AreEqual(0, node.Children.Count);
+        }
+
+        /// <summary>
+        /// Tests the validity of a node that is a member of an object.
+        /// </summary>
+        /// <param name="containerNode">The node of the container of this member.</param>
+        /// <param name="memberNode">The memeber node to validate.</param>
+        /// <param name="container">The value represented by the container node.</param>
+        /// <param name="member">The value of the member represented by the member node.</param>
+        /// <param name="memberName">The name of the member to validate.</param>
+        /// <param name="isReference">Indicate whether the member node is expected to contain a reference to the value it represents.</param>
         public static void TestMemberContentNode(IGraphNode containerNode, IGraphNode memberNode, object container, object member, string memberName, bool isReference)
         {
             if (containerNode == null) throw new ArgumentNullException(nameof(containerNode));
@@ -52,6 +103,11 @@ namespace SiliconStudio.Quantum.Tests
             Assert.AreEqual(isReference, memberNode.Content.IsReference);
         }
 
+        /// <summary>
+        /// Tests the validity of a reference to a non-null target object.
+        /// </summary>
+        /// <param name="reference">The reference to test.</param>
+        /// <param name="targetValue">The actual value pointed by the reference.</param>
         public static void TestNonNullObjectReference(IReference reference, object targetValue)
         {
             var objReference = reference as ObjectReference;
@@ -63,9 +119,15 @@ namespace SiliconStudio.Quantum.Tests
             // Check that the values match.
             Assert.AreEqual(targetValue, objReference.ObjectValue);
             // Check that the target is an object content node.
-            TestObjectContentNode(objReference.TargetNode, targetValue, objReference.TargetNode.Children.Count);
+            TestNonCollectionObjectContentNode(objReference.TargetNode, targetValue, objReference.TargetNode.Children.Count);
         }
 
+        /// <summary>
+        /// Tests the validity of a reference to a non-null target node.
+        /// </summary>
+        /// <param name="reference">The reference to test.</param>
+        /// <param name="targetNode">The node that is supposed to be the target of the reference.</param>
+        /// <param name="targetValue">The actual value pointed by the reference.</param>
         public static void TestNonNullObjectReference(IReference reference, IGraphNode targetNode, object targetValue)
         {
             if (targetNode == null) throw new ArgumentNullException(nameof(targetNode));
@@ -80,9 +142,13 @@ namespace SiliconStudio.Quantum.Tests
             // Check that the values match.
             Assert.AreEqual(targetValue, objReference.ObjectValue);
             // Check that the target is an object content node.
-            TestObjectContentNode(targetNode, targetValue, targetNode.Children.Count);
+            TestNonCollectionObjectContentNode(targetNode, targetValue, targetNode.Children.Count);
         }
 
+        /// <summary>
+        /// Tests the validity of a reference to a null target object.
+        /// </summary>
+        /// <param name="reference">The reference to test.</param>
         public static void TestNullObjectReference(IReference reference)
         {
             var objReference = reference as ObjectReference;
@@ -97,6 +163,11 @@ namespace SiliconStudio.Quantum.Tests
             Assert.AreEqual(null, objReference.ObjectValue);
         }
 
+        /// <summary>
+        /// Tests the validity of an enumerable reference.
+        /// </summary>
+        /// <param name="reference">The reference to test.</param>
+        /// <param name="targetValue">The actual collection pointed by the reference.</param>
         public static void TestReferenceEnumerable(IReference reference, object targetValue)
         {
             var referenceEnum = reference as ReferenceEnumerable;
@@ -124,19 +195,21 @@ namespace SiliconStudio.Quantum.Tests
             //Assert.IsNull(reference.Index);
         }
 
+        /// <summary>
+        /// Tests the validity of a node that represents a structure.
+        /// </summary>
+        /// <param name="structNode">The structure node to test.</param>
+        /// <param name="structValue">The value of the structure represented by this node.</param>
+        /// <param name="childCount">The number of members expected in the node.</param>
         public static void TestStructContentNode(IGraphNode structNode, object structValue, int childCount)
         {
             if (structNode == null) throw new ArgumentNullException(nameof(structNode));
             if (structValue == null) throw new ArgumentNullException(nameof(structValue));
-            // Check that the content is of the expected type.
-            Assert.AreEqual(typeof(MemberContent), structNode.Content.GetType());
             // Check that the content is properly referencing its node.
             Assert.AreEqual(structNode, structNode.Content.OwnerNode);
-            // A node with an ObjectContent should be a root node.
-            Assert.IsNotNull(structNode.Parent);
-            // A node with an ObjectContent should have the related object as value of its content.
+            // A struct node should have the related struct as value of its content.
             Assert.AreEqual(structValue, structNode.Content.Retrieve());
-            // A node with an ObjectContent should not contain a reference.
+            // A struct node should not contain a reference.
             Assert.AreEqual(false, structNode.Content.IsReference);
             // Check that we have the expected number of children.
             Assert.AreEqual(childCount, structNode.Children.Count);
