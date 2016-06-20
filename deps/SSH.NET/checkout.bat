7e60024704db5b630ac792cbabc82bfbed9a0f72
Manu <emmanuel.stapf@siliconstudio.co.jp>
2016-06-20 13:08:36 +0900

    [General] Add deployment of Linux packages
    Using the Renci.SshNet library we added a new task DeployOnLinux that
    copies all the files required to run on Linux.

--- deps/SSH.NET/checkout.bat
+++ deps/SSH.NET/checkout.bat
@@ -1,7 +1,4 @@
 "%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:sshnet/SSH.NET.git -b master ../../externals/SSH.NET
-cd ..\..\externals\SSH.NET
-"%ProgramFiles(x86)%\Git\Bin\git.exe" checkout 4f25cc00a7129bfd001c8d79a4ae3d69655751ca
-
 cd ..\..\deps\SSH.NET
 
 if NOT ERRORLEVEL 0 pause
