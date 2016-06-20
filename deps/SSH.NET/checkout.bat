fe49e6d709c40c97eb3ffbe2a8d9202d9f3d60b0
Manu <emmanuel.stapf@siliconstudio.co.jp>
2016-06-20 10:54:53 +0900

    [SSH.NET] Update to latest master

--- deps/SSH.NET/checkout.bat
+++ deps/SSH.NET/checkout.bat
@@ -1,5 +1,7 @@
 "%ProgramFiles(x86)%\Git\Bin\git.exe" clone git@github.com:sshnet/SSH.NET.git -b master ../../externals/SSH.NET
-cd ../../externals/SSH.NET
-"%ProgramFiles(x86)%\Git\Bin\git.exe" checkout e359bd8cedd411f2e5579f7b269b91835e800771
+cd ..\..\externals\SSH.NET
+"%ProgramFiles(x86)%\Git\Bin\git.exe" checkout 4f25cc00a7129bfd001c8d79a4ae3d69655751ca
+
+cd ..\..\deps\SSH.NET
 
 if NOT ERRORLEVEL 0 pause
