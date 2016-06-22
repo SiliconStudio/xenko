a5839f8beaf0c2a770ff3bc789e05f23dee4fabe
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-06-22 18:19:18 +0900

    [Xenko.Native] Audio Android OpenSLES all done except for 3D audio

--- sources/engine/SiliconStudio.Xenko.Native/OpenSLES.cpp
+++ sources/engine/SiliconStudio.Xenko.Native/OpenSLES.cpp
@@ -6,7 +6,7 @@
 #include "../../../deps/NativePath/NativePath.h"
 #include "../../../deps/NativePath/NativeDynamicLinking.h"
 #include "../../../deps/NativePath/NativeThreading.h"
-#include "../../../deps/NativePath/TINYSTL/unordered_map.h"
+#include "../../../deps/NativePath/TINYSTL/vector.h"
 #include "../../../deps/OpenSLES/OpenSLES.h"
 #include "../../../deps/OpenSLES/OpenSLES_Android.h"
 
@@ -43,6 +43,7 @@ extern "C" {
 		SLInterfaceID* SL_IID_BUFFERQUEUE_PTR = NULL;
 		SLInterfaceID* SL_IID_VOLUME_PTR = NULL;
 		SLInterfaceID* SL_IID_PLAY_PTR = NULL;
+		SLInterfaceID* SL_IID_PLAYBACKRATE_PTR = NULL;
 
 		npBool xnAudioInit()
 		{
@@ -63,6 +64,9 @@ extern "C" {
 			SL_IID_PLAY_PTR = (SLInterfaceID*)GetSymbolAddress(OpenSLESLibrary, "SL_IID_PLAY");
 			if (!SL_IID_PLAY_PTR) return false;
 
+			SL_IID_PLAYBACKRATE_PTR = (SLInterfaceID*)GetSymbolAddress(OpenSLESLibrary, "SL_IID_PLAYBACKRATE");
+			if (!SL_IID_PLAYBACKRATE_PTR) return false;
+
 			slCreateEngineFunc = (slCreateEnginePtr)GetSymbolAddress(OpenSLESLibrary, "slCreateEngine");
 			if (!slCreateEngineFunc) return false;
 
@@ -74,19 +78,17 @@ extern "C" {
 			SLObjectItf device; 
 			SLEngineItf engine;
 			SLObjectItf outputMix;
-
 		};
 
 		struct xnAudioBuffer
 		{
-			char* dataPtr;
 			int dataLength;
+			bool endOfStream;
+			char* dataPtr;
 		};
 
 		struct xnAudioListener
 		{
-			SLObjectItf object;
-			SL3DLocationItf listener;
 			xnAudioDevice* audioDevice;
 		};
 
@@ -95,12 +97,20 @@ extern "C" {
 			int sampleRate;
 			bool mono;
 			bool streamed;
+			bool looped;
+			volatile bool endOfStream;
+
 			xnAudioListener* listener;
 
 			SLObjectItf object;
 			SLPlayItf player;
 			SLAndroidSimpleBufferQueueItf queue;
 			SLVolumeItf volume;
+			SLPlaybackRateItf playRate;
+
+			tinystl::vector<xnAudioBuffer*> streamBuffers;
+			tinystl::vector<xnAudioBuffer*> freeBuffers;
+			SpinLock buffersLock;
 		};
 
 #define DEBUG_BREAK debugtrap();
@@ -183,23 +193,46 @@ extern "C" {
 		}
 
 		void PlayerCallback(SLAndroidSimpleBufferQueueItf bq, void *context) {
-//			assert(bq == bqPlayerBufferQueue);
-//			assert(NULL == context);
-//
-//			short *nextBuffer = buffer[curBuffer];
-//			int nextSize = sizeof(buffer[0]);
-//
-//			SLresult result;
-//			result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, nextBuffer, nextSize);
-//
-//			// Comment from sample code:
-//			// the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
-//			// which for this code example would indicate a programming error
-//			assert(SL_RESULT_SUCCESS == result);
-//
-//			curBuffer ^= 1;  // Switch buffer
-//							 // Render to the fresh buffer
-//			audioCallback(buffer[curBuffer], BUFFER_SIZE_IN_SAMPLES);
+			(void)bq;
+			auto source = static_cast<xnAudioSource*>(context);
+			if(!source->streamed) //looped
+			{
+				if (source->looped)
+				{
+					(*source->queue)->Enqueue(source->queue, source->streamBuffers[0]->dataPtr, source->streamBuffers[0]->dataLength);
+				}
+				else
+				{
+					(*source->player)->SetPlayState(source->player, SL_PLAYSTATE_STOPPED);
+				}
+			}
+			else
+			{
+				source->buffersLock.Lock();
+
+				//release the next buffer
+				if (!source->streamBuffers.empty())
+				{
+					auto nextBuffer = source->streamBuffers.front();
+					source->streamBuffers.erase(source->streamBuffers.begin());
+
+					if(nextBuffer->endOfStream && !source->looped)
+					{
+						(*source->player)->SetPlayState(source->player, SL_PLAYSTATE_STOPPED);
+						
+						//flush buffers
+						for(auto buffer : source->streamBuffers)
+						{
+							source->freeBuffers.push_back(buffer);
+						}
+						source->streamBuffers.clear();
+					}
+
+					source->freeBuffers.push_back(nextBuffer);
+				}
+				
+				source->buffersLock.Unlock();				
+			}
 		}
 
 		xnAudioSource* xnAudioSourceCreate(xnAudioListener* listener, int sampleRate, int maxNBuffers, npBool mono, npBool spatialized, npBool streamed)
@@ -211,6 +244,7 @@ extern "C" {
 			res->sampleRate = sampleRate;
 			res->mono = mono;
 			res->streamed = streamed;
+			res->looped = false;
 
 			SLDataFormat_PCM format;
 			format.bitsPerSample = SL_PCMSAMPLEFORMAT_FIXED_16;
@@ -219,16 +253,16 @@ extern "C" {
 			format.containerSize = 16;
 			format.formatType = SL_DATAFORMAT_PCM;
 			format.endianness = SL_BYTEORDER_LITTLEENDIAN;
-			format.channelMask = mono ? SL_SPEAKER_FRONT_LEFT : SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT;
+			format.channelMask = mono ? SL_SPEAKER_FRONT_CENTER : SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT;
 
 			SLDataLocator_AndroidSimpleBufferQueue bufferQueue = { SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, (SLuint32) maxNBuffers };
 
 			SLDataSource audioSrc = { &bufferQueue, &format };
 			SLDataLocator_OutputMix outMix = { SL_DATALOCATOR_OUTPUTMIX, listener->audioDevice->outputMix };
 			SLDataSink sink = { &outMix, NULL };
-			const SLInterfaceID ids[2] = { *SL_IID_BUFFERQUEUE_PTR, *SL_IID_VOLUME_PTR };
-			const SLboolean req[2] = { SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE };
-			SLresult result = (*listener->audioDevice->engine)->CreateAudioPlayer(listener->audioDevice->engine, &res->object, &audioSrc, &sink, 2, ids, req);
+			const SLInterfaceID ids[3] = { *SL_IID_BUFFERQUEUE_PTR,*SL_IID_PLAYBACKRATE_PTR, *SL_IID_VOLUME_PTR };
+			const SLboolean req[3] = { SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE };
+			auto result = (*listener->audioDevice->engine)->CreateAudioPlayer(listener->audioDevice->engine, &res->object, &audioSrc, &sink, 3, ids, req);
 			if (result != SL_RESULT_SUCCESS)
 			{
 				DEBUG_BREAK
@@ -252,13 +286,19 @@ extern "C" {
 				DEBUG_BREAK
 			}
 
-			result = (*res->object)->GetInterface(res->object, *SL_IID_BUFFERQUEUE_PTR, &res->queue);
+			result = (*res->object)->GetInterface(res->object, *SL_IID_VOLUME_PTR, &res->volume);
 			if (result != SL_RESULT_SUCCESS)
 			{
 				DEBUG_BREAK
 			}
 
-			result = (*res->object)->GetInterface(res->object, *SL_IID_VOLUME_PTR, &res->volume);
+			result = (*res->object)->GetInterface(res->object, *SL_IID_PLAYBACKRATE_PTR, &res->playRate);
+			if (result != SL_RESULT_SUCCESS)
+			{
+				DEBUG_BREAK
+			}
+
+			result = (*res->volume)->EnableStereoPosition(res->volume, SL_BOOLEAN_TRUE);
 			if (result != SL_RESULT_SUCCESS)
 			{
 				DEBUG_BREAK
@@ -280,38 +320,70 @@ extern "C" {
 
 		void xnAudioSourceSetPan(xnAudioSource* source, float pan)
 		{
+			(*source->volume)->SetStereoPosition(source->volume, SLpermille(pan * 1000.0f));
 		}
 
 		void xnAudioSourceSetLooping(xnAudioSource* source, npBool looping)
 		{
+			source->looped = looping;
 		}
 
 		void xnAudioSourceSetGain(xnAudioSource* source, float gain)
 		{
+			auto dbVolume = SLmillibel(log10f(gain) * 20 * 100);
+			(*source->volume)->SetVolumeLevel(source->volume, dbVolume);
 		}
 
 		void xnAudioSourceSetPitch(xnAudioSource* source, float pitch)
 		{
+			//(*source->playRate)->SetRate(source->playRate, SLpermille(pitch * 1000.0f));
 		}
 
 		void xnAudioSourceSetBuffer(xnAudioSource* source, xnAudioBuffer* buffer)
 		{
 			if (source->streamed) return;
 
+			source->buffersLock.Lock();
+
+			source->streamBuffers[0] = buffer;
 			(*source->queue)->Enqueue(source->queue, buffer->dataPtr, buffer->dataLength);
+
+			source->buffersLock.Unlock();
 		}
 
 		void xnAudioSourceQueueBuffer(xnAudioSource* source, xnAudioBuffer* buffer, short* pcm, int bufferSize, bool endOfStream)
 		{
 			if (!source->streamed) return;
 
-			(*source->queue)->Enqueue(source->queue, pcm, bufferSize);
+			buffer->endOfStream = endOfStream;
+			buffer->dataLength = bufferSize;
+			memcpy(buffer->dataPtr, pcm, bufferSize);
+
+			source->buffersLock.Lock();
+
+			source->streamBuffers.push_back(buffer);
+			(*source->queue)->Enqueue(source->queue, buffer->dataPtr, buffer->dataLength);
+
+			source->buffersLock.Unlock();
 		}
 
 		xnAudioBuffer* xnAudioSourceGetFreeBuffer(xnAudioSource* source)
 		{
 			if (!source->streamed) return NULL;
-			return NULL;
+
+			xnAudioBuffer* freeBuffer = NULL;
+
+			source->buffersLock.Lock();
+
+			if(!source->freeBuffers.empty())
+			{
+				freeBuffer = source->freeBuffers.back();
+				source->freeBuffers.pop_back();
+			}
+
+			source->buffersLock.Unlock();
+
+			return freeBuffer;
 		}
 
 		void xnAudioSourcePlay(xnAudioSource* source)
@@ -362,6 +434,7 @@ extern "C" {
 		{
 			(void)sampleRate;
 			(void)mono;
+			buffer->endOfStream = true;
 			buffer->dataLength = bufferSize;
 			memcpy(buffer->dataPtr, pcm, bufferSize);
 		}
