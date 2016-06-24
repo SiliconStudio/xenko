8323cadce16120cf7a24c6c44d39c4bb91cc7ee8
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-06-24 13:04:41 +0900

    [Assets] Add documentation and move classes related to AssetComposite in dedicated files

--- sources/assets/SiliconStudio.Assets/AssetComposite.cs
+++ sources/assets/SiliconStudio.Assets/AssetComposite.cs
@@ -4,79 +4,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using SiliconStudio.Assets.Analysis;
-using SiliconStudio.Core;
 
 namespace SiliconStudio.Assets
 {
-    [DataContract("AssetCompositeHierarchyData")]
-    public class AssetCompositeHierarchyData<TAssetPartDesign, TAssetPart>
-        where TAssetPartDesign : IAssetPartDesign<TAssetPart>
-        where TAssetPart : IIdentifiable
-    {
-        [DataMember(10)]
-        public List<Guid> RootPartIds { get; } = new List<Guid>();
-
-        [DataMember(20)]
-        public AssetPartCollection<TAssetPartDesign, TAssetPart> Parts { get; } = new AssetPartCollection<TAssetPartDesign, TAssetPart>();
-    }
-
-    public abstract class AssetCompositeHierarchy<TAssetPartDesign, TAssetPart> : AssetComposite
-        where TAssetPartDesign : IAssetPartDesign<TAssetPart>
-        where TAssetPart : class, IIdentifiable
-    {
-        public AssetCompositeHierarchyData<TAssetPartDesign, TAssetPart> Hierarchy { get; set; } = new AssetCompositeHierarchyData<TAssetPartDesign, TAssetPart>();
-
-        public abstract TAssetPart GetParent(TAssetPart part);
-
-        public abstract IEnumerable<TAssetPart> EnumerateChildParts(TAssetPart part, bool isRecursive);
-
-        public IEnumerable<TAssetPartDesign> EnumerateChildParts(TAssetPartDesign partDesign, bool isRecursive)
-        {
-            return EnumerateChildParts(partDesign.Part, isRecursive).Select(e => Hierarchy.Parts[e.Id]);
-        }
-
-        public override IEnumerable<AssetPart> CollectParts()
-        {
-            foreach (var part in Hierarchy.Parts)
-            {
-                yield return new AssetPart(part.Part.Id, part.BaseId, part.BasePartInstanceId);
-            }
-        }
-
-        public override void SetPart(Guid id, Guid baseId, Guid basePartInstanceId)
-        {
-            TAssetPartDesign partEntry;
-            if (Hierarchy.Parts.TryGetValue(id, out partEntry))
-            {
-                partEntry.BaseId = baseId;
-                partEntry.BasePartInstanceId = basePartInstanceId;
-            }
-        }
-
-        public override bool ContainsPart(Guid id)
-        {
-            return Hierarchy.Parts.ContainsKey(id);
-        }
-
-        public override void FixupPartReferences()
-        {
-            AssetCompositeAnalysis.FixupAssetPartReferences(this, ResolveReference);
-        }
-
-        protected virtual object ResolveReference(object partReference)
-        {
-            var reference = partReference as TAssetPart;
-            if (reference != null)
-            {
-                TAssetPartDesign realPart;
-                Hierarchy.Parts.TryGetValue(reference.Id, out realPart);
-                return realPart?.Part;
-            }
-            return null;
-        }
-    }
-
     /// <summary>
     /// Base class for an asset that supports inheritance by composition.
     /// </summary>
