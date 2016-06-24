809b86e885f40b452e82be10cc29bd2e33ef3ca0
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-06-24 11:51:06 +0900

    [UI] Simplified touch ray calculations for fullscreen ui component.

--- sources/engine/SiliconStudio.Xenko.UI/Rendering/UI/UIRenderFeature.Picking.cs
+++ sources/engine/SiliconStudio.Xenko.UI/Rendering/UI/UIRenderFeature.Picking.cs
@@ -17,7 +17,7 @@ namespace SiliconStudio.Xenko.Rendering.UI
 {
     public partial class UIRenderFeature 
     {
-        private Vector2 lastMousePosition;
+//        private Vector2 lastMousePosition;
 
         // object to avoid allocation at each element leave event
         private readonly HashSet<UIElement> newlySelectedElementParents = new HashSet<UIElement>();
@@ -130,36 +130,18 @@ namespace SiliconStudio.Xenko.Rendering.UI
         {
             uiRay = new Ray(new Vector3(float.NegativeInfinity), new Vector3(0, 1, 0));
 
-            if (uiComponent.IsFullScreen)
-            {
-                // TODO This math is exactly the same as before, maybe needs to be changed later
+            // TODO XK-3367 This only works for a single view
 
-                // here we use a trick to take into the calculation the viewport => we multiply the screen position by the viewport ratio (easier than modifying the view matrix)
-                var positionForHitTest = (Vector2.Demodulate(screenPosition, viewportTargetRatio) - viewportOffset) - new Vector2(0.5f);
+            // Get a touch ray in object (UI component) space
+            var touchRay = GetWorldRay(ref viewport, screenPosition, ref viewParameters.WorldViewProjectionMatrix);
 
-                // calculate the ray corresponding to the click
-                var rayDirectionView = Vector3.Normalize(new Vector3(positionForHitTest.X * viewParameters.FrustumHeight * viewParameters.AspectRatio, -positionForHitTest.Y * viewParameters.FrustumHeight, -1));
+            // If the click point is outside the canvas ignore any further testing
+            var dist = -touchRay.Position.Z / touchRay.Direction.Z;
+            if (Math.Abs(touchRay.Position.X + touchRay.Direction.X * dist) > uiComponent.Resolution.X * 0.5f ||
+                Math.Abs(touchRay.Position.Y + touchRay.Direction.Y * dist) > uiComponent.Resolution.Y * 0.5f)
+                return false;
 
-                // Flip the Y and Z axis because the UI component is reversed
-                rayDirectionView.Y *= -1;
-                rayDirectionView.Z *= -1;
-
-                uiRay = new Ray(new Vector3(0, 0, -uiComponent.Resolution.Z), rayDirectionView);
-            }
-            else
-            {
-                // TODO XK-3367 This only works for a single view
-                var touchRay = GetWorldRay(ref viewport, screenPosition, ref viewParameters.WorldViewProjectionMatrix);
-
-                // If the click point is outside the canvas ignore any testing
-                var dist = -touchRay.Position.Z / touchRay.Direction.Z;
-                if (Math.Abs(touchRay.Position.X + touchRay.Direction.X * dist) > uiComponent.Resolution.X * 0.5f ||
-                    Math.Abs(touchRay.Position.Y + touchRay.Direction.Y * dist) > uiComponent.Resolution.Y * 0.5f)
-                    return false;
-
-                uiRay = touchRay;
-            }
-
+            uiRay = touchRay;
             return true;
         }
 
@@ -273,7 +255,7 @@ namespace SiliconStudio.Xenko.Rendering.UI
             var overredElement = lastOveredElement;
 
             // determine currently overred element.
-            if (mousePosition != lastMousePosition)
+            if (mousePosition != state.LastMousePosition)
             {
                 Ray uiRay;
                 if (!GetTouchPosition(state.UIComponent, ref viewport, mousePosition, out uiRay))
@@ -312,7 +294,7 @@ namespace SiliconStudio.Xenko.Rendering.UI
 
             // update cached values
             state.LastOveredElement = overredElement;
-            lastMousePosition = mousePosition;
+            state.LastMousePosition = mousePosition;
         }
 
         private UIElement FindCommonParent(UIElement element1, UIElement element2)
