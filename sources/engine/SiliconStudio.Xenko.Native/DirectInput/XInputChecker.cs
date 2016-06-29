9085f0e6647f4def7d794f883111f8799c11497c
Manu <emmanuel.stapf@siliconstudio.co.jp>
2016-06-29 11:47:27 +0900

    [Native] Migrate IsXInputDevice to Xenko.Native instead of Core.Native.
    Refactor usage of Native code so that the PInvoke methods are defined in
    the same assembly as where the native code is. This makes is easier when
    targeting Mono as otherwise you need to define a Dllmap config files for
    all assemblies using the PInvoke.

---
+++ sources/engine/SiliconStudio.Xenko.Native/DirectInput/XInputChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SiliconStudio.Xenko.Native.DirectInput
+{
+    public static class XInputChecker
+    {
+        /// <summary>
+        /// Check if device represented by <paramref name="guid"/> is indeed an XInput device.
+        /// </summary>
+        /// <param name="guid">Guid of device to check.</param>
+        /// <returns>True if XInput device.</returns>
+        [DllImport(NativeInvoke.Library, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool IsXInputDevice(ref Guid guid);
+    }
+}
