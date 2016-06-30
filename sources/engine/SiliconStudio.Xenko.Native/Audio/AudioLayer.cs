4ce94d3b21dbe5e00827b655d70522ff08364f09
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-06-30 15:25:48 +0900

    [Xenko.Native - Audio] Added a SetMasterVolume method

--- sources/engine/SiliconStudio.Xenko.Native/Audio/AudioLayer.cs
+++ sources/engine/SiliconStudio.Xenko.Native/Audio/AudioLayer.cs
@@ -49,6 +49,10 @@ namespace SiliconStudio.Xenko.Native
         public static extern void Destroy(Device device);
 
         [SuppressUnmanagedCodeSecurity]
+        [DllImport(NativeInvoke.Library, EntryPoint = "xnAudioSetMasterVolume", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetMasterVolume(Device device, float volume);
+
+        [SuppressUnmanagedCodeSecurity]
         [DllImport(NativeInvoke.Library, EntryPoint = "xnAudioListenerCreate", CallingConvention = CallingConvention.Cdecl)]
         public static extern Listener ListenerCreate(Device device);
 
