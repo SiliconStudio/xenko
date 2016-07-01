1416f0880b8ee1d62f8e5e04b65a388836a9d100
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-07-01 11:47:10 +0900

    [Samples] Some samples updated.

--- sources/assets/SiliconStudio.Assets.CompilerApp/PackageBuilder.cs
+++ sources/assets/SiliconStudio.Assets.CompilerApp/PackageBuilder.cs
@@ -152,7 +152,7 @@ namespace SiliconStudio.Assets.CompilerApp
                 // Add specific steps to generate shaders
                 // TODO: This doesn't really belong here, where should we move it?
                 assetBuildResult.BuildSteps.Add(new WaitBuildStep());
-                assetBuildResult.BuildSteps.Add(new CompileDefaultSceneEffectCommand(context, package, assetBuildResult));
+                //assetBuildResult.BuildSteps.Add(new CompileDefaultSceneEffectCommand(context, package, assetBuildResult));
 
                 // Create the builder
                 var indexName = "index." + builderOptions.BuildProfile;
