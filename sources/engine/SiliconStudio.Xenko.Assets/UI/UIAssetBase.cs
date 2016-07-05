849a657e76f6718b3935c090dc3e956a42d3befe
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-07-05 17:36:32 +0900

    [Assets] Introduce a base class for UI assets

---
+++ sources/engine/SiliconStudio.Xenko.Assets/UI/UIAssetBase.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using SiliconStudio.Assets;
+using SiliconStudio.Core.Extensions;
+using SiliconStudio.Xenko.Assets.Entities;
+using SiliconStudio.Xenko.UI;
+
+namespace SiliconStudio.Xenko.Assets.UI
+{
+    /// <summary>
+    /// Base class for assets containing a hierarchy of <see cref="UIElement"/>.
+    /// </summary>
+    public abstract class UIAssetBase : AssetCompositeHierarchy<UIElementDesign, UIElement>
+    {
+        /// <inheritdoc/>
+        public override UIElement GetParent(UIElement part)
+        {
+            return part.Parent;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<UIElement> EnumerateChildParts(UIElement part, bool isRecursive)
+        {
+            var elementChildren = (IUIElementChildren)part;
+            var enumerator = isRecursive ? elementChildren.Children.DepthFirst(t => t.Children) : elementChildren.Children;
+            return enumerator.Cast<UIElement>();
+        }
+    }
+}
--- sources/engine/SiliconStudio.Xenko.Assets/UI/UIPageAsset.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/UI/UIPageAsset.cs
@@ -1,16 +1,12 @@
 ﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using System.Collections.Generic;
-using System.Linq;
 using SiliconStudio.Assets;
 using SiliconStudio.Assets.Compiler;
 using SiliconStudio.Assets.Serializers;
 using SiliconStudio.Core;
 using SiliconStudio.Core.Annotations;
-using SiliconStudio.Core.Extensions;
 using SiliconStudio.Core.Mathematics;
-using SiliconStudio.Xenko.Assets.Entities;
 using SiliconStudio.Xenko.Engine;
 using SiliconStudio.Xenko.UI;
 
@@ -25,7 +21,7 @@ namespace SiliconStudio.Xenko.Assets.UI
     [AssetFormatVersion(XenkoConfig.PackageName, CurrentVersion)]
     [Display("UI")]
     [AssetPartReference(typeof(UIElement))]
-    public sealed class UIPageAsset : AssetCompositeHierarchy<UIElementDesign, UIElement>
+    public sealed class UIPageAsset : UIAssetBase
     {
         private const string CurrentVersion = "0.0.0";
 
@@ -69,18 +65,6 @@ namespace SiliconStudio.Xenko.Assets.UI
 
             [DataMember]
             public float AdornerBorderThickness { get; set; } = 2.0f;
-        }
-
-        public override UIElement GetParent(UIElement element)
-        {
-            return element.Parent;
-        }
-
-        public override IEnumerable<UIElement> EnumerateChildParts(UIElement part, bool isRecursive)
-        {
-            var elementChildren = (IUIElementChildren)part;
-            var enumerator = isRecursive ? elementChildren.Children.DepthFirst(t => t.Children) : elementChildren.Children;
-            return enumerator.Cast<UIElement>();
         }
     }
 }
