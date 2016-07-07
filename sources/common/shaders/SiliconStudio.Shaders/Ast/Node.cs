969677b579f14d20455220011720e467fb8e5cdd
Virgile Bello <virgile@siliconstudio.co.jp>
2016-07-07 20:34:52 +0900

    [Shaders] Removed "using System.Runtime.Serialization;" which was causing ambiguities on Windows10

--- sources/common/shaders/SiliconStudio.Shaders/Ast/Node.cs
+++ sources/common/shaders/SiliconStudio.Shaders/Ast/Node.cs
@@ -2,7 +2,6 @@
 // This file is distributed under GPL v3. See LICENSE.md for details.
 using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Threading;
 using SiliconStudio.Core;
 using SiliconStudio.Shaders.Visitor;
