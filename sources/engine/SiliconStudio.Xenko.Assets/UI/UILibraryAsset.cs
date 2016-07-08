046314ad10490cdd6c8af6e518416e70daaa17a7
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-07-08 21:22:18 +0900

    [Editor] Introduce the concept of IUIElementFactory. Implemented for types but not yet for libraries

--- sources/engine/SiliconStudio.Xenko.Assets/UI/UILibraryAsset.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/UI/UILibraryAsset.cs
@@ -4,6 +4,7 @@ using SiliconStudio.Assets;
 using SiliconStudio.Assets.Compiler;
 using SiliconStudio.Assets.Serializers;
 using SiliconStudio.Core;
+using SiliconStudio.Xenko.Assets.Entities;
 using SiliconStudio.Xenko.UI;
 
 namespace SiliconStudio.Xenko.Assets.UI
@@ -27,6 +28,40 @@ namespace SiliconStudio.Xenko.Assets.UI
         /// Gets the dictionary of publicly exposed controls.
         /// </summary>
         [DataMember(20)]
-        public Dictionary<string, Guid> PublicUIElements { get; } = new Dictionary<string, Guid>();
+        public Dictionary<Guid, string> PublicUIElements { get; } = new Dictionary<Guid, string>();
+
+        /// <summary>
+        /// Creates a instance of the given control that can be added to another <see cref="UIAssetBase"/>.
+        /// </summary>
+        /// <param name="targetContainer">The container in which the instance will be added.</param>
+        /// <param name="targetLocation">The location of the <see paramref="targetContainer"/> asset.</param>
+        /// <returns>An <see cref="AssetCompositeHierarchyData{UIElementDesign, UIElement}"/> containing the cloned elements of </returns>
+        /// <remarks>This method will update the <see cref="Asset.BaseParts"/> property of the <see paramref="targetContainer"/>.</remarks>
+        public AssetCompositeHierarchyData<UIElementDesign, UIElement> CreateElementInstance(UIAssetBase targetContainer, string targetLocation)
+        {
+            Guid unused;
+            return CreateElementInstance(targetContainer, targetLocation, out unused);
+        }
+
+        /// <summary>
+        /// Creates a instance of the given control that can be added to another <see cref="UIAssetBase"/>.
+        /// </summary>
+        /// <param name="targetContainer">The container in which the instance will be added.</param>
+        /// <param name="targetLocation">The location of this asset.</param>
+        /// <param name="instanceId">The identifier of the created instance.</param>
+        /// <returns>An <see cref="AssetCompositeHierarchyData{UIElementDesign, UIElement}"/> containing the cloned elements of </returns>
+        /// <remarks>This method will update the <see cref="Asset.BaseParts"/> property of the <see paramref="targetContainer"/>.</remarks>
+        public AssetCompositeHierarchyData<UIElementDesign, UIElement> CreateElementInstance(UIAssetBase targetContainer, string targetLocation, out Guid instanceId)
+        {
+            var instance = (UILibraryAsset)CreateChildAsset(targetLocation);
+
+            targetContainer.AddBasePart(instance.Base);
+            instanceId = Guid.NewGuid();
+            foreach (var elementEntry in instance.Hierarchy.Parts)
+            {
+                elementEntry.BasePartInstanceId = instanceId;
+            }
+            return instance.Hierarchy;
+        }
     }
 }
