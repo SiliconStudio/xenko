e7030b9aae0b2d7048ec5d464f8dfe50643a9271
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-07-14 15:57:08 +0900

    [Core] Better in-place matrix transpose

--- sources/common/core/SiliconStudio.Core.Mathematics/Matrix.cs
+++ sources/common/core/SiliconStudio.Core.Mathematics/Matrix.cs
@@ -510,7 +510,22 @@ namespace SiliconStudio.Core.Mathematics
         /// </summary>
         public void Transpose()
         {
-            Transpose(ref this, out this);
+            float temp;
+            temp = M21; M21 = M12; M12 = temp;
+            temp = M31; M31 = M13; M13 = temp;
+            temp = M41; M41 = M14; M14 = temp;
+
+            temp = M12; M12 = M21; M21 = temp;
+            temp = M32; M32 = M23; M23 = temp;
+            temp = M42; M42 = M24; M24 = temp;
+
+            temp = M13; M13 = M31; M31 = temp;
+            temp = M23; M23 = M32; M32 = temp;
+            temp = M43; M43 = M34; M34 = temp;
+
+            temp = M14; M14 = M41; M41 = temp;
+            temp = M24; M24 = M42; M42 = temp;
+            temp = M34; M34 = M43; M43 = temp;
         }
 
         /// <summary>
@@ -1344,9 +1359,8 @@ namespace SiliconStudio.Core.Mathematics
         /// <returns>The transpose of the specified matrix.</returns>
         public static Matrix Transpose(Matrix value)
         {
-            Matrix result;
-            Transpose(ref value, out result);
-            return result;
+            value.Transpose();
+            return value;
         }
 
         /// <summary>
