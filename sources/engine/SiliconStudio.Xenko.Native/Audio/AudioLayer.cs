4c38e35c4eef45f5c0fba49bf0120513c1eac96a
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2016-07-20 16:48:35 +0900

    [Xenko.Native - Audio] Removed xnAudioSourceFlushBuffers

--- sources/engine/SiliconStudio.Xenko.Native/Audio/AudioLayer.cs
+++ sources/engine/SiliconStudio.Xenko.Native/Audio/AudioLayer.cs
@@ -115,10 +115,6 @@ namespace SiliconStudio.Xenko.Native
         [DllImport(NativeInvoke.Library, EntryPoint = "xnAudioSourceQueueBuffer", CallingConvention = CallingConvention.Cdecl)]
         public static extern void SourceQueueBuffer(Source source, Buffer buffer, IntPtr pcm, int bufferSize, BufferType streamType);
 
-//        [SuppressUnmanagedCodeSecurity]
-//        [DllImport(NativeInvoke.Library, EntryPoint = "xnAudioSourceFlushBuffers", CallingConvention = CallingConvention.Cdecl)]
-//        public static extern void SourceFlushBuffers(Source source);
-
         [SuppressUnmanagedCodeSecurity]
         [DllImport(NativeInvoke.Library, EntryPoint = "xnAudioSourceGetFreeBuffer", CallingConvention = CallingConvention.Cdecl)]
         public static extern Buffer SourceGetFreeBuffer(Source source);
