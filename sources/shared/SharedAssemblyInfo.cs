311d3a29c7b1f9c1863d1455777617e964d4c219
Virgile Bello <virgile@siliconstudio.co.jp>
2016-07-22 13:04:42 +0900

    [Build] Bump version to 1.7.6

--- sources/shared/SharedAssemblyInfo.cs
+++ sources/shared/SharedAssemblyInfo.cs
@@ -35,7 +35,7 @@ namespace SiliconStudio
         /// <summary>
         /// The .NET current assembly version as text, not including pre-release (alpha, beta...) information.
         /// </summary>
-        public const string CurrentAssemblyAsText = "1.7.5";
+        public const string CurrentAssemblyAsText = "1.7.6";
 
         /// <summary>
         /// The Store current version as text, including pre-release (alpha, beta...) information
