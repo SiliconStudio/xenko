bb1ce2e4bdae98733f5dad468540f5986ce9bb00
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-08-09 18:37:17 +0900

    [Core] Implement ConcurrentPool using ConcurrentQueue

--- sources/common/core/SiliconStudio.Core/Threading/ConcurrentPool.cs
+++ sources/common/core/SiliconStudio.Core/Threading/ConcurrentPool.cs
@@ -6,25 +6,25 @@ namespace SiliconStudio.Core.Threading
 {
     public class ConcurrentPool<T> where T : class
     {
-        private readonly ConcurrentBag<T> items;
+        private readonly ConcurrentQueue<T> items;
         private readonly Func<T> factory;
 
         public ConcurrentPool(Func<T> factory)
         {
             this.factory = factory;
-            items = new ConcurrentBag<T>();
+            items = new ConcurrentQueue<T>();
         }
 
         public ConcurrentPool(IEnumerable<T> collection, Func<T> factory)
         {
-            items = new ConcurrentBag<T>(collection);
+            items = new ConcurrentQueue<T>(collection);
             this.factory = factory;
         }
 
         public T Acquire()
         {
             T result;
-            if (!items.TryTake(out result))
+            if (!items.TryDequeue(out result))
             {
                 result = factory();
             }
@@ -33,7 +33,7 @@ namespace SiliconStudio.Core.Threading
 
         public void Release(T item)
         {
-            items.Add(item);
+            items.Enqueue(item);
         }
     }
 }
