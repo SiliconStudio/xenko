8f51c62781c1b55c386be1ea3f2203a9cf6a965d
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2016-08-29 14:04:54 +0900

    [Samples] CustomParticles added to the common particle sample project.

---
+++ samples/Particles/SimpleParticles/SimpleParticles.Game/Materials/ParticleCustomShaderKeys.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Graphics;
+using SiliconStudio.Xenko.Shaders;
+
+//namespace SiliconStudio.Xenko.Rendering
+namespace SiliconStudio.Xenko.Rendering
+{
+    public partial class ParticleCustomShaderKeys
+    {
+        public static readonly PermutationParameterKey<ShaderSource> BaseColor = ParameterKeys.NewPermutation<ShaderSource>();
+        public static readonly ObjectParameterKey<Texture> EmissiveMap = ParameterKeys.NewObject<Texture>();
+        public static readonly ValueParameterKey<Color4> EmissiveValue = ParameterKeys.NewValue<Color4>();
+
+        public static readonly PermutationParameterKey<ShaderSource> BaseIntensity = ParameterKeys.NewPermutation<ShaderSource>();
+        public static readonly ObjectParameterKey<Texture> IntensityMap = ParameterKeys.NewObject<Texture>();
+        public static readonly ValueParameterKey<float> IntensityValue = ParameterKeys.NewValue<float>();
+    }
+}
