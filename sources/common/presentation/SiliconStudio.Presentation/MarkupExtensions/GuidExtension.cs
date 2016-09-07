b16d1af30fa8779663236ff780267d502330658e
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-09-07 13:54:11 +0900

    [Presentation] Added markup extension for Guid.

---
+++ sources/common/presentation/SiliconStudio.Presentation/MarkupExtensions/GuidExtension.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
+using System.Windows.Markup;
+
+namespace SiliconStudio.Presentation.MarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(Guid))]
+    public sealed class GuidExtension : MarkupExtension
+    {
+        public Guid Value { get; set; }
+
+        public GuidExtension()
+        {
+            Value = Guid.Empty;
+        }
+
+        public GuidExtension(object value)
+        {
+            Guid guid;
+            Guid.TryParse(value as string, out guid);
+            Value = guid;
+        }
+
+        /// <inheritdoc/>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Value;
+        }
+    }
+}
