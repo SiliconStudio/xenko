7aff1e683e5d22460ae0bfc85c1b34aa5e0ed4ab
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-09-16 12:07:27 +0900

    [Assets] Fixed AssetMemberVisitorBase: logic was reversed.

--- sources/assets/SiliconStudio.Assets/Visitors/AssetMemberVisitorBase.cs
+++ sources/assets/SiliconStudio.Assets/Visitors/AssetMemberVisitorBase.cs
@@ -8,6 +8,10 @@ namespace SiliconStudio.Assets.Visitors
     {
         private readonly MemberPath memberPath;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssetMemberVisitorBase"/> class.
+        /// </summary>
+        /// <param name="path">The path to check against the current member path being visited.</param>
         protected AssetMemberVisitorBase(MemberPath path)
         {
             if (path == null) throw new ArgumentNullException(nameof(path));
@@ -18,26 +22,18 @@ namespace SiliconStudio.Assets.Visitors
         public override void VisitArrayItem(Array array, ArrayDescriptor descriptor, int index, object item, ITypeDescriptor itemDescriptor)
         {
             if (CurrentPath.Match(memberPath))
-            {
-                base.VisitArrayItem(array, descriptor, index, item, itemDescriptor);
-            }
-            else
-            {
                 VisitAssetMember(item, itemDescriptor);
-            }
+            else
+                base.VisitArrayItem(array, descriptor, index, item, itemDescriptor);
         }
 
         /// <inheritdoc/>
         public override void VisitCollectionItem(IEnumerable collection, CollectionDescriptor descriptor, int index, object item, ITypeDescriptor itemDescriptor)
         {
             if (CurrentPath.Match(memberPath))
-            {
-                base.VisitCollectionItem(collection, descriptor, index, item, itemDescriptor);
-            }
-            else
-            {
                 VisitAssetMember(item, itemDescriptor);
-            }
+            else
+                base.VisitCollectionItem(collection, descriptor, index, item, itemDescriptor);
         }
 
         /// <inheritdoc/>
@@ -45,12 +41,12 @@ namespace SiliconStudio.Assets.Visitors
         {
             if (CurrentPath.Match(memberPath))
             {
-                base.VisitDictionaryKeyValue(dictionary, descriptor, key, keyDescriptor, value, valueDescriptor);
+                Visit(key, keyDescriptor);
+                VisitAssetMember(value, valueDescriptor);
             }
             else
             {
-                Visit(key, keyDescriptor);
-                VisitAssetMember(value, valueDescriptor);
+                base.VisitDictionaryKeyValue(dictionary, descriptor, key, keyDescriptor, value, valueDescriptor);
             }
         }
 
@@ -58,28 +54,25 @@ namespace SiliconStudio.Assets.Visitors
         public override void VisitObjectMember(object container, ObjectDescriptor containerDescriptor, IMemberDescriptor member, object value)
         {
             if (CurrentPath.Match(memberPath))
-            {
-                base.VisitObjectMember(container, containerDescriptor, member, value);
-            }
-            else
-            {
                 VisitAssetMember(value, member.TypeDescriptor);
-            }
+            else
+                base.VisitObjectMember(container, containerDescriptor, member, value);
         }
 
         /// <inheritdoc/>
         public override void VisitPrimitive(object primitive, PrimitiveDescriptor descriptor)
         {
             if (CurrentPath.Match(memberPath))
-            {
-                base.VisitPrimitive(primitive, descriptor);
-            }
-            else
-            {
                 VisitAssetMember(primitive, descriptor);
-            }
+            else
+                base.VisitPrimitive(primitive, descriptor);
         }
 
+        /// <summary>
+        /// Called when <see cref="AssetVisitorBase.CurrentPath"/> matches the <see cref="MemberPath"/> given when creating this instance.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="descriptor"></param>
         protected abstract void VisitAssetMember(object value, ITypeDescriptor descriptor);
     }
 }
