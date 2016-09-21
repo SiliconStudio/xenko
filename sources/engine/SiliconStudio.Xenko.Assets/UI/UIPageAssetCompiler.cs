27a97ed877c85bd6192fb7d41f551d1563509705
Nicolas Musset <nicolas.musset@siliconstudio.co.jp>
2016-09-21 12:58:10 +0900

    [Editor] Allows UIPageRootViewModel to have no root and to add a new one in that case. (XK-3524)

--- sources/engine/SiliconStudio.Xenko.Assets/UI/UIPageAssetCompiler.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/UI/UIPageAssetCompiler.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using System.Linq;
 using SiliconStudio.BuildEngine;
 using SiliconStudio.Core;
 
@@ -23,7 +22,12 @@ namespace SiliconStudio.Xenko.Assets.UI
 
             protected override ComponentBase Create(ICommandContext commandContext)
             {
-                return new Engine.UIPage { RootElement = AssetParameters.Hierarchy.Parts[AssetParameters.Hierarchy.RootPartIds.Single()].UIElement };
+                return new Engine.UIPage
+                {
+                    RootElement = AssetParameters.Hierarchy.RootPartIds.Count == 1
+                        ? AssetParameters.Hierarchy.Parts[AssetParameters.Hierarchy.RootPartIds[0]].UIElement
+                        : null,
+                };
             }
         }
     }
