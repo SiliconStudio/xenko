1ed06dc401bee4cc5920e3d55bf72a207d3ae191
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-09-27 18:25:01 +0900

    [Engine] Fix bounding box calculation for skinned meshes. Perform blend matrix calculation on transform updates

--- sources/engine/SiliconStudio.Xenko.Engine/Rendering/RenderMesh.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Rendering/RenderMesh.cs
@@ -24,12 +24,18 @@ namespace SiliconStudio.Xenko.Rendering
         // Material
         // TODO: Extract with MaterialRenderFeature
         public Material Material;
+
         // TODO GRAPHICS REFACTOR store that in RenderData (StaticObjectNode?)
         internal MaterialRenderFeature.MaterialInfo MaterialInfo;
 
         public bool IsShadowCaster;
+
         public bool IsShadowReceiver;
 
+        public bool IsScalingNegative;
+
         public Matrix World = Matrix.Identity;
+
+        public Matrix[] BlendMatrices;
     }
 }
