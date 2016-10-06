7f39f2b5c35bb24161be289156d66ef2758b706b
Manu <manu-silicon@users.noreply.github.com>
2016-10-06 15:51:04 -0700

    Remove non-needed using clause

--- sources/assets/SiliconStudio.PackageManager/INugetLogger.cs
+++ sources/assets/SiliconStudio.PackageManager/INugetLogger.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using NuGet;
-
 namespace SiliconStudio.PackageManager
 {
     public interface INugetLogger
