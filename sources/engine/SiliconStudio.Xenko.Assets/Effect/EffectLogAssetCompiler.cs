fa4513250072dfb6bd13379385b8735ecdc98d0c
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2016-10-21 11:24:32 +0900

    [Assets] Compile EffectLogAsset from in-memory-version when dirty

--- sources/engine/SiliconStudio.Xenko.Assets/Effect/EffectLogAssetCompiler.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/Effect/EffectLogAssetCompiler.cs
@@ -3,16 +3,12 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using SiliconStudio.Assets;
 using SiliconStudio.Assets.Compiler;
 using SiliconStudio.BuildEngine;
 using SiliconStudio.Core.IO;
-using SiliconStudio.Core.Serialization;
-using SiliconStudio.Core.Serialization.Assets;
-using SiliconStudio.Xenko.Graphics;
-using SiliconStudio.Xenko.Rendering;
-using SiliconStudio.Xenko.Shaders.Compiler;
 
 namespace SiliconStudio.Xenko.Assets.Effect
 {
@@ -25,20 +21,20 @@ namespace SiliconStudio.Xenko.Assets.Effect
         {
             var originalSourcePath = asset.AbsoluteSourceLocation;
             result.ShouldWaitForPreviousBuilds = true;
-            result.BuildSteps = new AssetBuildStep(AssetItem) { new EffectLogBuildStep(context, originalSourcePath, AssetItem.Package) };
+            result.BuildSteps = new AssetBuildStep(AssetItem) { new EffectLogBuildStep(context, originalSourcePath, AssetItem) };
         }
 
         public class EffectLogBuildStep : EnumerableBuildStep
         {
             private readonly UFile originalSourcePath;
             private readonly AssetCompilerContext context;
-            private readonly Package package;
+            private readonly AssetItem assetItem;
 
-            public EffectLogBuildStep(AssetCompilerContext context, UFile originalSourcePath, Package package)
+            public EffectLogBuildStep(AssetCompilerContext context, UFile originalSourcePath, AssetItem assetItem)
             {
                 this.context = context;
                 this.originalSourcePath = originalSourcePath;
-                this.package = package;
+                this.assetItem = assetItem;
             }
 
             /// <inheritdoc/>
@@ -48,14 +44,14 @@ namespace SiliconStudio.Xenko.Assets.Effect
 
                 var urlRoot = originalSourcePath.GetParent();
 
-                var fileStream = new FileStream(originalSourcePath.ToWindowsPath(), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
-                using (var recordedEffectCompile = new EffectLogStore(fileStream))
+                var stream = new MemoryStream(Encoding.UTF8.GetBytes(((EffectLogAsset)assetItem.Asset).Text));
+                using (var recordedEffectCompile = new EffectLogStore(stream))
                 {
                     recordedEffectCompile.LoadNewValues();
 
                     foreach (var entry in recordedEffectCompile.GetValues())
                     {
-                        steps.Add(EffectCompileCommand.FromRequest(context, package, urlRoot, entry.Key));
+                        steps.Add(EffectCompileCommand.FromRequest(context, assetItem.Package, urlRoot, entry.Key));
                     }
                 }
 
@@ -67,7 +63,7 @@ namespace SiliconStudio.Xenko.Assets.Effect
             /// <inheritdoc/>
             public override BuildStep Clone()
             {
-                return new EffectLogBuildStep(context, originalSourcePath, package);
+                return new EffectLogBuildStep(context, originalSourcePath, assetItem);
             }
 
             /// <inheritdoc/>
