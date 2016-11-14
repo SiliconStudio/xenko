a8b870cd45b47e9f4e41ff22a8f1efb89fadad06
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-11-14 16:20:54 +0900

    [Assets] Fix loading of entity component collections that are in the old (pre-1.9) format

--- sources/common/core/SiliconStudio.Core.Design/Yaml/CollectionWithIdsSerializer.cs
+++ sources/common/core/SiliconStudio.Core.Design/Yaml/CollectionWithIdsSerializer.cs
@@ -37,7 +37,7 @@ namespace SiliconStudio.Core.Yaml
             if (transformed)
                 base.ReadYamlAfterTransform(ref objectContext, true);
             else
-                collectionSerializer.ReadYaml(ref objectContext);
+                GetCollectionSerializerForNonTransformedObject().ReadYaml(ref objectContext);
         }
 
         /// <inheritdoc/>
@@ -46,7 +46,12 @@ namespace SiliconStudio.Core.Yaml
             if (transformed)
                 base.WriteYamlAfterTransform(ref objectContext, true);
             else
-                collectionSerializer.WriteYaml(ref objectContext);
+                GetCollectionSerializerForNonTransformedObject().WriteYaml(ref objectContext);
+        }
+
+        protected virtual CollectionSerializer GetCollectionSerializerForNonTransformedObject()
+        {
+            return collectionSerializer;
         }
 
         /// <inheritdoc/>
