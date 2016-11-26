19097c4b619dd6b587c28348e9b623308521ce1b
Virgile Bello <virgile@siliconstudio.co.jp>
2016-11-26 21:11:57 +0900

    [Presentation] Added a TextBlockFormatting class to be able to create Run directly in value converters

---
+++ sources/common/presentation/SiliconStudio.Presentation/Controls/TextBlockFormatting.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2016 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace SiliconStudio.Presentation.Controls
+{
+    public static class TextBlockFormatting
+    {
+        public static Inline GetFormattedText(DependencyObject obj)
+        {
+            return (Inline)obj.GetValue(FormattedTextProperty);
+        }
+
+        public static void SetFormattedText(DependencyObject obj, Inline value)
+        {
+            obj.SetValue(FormattedTextProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the dependency property which permits to directly bind a inline to a <see cref="TextBox"/>.
+        /// </summary>
+        public static readonly DependencyProperty FormattedTextProperty =
+            DependencyProperty.RegisterAttached(
+                "FormattedText",
+                typeof(Inline),
+                typeof(TextBlockFormatting),
+                new PropertyMetadata(null, OnFormattedTextChanged));
+
+        private static void OnFormattedTextChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = o as TextBlock;
+            if (textBlock == null) return;
+
+            var inline = (Inline)e.NewValue;
+            if (inline == null)
+            {
+                textBlock.Inlines.Clear();
+            }
+            else
+            {
+                textBlock.Inlines.Add(inline);
+            }
+        }
+    }
+}
