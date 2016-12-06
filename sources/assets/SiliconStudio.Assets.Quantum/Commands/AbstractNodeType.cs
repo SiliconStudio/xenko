604d3978a84196a1498d55d0d76558cc01f89c7f
Virgile Bello <virgile@siliconstudio.co.jp>
2016-12-06 23:47:03 +0900

    [Editor] Allow use of shared objects
    (cherry picked from commit 016b4208c7f13fa55574310838495ce9a750c766)

---
+++ sources/assets/SiliconStudio.Assets.Quantum/Commands/AbstractNodeType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SiliconStudio.Core.Reflection;
+
+namespace SiliconStudio.Assets.Quantum.Commands
+{
+    /// <summary>
+    /// Represents a type for <see cref="CreateNewInstanceCommand"/>.
+    /// </summary>
+    public class AbstractNodeType : AbstractNodeEntry
+    {
+        public AbstractNodeType(Type type)
+        {
+            Type = type;
+        }
+
+        public Type Type { get; }
+
+        /// <inheritdoc/>
+        public override string DisplayValue => Type.GetDisplayName();
+
+        /// <inheritdoc/>
+        public override object GenerateValue(object currentValue) => ObjectFactoryRegistry.NewInstance(Type);
+
+        /// <inheritdoc/>
+        public override bool IsMatchingValue(object value) => value?.GetType() == Type;
+    }
+}
