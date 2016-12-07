fc3e63ff54d4217cee078ef83e209c0f503cb05b
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2016-12-07 14:55:23 +0900

    [Quantum] Add a test project for SiliconStudio.Presentation.Quantum

---
+++ sources/common/presentation/SiliconStudio.Presentation.Quantum.Tests/TestDependentProperties.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using SiliconStudio.Quantum;
+
+namespace SiliconStudio.Presentation.Tests
+{
+    [TestFixture]
+    public class TestDependentProperties
+    {
+        [Test]
+        public void TestSimple()
+        {
+            var nodeContainer = new NodeContainer();
+            var container = new Types.DependentPropertyContainer { Instance = new Types.SimpleObject { Name = "Test" } };
+            var containerNode = nodeContainer.GetOrCreateNode(container);
+            var test = Helper.CreateTestContext(containerNode);
+        }
+    }
+}
---
+++ sources/common/presentation/SiliconStudio.Presentation.Quantum.Tests/Types.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Presentation.Quantum;
+using SiliconStudio.Quantum;
+using SiliconStudio.Quantum.Contents;
+using SiliconStudio.Quantum.References;
+
+namespace SiliconStudio.Presentation.Tests
+{
+    public static class Types
+    {
+        public class TestPropertiesProvider : IPropertiesProviderViewModel
+        {
+            private readonly IGraphNode rootNode;
+
+            public TestPropertiesProvider(IGraphNode rootNode)
+            {
+                this.rootNode = rootNode;
+            }
+            public bool CanProvidePropertiesViewModel => true;
+
+            public IGraphNode GetRootNode()
+            {
+                return rootNode;
+            }
+
+            public bool ShouldExpandReference(MemberContent member, ObjectReference reference) => true;
+
+            public bool ShouldConstructMember(MemberContent content) => true;
+        }
+
+        public class SimpleObject
+        {
+            public string Name { get; set; }
+        }
+
+        public class DependentPropertyContainer
+        {
+            public string Title => Instance.Name;
+
+            public SimpleObject Instance { get; set; }
+        }
+    }
+}
