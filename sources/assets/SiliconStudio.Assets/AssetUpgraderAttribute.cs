273f6b7bad6f0aa413a2522cdbda0f61e63a2426
Nicolas Musset <nicolas.musset@xenko.com>
2017-01-06 18:34:03 +0900

    [Assets] Apply BaseTypeRequired attribute on some attributes.

--- sources/assets/SiliconStudio.Assets/AssetUpgraderAttribute.cs
+++ sources/assets/SiliconStudio.Assets/AssetUpgraderAttribute.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 using System;
+using SiliconStudio.Core.Annotations;
 
 namespace SiliconStudio.Assets
 {
@@ -8,6 +9,7 @@ namespace SiliconStudio.Assets
     /// Describes which upgrader type to use to upgrade an asset, depending on this current version number.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    [BaseTypeRequired(typeof(Asset))]
     public class AssetUpgraderAttribute : Attribute
     {
         /// <summary>
