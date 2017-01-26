8df1f0eb4cdabb5af19b6de440ba23a1924fef89
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2017-01-26 18:09:02 +0900

    [Samples] Samples updated for the new animation api.

---
+++ samples/Particles/ParticlesSample/ParticlesSample.Game/AnimationStart.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Animations;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine;
+
+namespace ParticlesSample
+{
+    [DataContract("PlayAnimation")]
+    public class PlayAnimation
+    {
+        public AnimationClip Clip;
+        public AnimationBlendOperation BlendOperation = AnimationBlendOperation.LinearBlend;
+        public double StartTime = 0;
+    }
+    
+    /// <summary>
+    /// Script which starts a few playing animations on a target and halts
+    /// </summary>
+    public class AnimationStart : StartupScript
+    {
+        /// <summary>
+        /// If <c>null</c>, will target the same entity it is attached to.
+        /// </summary>
+        public AnimationComponent Target { get; set; }
+
+        /// <summary>
+        /// Al list of animations to be loaded when the script starts
+        /// </summary>
+        public readonly List<PlayAnimation> Animations = new List<PlayAnimation>();
+
+        public override void Start()
+        {
+            var animComponent = Target ?? Entity.Get<AnimationComponent>();
+        
+            if (animComponent != null)
+                PlayAnimations(animComponent);
+
+            // Destroy this script since it's no longer needed
+            Entity.Remove(this);
+        }
+
+        private void PlayAnimations(AnimationComponent animComponent)
+        {
+            foreach (var anim in Animations)
+            {
+                if (anim.Clip != null)
+                    animComponent.Add(anim.Clip, anim.StartTime, anim.BlendOperation);
+            }
+
+            Animations.Clear();
+        }
+    }
+}
