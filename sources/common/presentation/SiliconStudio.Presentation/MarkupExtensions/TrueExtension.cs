4a9426a545eed6999ffd7cabb7b391408897cb45
Nicolas Musset <nicolas.musset@xenko.com>
2017-02-22 19:01:52 +0900

    [Presentation] Use known boxed boolean in markup extensions and value converters.

--- sources/common/presentation/SiliconStudio.Presentation/MarkupExtensions/TrueExtension.cs
+++ sources/common/presentation/SiliconStudio.Presentation/MarkupExtensions/TrueExtension.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+﻿// Copyright (c) 2014-2017 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
 using System;
 using System.Windows.Markup;
+using SiliconStudio.Presentation.Internal;
 
 namespace SiliconStudio.Presentation.MarkupExtensions
 {
@@ -10,7 +12,7 @@ namespace SiliconStudio.Presentation.MarkupExtensions
     {
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return true;
+            return KnownBoxes.TrueBox;
         }
     }
 }
