c3aa7ca5e3906abf8c0598c562792616b643bd4c
dfkeenan <dfkeenan@gmail.com>
2017-02-25 16:52:09 +1000

    Added Get component(s) in children/parents helper methods.

--- sources/engine/SiliconStudio.Xenko.Engine/Engine/EntityExtensions.cs
+++ sources/engine/SiliconStudio.Xenko.Engine/Engine/EntityExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
+using System;
 using System.Collections.Generic;
 using SiliconStudio.Xenko.Engine.Design;
 
@@ -79,6 +80,145 @@ namespace SiliconStudio.Xenko.Engine
         public static void EnableAll(this Entity entity, bool enabled = true, bool applyOnChildren = false)
         {
             Enable<ActivableEntityComponent>(entity, enabled, applyOnChildren);
+        }
+
+        /// <summary>
+        /// Performs a breadth first search of the entity and it's children for a component of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of component.</typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="includeDisabled">Should search include <see cref="ActivableEntityComponent"/> where <see cref="ActivableEntityComponent.Enabled"/> is <c>false</c>.</param>
+        /// <returns>The component or null if does no exist.</returns>
+        /// <exception cref="ArgumentNullException">The entity was <c>null</c>.</exception>
+        public static T GetComponentInChildren<T>(this Entity entity, bool includeDisabled = false) where T : EntityComponent
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            //breadth first
+            var queue = new Queue<Entity>();
+            queue.Enqueue(entity);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+
+                var component = current.Get<T>();
+
+                var isEnabled = ((component as ActivableEntityComponent)?.Enabled).GetValueOrDefault(true);
+                if (component != null && (isEnabled || includeDisabled))
+                {
+                    return component;
+                }
+
+                var children = current.Transform.Children;
+
+                for (int i = 0; i < children.Count; i++)
+                {
+                    queue.Enqueue(children[i].Entity);
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Performs a depth first search of the entity and it's children for all components of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of component.</typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="includeDisabled">Should search include <see cref="ActivableEntityComponent"/> where <see cref="ActivableEntityComponent.Enabled"/> is <c>false</c>.</param>
+        /// <returns>An iteration on the components.</returns>
+        /// <exception cref="ArgumentNullException">The entity was <c>null</c>.</exception>
+        public static IEnumerable<T> GetComponentsInChildren<T>(this Entity entity, bool includeDisabled = false) where T : EntityComponent
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            //depth first
+            var stack = new Stack<Entity>();
+            stack.Push(entity);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                foreach (var component in current.GetAll<T>())
+                {
+                    var isEnabled = ((component as ActivableEntityComponent)?.Enabled).GetValueOrDefault(true);
+                    if (component != null && (isEnabled || includeDisabled))
+                    {
+                        yield return component;
+                    }
+                }
+
+                var children = current.Transform.Children;
+
+                for (int i = 0; i < children.Count; i++)
+                {
+                    stack.Push(children[i].Entity);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs a search of the entity and it's ancestors for a component of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of component.</typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="includeDisabled">Should search include <see cref="ActivableEntityComponent"/> where <see cref="ActivableEntityComponent.Enabled"/> is <c>false</c>.</param>
+        /// <returns>The component or <c>null</c> if does no exist.</returns>
+        /// <exception cref="ArgumentNullException">The entity was <c>null</c>.</exception>
+        public static T GetComponentInParent<T>(this Entity entity, bool includeDisabled = false) where T : EntityComponent
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            var current = entity;
+
+            do
+            {
+                var component = current.Get<T>();
+
+                var isEnabled = ((component as ActivableEntityComponent)?.Enabled).GetValueOrDefault(true);
+                if (component != null && (isEnabled || includeDisabled))
+                {
+                    return component;
+                }
+
+            } while ((current = current.GetParent()) != null);
+
+            return null;
+        }
+
+        /// <summary>
+        /// Performs a search of the entity and it's ancestors for all components of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of component.</typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="includeDisabled">Should search include <see cref="ActivableEntityComponent"/> where <see cref="ActivableEntityComponent.Enabled"/> is <c>false</c>.</param>
+        /// <returns>An iteration on the components.</returns>
+        /// <exception cref="ArgumentNullException">The entity was <c>null</c>.</exception>
+        public static IEnumerable<T> GetComponentsInParent<T>(this Entity entity, bool includeDisabled = false) where T : EntityComponent
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            var current = entity;
+
+            do
+            {
+                foreach (var component in current.GetAll<T>())
+                {
+                    var isEnabled = ((component as ActivableEntityComponent)?.Enabled).GetValueOrDefault(true);
+                    if (component != null && (isEnabled || includeDisabled))
+                    {
+                        yield return component;
+                    }
+                }
+
+
+            } while ((current = current.GetParent()) != null);
         }
     }
 }
