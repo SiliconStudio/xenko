0b2ff636bf99e75e99e57eed115ff9cbb037a6bb
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2017-03-13 20:15:03 +0900

    [Build] Bump to version 1.10.1

--- sources/shared/SharedAssemblyInfo.cs
+++ sources/shared/SharedAssemblyInfo.cs
@@ -35,7 +35,7 @@ namespace SiliconStudio
         /// <summary>
         /// The .NET current assembly version as text, not including pre-release (alpha, beta...) information.
         /// </summary>
-        public const string CurrentAssemblyAsText = "1.10.0";
+        public const string CurrentAssemblyAsText = "1.10.1";
 
         /// <summary>
         /// The Store current version as text, including pre-release (alpha, beta...) information
