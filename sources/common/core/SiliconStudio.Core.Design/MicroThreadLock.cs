01d72b15dedf4e7da9ed801009d5d2f97470c357
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2017-03-14 17:12:46 +0900

    [Core.Design] Rework the MicroThreadLock to ensure it doesn't fail in some edge cases.

--- sources/common/core/SiliconStudio.Core.Design/MicroThreadLock.cs
+++ sources/common/core/SiliconStudio.Core.Design/MicroThreadLock.cs
@@ -13,9 +13,10 @@ namespace SiliconStudio.Core
     public class MicroThreadLock : IDisposable
     {
         private readonly MicroThreadLocal<MicroThreadAsyncLock> asyncLocks = new MicroThreadLocal<MicroThreadAsyncLock>();
-        private readonly ThreadLocal<MicroThreadSyncLock> syncLocks = new ThreadLocal<MicroThreadSyncLock>();
         private readonly Queue<MicroThreadLockBase> lockQueue = new Queue<MicroThreadLockBase>();
         private readonly object syncLock = new object();
+        private int currentSyncLockThread;
+        private MicroThreadSyncLock currentSyncLock;
         private bool isDisposed;
 
         /// <inheritdoc/>
@@ -26,14 +27,28 @@ namespace SiliconStudio.Core
         }
 
         /// <summary>
-        /// Acquires a synchronous lock. The lock will use a standard <see cref="Monitor"/> and therefore should not be used for asynchronous work.
+        /// Reserves the lock in order to use synchronous locking. The lock will be bound to the calling thread and therefore should be released on the same thread.
         /// </summary>
-        /// <returns>A task that completes when the lock is acquired.</returns>
-        /// <remarks>This way of acquiring the lock is only valid when not in a <see cref="MicroThread"/>.</remarks>
-        public Task<IDisposable> LockSync()
+        /// <returns>A task that completes when the lock is reserved. The result of the task is an <see cref="ISyncLockable"/> object allowing to do the lock.</returns>
+        public async Task<ISyncLockable> ReserveSyncLock()
         {
-            if (Scheduler.CurrentMicroThread != null) throw new InvalidOperationException($"Synchronous lock cannot be acquired from a micro-thread. Use {nameof(LockAsync)}.");
-            return Lock();
+            if (isDisposed) throw new ObjectDisposedException(nameof(MicroThreadLock));
+            
+            // If we already acquired the lock in this thread, we're just re-entering
+            if (currentSyncLockThread == Thread.CurrentThread.ManagedThreadId)
+            {
+                var currentLock = currentSyncLock;
+                // Reentering will happen in the Lock() method
+                return currentLock;
+            }
+
+            // Select the proper type of lock depending on whether we're in a micro-thread or not.
+            var newLock =  new MicroThreadSyncLock(this);
+            AcquireOrEnqueue(newLock);
+            await newLock.Acquired;
+
+            // In the case of sync, we need to register in the proper thread, so call to Register() is defered to later.
+            return newLock;
         }
 
         /// <summary>
@@ -41,57 +56,42 @@ namespace SiliconStudio.Core
         /// </summary>
         /// <returns>A task that completes when the lock is acquired.</returns>
         /// <remarks>This way of acquiring the lock is only valid when in a <see cref="MicroThread"/>.</remarks>
-        public Task<IDisposable> LockAsync()
+        public async Task<IDisposable> LockAsync()
         {
-            if (Scheduler.CurrentMicroThread == null) throw new InvalidOperationException($"Aynchronous lock can only be acquired from a micro-thread. Use {nameof(LockSync)}.");
-            return Lock();
-        }
-
-        private async Task<IDisposable> Lock()
-        {
+            if (Scheduler.CurrentMicroThread == null) throw new InvalidOperationException($"Aynchronous lock can only be acquired from a micro-thread. Use {nameof(ReserveSyncLock)}.");
             if (isDisposed) throw new ObjectDisposedException(nameof(MicroThreadLock));
 
-            // Two cases here:
-            // 1) We're in a micro-thread, we lock the database "asynchronously" using a MicroThreadLocal storage to handle re-entrancy, allowing awaits during the lock.
-            // 2) We're not in a micro-thread, we lock the database "synchronously" using a ThreadLocal storage to handle re-entrancy and a standard Monitor.
-            var inMicroThread = Scheduler.CurrentMicroThread != null;
-
-            if (inMicroThread)
+            // If we already acquired the lock in this micro-thread, we're just re-entering
+            if (asyncLocks.IsValueCreated && asyncLocks.Value != null)
             {
-                // If we already acquired the lock in this micro-thread, we're just re-entering
-                if (asyncLocks.IsValueCreated && asyncLocks.Value != null)
-                {
-                    var currentLock = asyncLocks.Value;
-                    currentLock.Reenter();
-                    return currentLock;
-                }
-            }
-            else
-            {
-                // If we already acquired the lock in this thread, we're just re-entering
-                if (syncLocks.IsValueCreated && syncLocks.Value != null)
-                {
-                    var currentLock = syncLocks.Value;
-                    currentLock.Reenter();
-                    return currentLock;
-                }
+                var currentLock = asyncLocks.Value;
+                currentLock.Reenter();
+                return currentLock;
             }
 
             // Select the proper type of lock depending on whether we're in a micro-thread or not.
-            var newLock = inMicroThread ? (MicroThreadLockBase)new MicroThreadAsyncLock(this) : new MicroThreadSyncLock(this);
+            var newLock = new MicroThreadAsyncLock(this);
+            AcquireOrEnqueue(newLock);
+            await newLock.Acquired;
+
+            // In the case of async we can register immediately after acquiring. For sync, we need to register in the proper thread.
+            newLock.Register();
+
+            return newLock;
+        }
+
+        private void AcquireOrEnqueue(MicroThreadLockBase lockToAcquire)
+        {
             lock (lockQueue)
             {
                 if (lockQueue.Count == 0)
                 {
                     // Nothing else is in the queue, let's acquire immediately.
-                    newLock.Acquire();
+                    lockToAcquire.Acquire();
                 }
                 // Let's enqueue this new lock so it can be notified by the previous lock when it can be acquired.
-                lockQueue.Enqueue(newLock);
+                lockQueue.Enqueue(lockToAcquire);
             }
-            await newLock.Acquired;
-            newLock.Enter();
-            return newLock;
         }
 
         private abstract class MicroThreadLockBase : IDisposable
@@ -116,7 +116,7 @@ namespace SiliconStudio.Core
                 --reentrancy;
                 if (reentrancy == 0)
                 {
-                    Unregister();
+                    Release();
                     lock (MicroThreadLock.lockQueue)
                     {
                         // Remove ourself from the queue.
@@ -134,14 +134,9 @@ namespace SiliconStudio.Core
 
             internal void Acquire()
             {
-                acquisition.SetResult(0);
-            }
-
-            internal virtual void Enter()
-            {
                 if (reentrancy != 0) throw new InvalidOperationException("Trying to enter a lock that has already been entered");
                 ++reentrancy;
-                Register();
+                acquisition.SetResult(0);
             }
 
             internal virtual void Reenter()
@@ -150,8 +145,7 @@ namespace SiliconStudio.Core
                 ++reentrancy;
             }
 
-            internal abstract void Register();
-            internal abstract void Unregister();
+            internal abstract void Release();
         }
 
         private class MicroThreadAsyncLock : MicroThreadLockBase
@@ -161,20 +155,21 @@ namespace SiliconStudio.Core
             {
             }
 
-
-            internal override void Register()
+            internal void Register()
             {
                 MicroThreadLock.asyncLocks.Value = this;
             }
 
-            internal override void Unregister()
+            internal override void Release()
             {
                 MicroThreadLock.asyncLocks.Value = null;
             }
         }
 
-        private class MicroThreadSyncLock : MicroThreadLockBase
+        private class MicroThreadSyncLock : MicroThreadLockBase, ISyncLockable
         {
+            private bool locked;
+
             public MicroThreadSyncLock(MicroThreadLock microThreadLock)
                 : base(microThreadLock)
             {
@@ -186,25 +181,44 @@ namespace SiliconStudio.Core
                 base.Dispose();
             }
 
-            internal override void Enter()
-            {
-                Monitor.Enter(MicroThreadLock.syncLock);
-                base.Enter();
-            }
             internal override void Reenter()
             {
                 Monitor.Enter(MicroThreadLock.syncLock);
                 base.Reenter();
             }
 
-            internal override void Register()
+            internal void Take()
             {
-                MicroThreadLock.syncLocks.Value = this;
+                if (MicroThreadLock.currentSyncLockThread != 0)
+                    throw new InvalidOperationException("Trying to lock while another thread owns the lock.");
+
+                MicroThreadLock.currentSyncLockThread = Thread.CurrentThread.ManagedThreadId;
+                MicroThreadLock.currentSyncLock = this;
             }
 
-            internal override void Unregister()
+            internal override void Release()
             {
-                MicroThreadLock.syncLocks.Value = null;
+                if (MicroThreadLock.currentSyncLockThread != Thread.CurrentThread.ManagedThreadId)
+                    throw new InvalidOperationException("Trying to unlock while another thread owns the lock.");
+
+                MicroThreadLock.currentSyncLockThread = 0;
+                MicroThreadLock.currentSyncLock = null;
+            }
+
+            public IDisposable Lock()
+            {
+                if (!locked)
+                {
+                    // We register here because we are in the proper thread.
+                    Take();
+                    Monitor.Enter(MicroThreadLock.syncLock);
+                }
+                else
+                {
+                    Reenter();
+                }
+                locked = true;
+                return this;
             }
         }
     }
