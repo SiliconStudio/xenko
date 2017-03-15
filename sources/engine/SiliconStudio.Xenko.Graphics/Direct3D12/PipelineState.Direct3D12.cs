0ac2e7b30031af8f17d6bc4bf6109382eceae74e
Wojciech Figat <g-wc-figat@gametools.jp>
2017-03-15 18:09:11 +0900

    [Graphics][WIP] DirectX 12 pipeline state for compute

--- sources/engine/SiliconStudio.Xenko.Graphics/Direct3D12/PipelineState.Direct3D12.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics/Direct3D12/PipelineState.Direct3D12.cs
@@ -39,11 +39,9 @@ namespace SiliconStudio.Xenko.Graphics
 
                     // TODO D3D12 for now, we don't control register so we simply generate one resource table per shader stage and per descriptor set layout
                     //            we should switch to a model where we make sure VS/PS don't overlap for common descriptors so that they can be shared
-                    var srvDescriptorRangesVS = new List<DescriptorRange>();
-                    var srvDescriptorRangesPS = new List<DescriptorRange>();
-                    var samplerDescriptorRangesVS = new List<DescriptorRange>();
-                    var samplerDescriptorRangesPS = new List<DescriptorRange>();
-
+                    var srvDescriptorRanges = new Dictionary<ShaderStage, List<DescriptorRange>>();
+                    var samplerDescriptorRanges = new Dictionary<ShaderStage, List<DescriptorRange>>();
+                    
                     int descriptorSrvOffset = 0;
                     int descriptorSamplerOffset = 0;
                     foreach (var item in layout.Layout.Entries)
@@ -57,16 +55,12 @@ namespace SiliconStudio.Xenko.Graphics
                                 continue;
 
                             List<DescriptorRange> descriptorRanges;
-                            switch (binding.Stage)
                             {
-                                case ShaderStage.Vertex:
-                                    descriptorRanges = isSampler ? samplerDescriptorRangesVS : srvDescriptorRangesVS;
-                                    break;
-                                case ShaderStage.Pixel:
-                                    descriptorRanges = isSampler ? samplerDescriptorRangesPS : srvDescriptorRangesPS;
-                                    break;
-                                default:
-                                    throw new NotImplementedException();
+                                var dictionary = isSampler ? samplerDescriptorRanges : srvDescriptorRanges;
+                                if (dictionary.TryGetValue(binding.Stage, out descriptorRanges) == false)
+                                {
+                                    descriptorRanges = dictionary[binding.Stage] = new List<DescriptorRange>();
+                                }
                             }
 
                             if (isSampler)
@@ -128,24 +122,15 @@ namespace SiliconStudio.Xenko.Graphics
                             descriptorSrvOffset += item.ArraySize;
                         }
                     }
-                    if (srvDescriptorRangesVS.Count > 0)
+
+                    foreach (var stage in srvDescriptorRanges)
                     {
-                        rootSignatureParameters.Add(new RootParameter(ShaderVisibility.Vertex, srvDescriptorRangesVS.ToArray()));
+                        rootSignatureParameters.Add(new RootParameter(ShaderStage2ShaderVisibility(stage.Key), stage.Value.ToArray()));
                         SrvBindCounts[layoutIndex]++;
                     }
-                    if (srvDescriptorRangesPS.Count > 0)
+                    foreach (var stage in samplerDescriptorRanges)
                     {
-                        rootSignatureParameters.Add(new RootParameter(ShaderVisibility.Pixel, srvDescriptorRangesPS.ToArray()));
-                        SrvBindCounts[layoutIndex]++;
-                    }
-                    if (samplerDescriptorRangesVS.Count > 0)
-                    {
-                        rootSignatureParameters.Add(new RootParameter(ShaderVisibility.Vertex, samplerDescriptorRangesVS.ToArray()));
-                        SamplerBindCounts[layoutIndex]++;
-                    }
-                    if (samplerDescriptorRangesPS.Count > 0)
-                    {
-                        rootSignatureParameters.Add(new RootParameter(ShaderVisibility.Pixel, samplerDescriptorRangesPS.ToArray()));
+                        rootSignatureParameters.Add(new RootParameter(ShaderStage2ShaderVisibility(stage.Key), stage.Value.ToArray()));
                         SamplerBindCounts[layoutIndex]++;
                     }
                 }
@@ -153,27 +138,32 @@ namespace SiliconStudio.Xenko.Graphics
 
                 var rootSignature = NativeDevice.CreateRootSignature(0, rootSignatureDesc.Serialize());
 
-                var inputElements = new InputElement[pipelineStateDescription.InputElements.Length];
-                for (int i = 0; i < inputElements.Length; ++i)
+                InputElement[] inputElements = null;
+                if (pipelineStateDescription.InputElements != null)
                 {
-                    var inputElement = pipelineStateDescription.InputElements[i];
-                    inputElements[i] = new InputElement
+                    inputElements = new InputElement[pipelineStateDescription.InputElements.Length];
+                    for (int i = 0; i < inputElements.Length; ++i)
                     {
-                        Format = (SharpDX.DXGI.Format)inputElement.Format,
-                        AlignedByteOffset = inputElement.AlignedByteOffset,
-                        SemanticName = inputElement.SemanticName,
-                        SemanticIndex = inputElement.SemanticIndex,
-                        Slot = inputElement.InputSlot,
-                        Classification = (SharpDX.Direct3D12.InputClassification)inputElement.InputSlotClass,
-                        InstanceDataStepRate = inputElement.InstanceDataStepRate,
-                    };
+                        var inputElement = pipelineStateDescription.InputElements[i];
+                        inputElements[i] = new InputElement
+                        {
+                            Format = (SharpDX.DXGI.Format)inputElement.Format,
+                            AlignedByteOffset = inputElement.AlignedByteOffset,
+                            SemanticName = inputElement.SemanticName,
+                            SemanticIndex = inputElement.SemanticIndex,
+                            Slot = inputElement.InputSlot,
+                            Classification = (SharpDX.Direct3D12.InputClassification)inputElement.InputSlotClass,
+                            InstanceDataStepRate = inputElement.InstanceDataStepRate,
+                        };
+                    }
                 }
 
                 PrimitiveTopologyType primitiveTopologyType;
                 switch (pipelineStateDescription.PrimitiveType)
                 {
                     case PrimitiveType.Undefined:
-                        throw new ArgumentOutOfRangeException();
+                        primitiveTopologyType = PrimitiveTopologyType.Undefined;
+                        break;
                     case PrimitiveType.PointList:
                         primitiveTopologyType = PrimitiveTopologyType.Point;
                         break;
@@ -197,54 +187,71 @@ namespace SiliconStudio.Xenko.Graphics
                         break;
                 }
 
-                var nativePipelineStateDescription = new GraphicsPipelineStateDescription
+                // Check if it should use compute pipeline state
+                var computeShader = pipelineStateDescription.EffectBytecode.Stages.FirstOrDefault(e => e.Stage == ShaderStage.Compute);
+                if (computeShader != null)
                 {
-                    InputLayout = new InputLayoutDescription(inputElements),
-                    RootSignature = rootSignature,
-                    RasterizerState = CreateRasterizerState(pipelineStateDescription.RasterizerState),
-                    BlendState = CreateBlendState(pipelineStateDescription.BlendState),
-                    SampleMask = (int)pipelineStateDescription.SampleMask,
-                    DepthStencilFormat = (SharpDX.DXGI.Format)pipelineStateDescription.Output.DepthStencilFormat,
-                    DepthStencilState = CreateDepthStencilState(pipelineStateDescription.DepthStencilState),
-                    RenderTargetCount = pipelineStateDescription.Output.RenderTargetCount,
-                    // TODO D3D12 hardcoded
-                    StreamOutput = new StreamOutputDescription(),
-                    PrimitiveTopologyType = primitiveTopologyType,
-                    // TODO D3D12 hardcoded
-                    SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
-                };
-
-                fixed (PixelFormat* renderTargetFormats = &pipelineStateDescription.Output.RenderTargetFormat0)
-                {
-                    for (int i = 0; i < pipelineStateDescription.Output.RenderTargetCount; ++i)
-                        nativePipelineStateDescription.RenderTargetFormats[i] = (SharpDX.DXGI.Format)renderTargetFormats[i];
-                }
-
-                foreach (var stage in pipelineStateDescription.EffectBytecode.Stages)
-                {
-                    switch (stage.Stage)
+                    var nativePipelineStateDescription = new ComputePipelineStateDescription
                     {
-                        case ShaderStage.Vertex:
-                            nativePipelineStateDescription.VertexShader = stage.Data;
-                            break;
-                        case ShaderStage.Hull:
-                            nativePipelineStateDescription.HullShader = stage.Data;
-                            break;
-                        case ShaderStage.Domain:
-                            nativePipelineStateDescription.DomainShader = stage.Data;
-                            break;
-                        case ShaderStage.Geometry:
-                            nativePipelineStateDescription.GeometryShader = stage.Data;
-                            break;
-                        case ShaderStage.Pixel:
-                            nativePipelineStateDescription.PixelShader = stage.Data;
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
+                        ComputeShader = computeShader.Data,
+                        Flags =  PipelineStateFlags.None,
+                        RootSignature = rootSignature,
+                    };
+
+                    CompiledState = NativeDevice.CreateComputePipelineState(nativePipelineStateDescription);
+                }
+                else
+                {
+                    var nativePipelineStateDescription = new GraphicsPipelineStateDescription
+                    {
+                        InputLayout = new InputLayoutDescription(inputElements),
+                        RootSignature = rootSignature,
+                        RasterizerState = CreateRasterizerState(pipelineStateDescription.RasterizerState),
+                        BlendState = CreateBlendState(pipelineStateDescription.BlendState),
+                        SampleMask = (int)pipelineStateDescription.SampleMask,
+                        DepthStencilFormat = (SharpDX.DXGI.Format)pipelineStateDescription.Output.DepthStencilFormat,
+                        DepthStencilState = CreateDepthStencilState(pipelineStateDescription.DepthStencilState),
+                        RenderTargetCount = pipelineStateDescription.Output.RenderTargetCount,
+                        // TODO D3D12 hardcoded
+                        StreamOutput = new StreamOutputDescription(),
+                        PrimitiveTopologyType = primitiveTopologyType,
+                        // TODO D3D12 hardcoded
+                        SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
+                    };
+
+                    fixed (PixelFormat* renderTargetFormats = &pipelineStateDescription.Output.RenderTargetFormat0)
+                    {
+                        for (int i = 0; i < pipelineStateDescription.Output.RenderTargetCount; ++i)
+                            nativePipelineStateDescription.RenderTargetFormats[i] = (SharpDX.DXGI.Format)renderTargetFormats[i];
                     }
+
+                    foreach (var stage in pipelineStateDescription.EffectBytecode.Stages)
+                    {
+                        switch (stage.Stage)
+                        {
+                            case ShaderStage.Vertex:
+                                nativePipelineStateDescription.VertexShader = stage.Data;
+                                break;
+                            case ShaderStage.Hull:
+                                nativePipelineStateDescription.HullShader = stage.Data;
+                                break;
+                            case ShaderStage.Domain:
+                                nativePipelineStateDescription.DomainShader = stage.Data;
+                                break;
+                            case ShaderStage.Geometry:
+                                nativePipelineStateDescription.GeometryShader = stage.Data;
+                                break;
+                            case ShaderStage.Pixel:
+                                nativePipelineStateDescription.PixelShader = stage.Data;
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+
+                    CompiledState = NativeDevice.CreateGraphicsPipelineState(nativePipelineStateDescription);
                 }
 
-                CompiledState = NativeDevice.CreateGraphicsPipelineState(nativePipelineStateDescription);
                 RootSignature = rootSignature;
                 PrimitiveTopology = (PrimitiveTopology)pipelineStateDescription.PrimitiveType;
                 HasScissorEnabled = pipelineStateDescription.RasterizerState.ScissorTestEnable;
@@ -329,6 +336,21 @@ namespace SiliconStudio.Xenko.Graphics
             nativeDescription.BackFace.Comparison = (SharpDX.Direct3D12.Comparison)description.BackFace.StencilFunction;
 
             return nativeDescription;
+        }
+
+        private static ShaderVisibility ShaderStage2ShaderVisibility(ShaderStage stage)
+        {
+            switch (stage)
+            {
+                case ShaderStage.Vertex: return ShaderVisibility.Vertex;
+                case ShaderStage.Hull: return ShaderVisibility.Hull;
+                case ShaderStage.Domain: return ShaderVisibility.Domain;
+                case ShaderStage.Geometry: return ShaderVisibility.Geometry;
+                case ShaderStage.Pixel: return ShaderVisibility.Pixel;
+                case ShaderStage.Compute: return ShaderVisibility.All;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stage), stage, null);
+            }
         }
     }
 }
