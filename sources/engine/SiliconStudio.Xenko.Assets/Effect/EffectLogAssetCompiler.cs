0571eb15676180d93f207fa30f2afa4f8f5f15e7
Giovanni <giovanni.petrantoni@siliconstudio.co.jp>
2017-03-21 20:39:51 +0900

    [Assets] Fixed EffectLogAssetCompiler prepare, removed the not needed build step

--- sources/engine/SiliconStudio.Xenko.Assets/Effect/EffectLogAssetCompiler.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/Effect/EffectLogAssetCompiler.cs
@@ -21,58 +21,21 @@ namespace SiliconStudio.Xenko.Assets.Effect
     {
         protected override void Prepare(AssetCompilerContext context, AssetItem assetItem, string targetUrlInStorage, AssetCompilerResult result)
         {
-            var asset = (EffectLogAsset)assetItem.Asset;
             var originalSourcePath = assetItem.FullPath;
             result.ShouldWaitForPreviousBuilds = true;
-            result.BuildSteps = new AssetBuildStep(assetItem) { new EffectLogBuildStep(context, originalSourcePath, assetItem) };
-        }
+            result.BuildSteps = new AssetBuildStep(assetItem);
 
-        public class EffectLogBuildStep : EnumerableBuildStep
-        {
-            private readonly UFile originalSourcePath;
-            private readonly AssetCompilerContext context;
-            private readonly AssetItem assetItem;
+            var urlRoot = originalSourcePath.GetParent();
 
-            public EffectLogBuildStep(AssetCompilerContext context, UFile originalSourcePath, AssetItem assetItem)
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(((EffectLogAsset)assetItem.Asset).Text));
+            using (var recordedEffectCompile = new EffectLogStore(stream))
             {
-                this.context = context;
-                this.originalSourcePath = originalSourcePath;
-                this.assetItem = assetItem;
-            }
+                recordedEffectCompile.LoadNewValues();
 
-            /// <inheritdoc/>
-            public override Task<ResultStatus> Execute(IExecuteContext executeContext, BuilderContext builderContext)
-            {
-                var steps = new List<BuildStep>();
-
-                var urlRoot = originalSourcePath.GetParent();
-
-                var stream = new MemoryStream(Encoding.UTF8.GetBytes(((EffectLogAsset)assetItem.Asset).Text));
-                using (var recordedEffectCompile = new EffectLogStore(stream))
+                foreach (var entry in recordedEffectCompile.GetValues())
                 {
-                    recordedEffectCompile.LoadNewValues();
-
-                    foreach (var entry in recordedEffectCompile.GetValues())
-                    {
-                        steps.Add(EffectCompileCommand.FromRequest(context, assetItem.Package, urlRoot, entry.Key));
-                    }
+                    result.BuildSteps.Add(EffectCompileCommand.FromRequest(context, assetItem.Package, urlRoot, entry.Key));
                 }
-
-                Steps = steps;
-
-                return base.Execute(executeContext, builderContext);
-            }
-
-            /// <inheritdoc/>
-            public override BuildStep Clone()
-            {
-                return new EffectLogBuildStep(context, originalSourcePath, assetItem);
-            }
-
-            /// <inheritdoc/>
-            public override string ToString()
-            {
-                string title = "Recompile effects "; try { title += Path.GetFileName(originalSourcePath) ?? "[File]"; } catch { title += "[INVALID PATH]"; } return title;
             }
         }
     }
