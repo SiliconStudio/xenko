265734e8807480747048ec13be4fffbe0314a2cb
Jörg Wollenschläger <joerg.wollenschlaeger@siliconstudio.co.jp>
2017-04-04 19:38:35 +0900

    [License] Fix some headers

--- sources/engine/SiliconStudio.Xenko.Graphics/OpenGL/SwapChainGraphicsPresenter.Android.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics/OpenGL/SwapChainGraphicsPresenter.Android.cs
@@ -1,5 +1,4 @@
-﻿
-// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
+﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 #if SILICONSTUDIO_PLATFORM_ANDROID
 using System;
