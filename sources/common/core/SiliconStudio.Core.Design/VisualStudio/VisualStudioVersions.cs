7a9e64d2d0c1c0c54afc3100b3ac6222a58fcd5f
Alexander Radkov <Alexander.Radkov@siliconstudio.co.jp>
2017-04-04 12:29:35 +0900

    [Core] Further refactoring of the IDE dictionary and removed code which changes settings without the user's consent.

--- sources/common/core/SiliconStudio.Core.Design/VisualStudio/VisualStudioVersions.cs
+++ sources/common/core/SiliconStudio.Core.Design/VisualStudio/VisualStudioVersions.cs
@@ -1,48 +1,23 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
+
 using System.Collections.Generic;
 using System.IO;
-
 using Microsoft.Win32;
-using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Setup.Configuration;
 
 namespace SiliconStudio.Core.VisualStudio
 {
-    public struct IDEDictionaryKey
-    {
-        int key;
-
-        public int Key { get => key; set => key = value; }
-
-        public IDEDictionaryKey(int key)
-        {
-            this.key = key;
-        }
-
-        public override int GetHashCode() => key;
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is IDEDictionaryKey))
-                return false;
-
-            return ((IDEDictionaryKey)obj).Key == key;
-        }
-    }
-
     public struct IDEInfo
     {
-        public string DisplayName { get; set; }
         public string InstallationPath { get; set; }
     }
 
     public static class VisualStudioVersions
     {
-        private static Dictionary<IDEDictionaryKey, IDEInfo> ideDictionary;
+        private static Dictionary<string, IDEInfo> ideDictionary;
 
         public const string DefaultIDE = "Default IDE";
-        private const string VisualStudio2015 = "Visual Studio 2015";
 
         private const int maximumSlotSize = 255;    // Workaround for the Configuration lack of proper enumeration
 
@@ -51,17 +26,21 @@ namespace SiliconStudio.Core.VisualStudio
             if (ideDictionary != null)
                 return;
 
-            ideDictionary = new Dictionary<IDEDictionaryKey, IDEInfo>();
+            ideDictionary = new Dictionary<string, IDEInfo>();
 
-            ideDictionary.Add(new IDEDictionaryKey(ideDictionary.Count),
-                new IDEInfo { DisplayName = "Default IDE", InstallationPath = null });
+            ideDictionary.Add(DefaultIDE, new IDEInfo { InstallationPath = null });
 
             // Visual Studio 14.0 (2015)
-            var vs14InstallPath = GetSpecificVisualStudioPath("14.0");
-            if (vs14InstallPath != null)
+            var localMachine32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+            using (var subkey = localMachine32.OpenSubKey(string.Format(@"SOFTWARE\Microsoft\{0}\{1}", "VisualStudio", "14.0")))
             {
-                ideDictionary.Add(new IDEDictionaryKey(ideDictionary.Count),
-                    new IDEInfo { DisplayName = "Visual Studio 2015", InstallationPath = vs14InstallPath });
+                var path = (string)subkey?.GetValue("InstallDir");
+
+                var vs14InstallPath = (path != null) ? Path.Combine(path, "devenv.exe") : null;
+                if (vs14InstallPath != null)
+                {
+                    ideDictionary.Add("Visual Studio 2015", new IDEInfo { InstallationPath = vs14InstallPath });
+                }
             }
 
             // Visual Studio 15.0 (2017) and later
@@ -80,8 +59,7 @@ namespace SiliconStudio.Core.VisualStudio
                     if (path == null)
                         continue;
 
-                    ideDictionary.Add(new IDEDictionaryKey(ideDictionary.Count),
-                        new IDEInfo { DisplayName = inst[i].GetDisplayName(), InstallationPath = path });
+                    ideDictionary.Add(inst[i].GetDisplayName(), new IDEInfo { InstallationPath = path });
                 }
             }
         }
@@ -92,85 +70,20 @@ namespace SiliconStudio.Core.VisualStudio
             {
                 BuildDictionary();
 
-                foreach (var ideInfo in ideDictionary.Values)
-                    yield return ideInfo.DisplayName;
+                foreach (var key in ideDictionary.Keys)
+                    yield return key;
             }
         }
 
-        public static string GetVersionNumber(string visualStudioVersion)
+        public static string GetVisualStudioPath(string ideDisplayName)
         {
-            switch (visualStudioVersion)
-            {
-                case VisualStudio2015:
-                    return ("14.0");
-                default:
-                    {
-                        var configuration = new SetupConfiguration();
+            BuildDictionary();
 
-                        var instances = configuration.EnumAllInstances();
-                        instances.Reset();
-                        var inst = new ISetupInstance[maximumSlotSize];
-                        int pceltFetched;
-                        instances.Next(maximumSlotSize, inst, out pceltFetched);
-
-                        for (int i = 0; i < pceltFetched; i++)
-                        {
-                            if (visualStudioVersion.Equals(inst[i].GetDisplayName()))
-                                return inst[i].GetInstallationVersion();
-                        }
-                    }
-                    return null;
-            }
-        }
-
-        public static string GetVisualStudioPath(string visualStudioVersion)
-        {
-            switch (visualStudioVersion)
-            {
-                case VisualStudio2015:
-                    return GetSpecificVisualStudioPath(GetVersionNumber(visualStudioVersion));
-                default:
-                    return GetGenericVisualStudioPath(visualStudioVersion);
-            }
-        }
-
-        private static string GetGenericVisualStudioPath(string displayName)
-        {
-            var configuration = new SetupConfiguration();
-
-            var instances = configuration.EnumAllInstances();
-            instances.Reset();
-            var inst = new ISetupInstance[maximumSlotSize];
-            int pceltFetched;
-            instances.Next(maximumSlotSize, inst, out pceltFetched);
-
-            for (int i = 0; i < pceltFetched; i++)
-            {
-                var path = Path.Combine(inst[i].ResolvePath(), "Common7\\IDE\\devenv.exe");
-                if (path == null)
-                    continue;
-
-                if (displayName.Equals(inst[i].GetDisplayName()))
-                    return path;
-            }
+            IDEInfo ideInfo;
+            if (ideDictionary.TryGetValue(ideDisplayName, out ideInfo))
+                return ideInfo.InstallationPath;
 
             return null;
-        }
-
-        private static string GetSpecificVisualStudioPath(string version)
-        {
-            var localMachine32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
-            using (var subkey = localMachine32.OpenSubKey(string.Format(@"SOFTWARE\Microsoft\{0}\{1}", "VisualStudio", version)))
-            {
-                if (subkey == null)
-                    return null;
-
-                var path = (string)subkey.GetValue("InstallDir");
-                if (path == null)
-                    return null;
-
-                return Path.Combine(path, "devenv.exe");
-            }
         }
     }
 }
