94c0c719857b65e251fb6f630ea04f030a3e66e3
Giovanni <giovanni.petrantoni@siliconstudio.co.jp>
2017-04-10 12:45:19 +0900

    [Audio] Moved module initializer to proper file

--- sources/engine/SiliconStudio.Xenko.Audio/AudioDevice.cs
+++ sources/engine/SiliconStudio.Xenko.Audio/AudioDevice.cs
@@ -1,21 +1,8 @@
 ﻿// Copyright (c) 2014 Silicon Studio Corp. (http://siliconstudio.co.jp)
 // This file is distributed under GPL v3. See LICENSE.md for details.
 
-using SiliconStudio.Core;
-using SiliconStudio.Core.Reflection;
-
 namespace SiliconStudio.Xenko.Audio
 {
-    internal class Module
-    {
-        [ModuleInitializer]
-        public static void Initialize()
-        {
-            // Make sure that this assembly is registered
-            AssemblyRegistry.Register(typeof(Module).Assembly, AssemblyCommonCategories.Assets);
-        }
-    }
-
     /// <summary>
     /// Reprensent an Audio Hardware Device.
     /// Can be used when creating an <see cref="AudioEngine"/> to specify the device on which to play the sound.
