ed79e6194c5b781dcccb1b9fd62bdf6b59030424
Virgile Bello <virgile@siliconstudio.co.jp>
2017-04-12 19:46:31 +0900

    [VSPackage] Added integration unit tests

---
+++ sources/tools/SiliconStudio.Xenko.VisualStudio.Package.Tests/Module.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2017 Silicon Studio Corp. (http://siliconstudio.co.jp)
+// This file is distributed under GPL v3. See LICENSE.md for details.
+
+using System;
+using System.IO;
+using SiliconStudio.Assets;
+using SiliconStudio.Core;
+
+namespace SiliconStudio.Xenko.VisualStudio.Package.Tests
+{
+    public class Module
+    {
+        [ModuleInitializer]
+        internal static void Initialize()
+        {
+            // Override search path since we are in a unit test directory
+            DirectoryHelper.PackageDirectoryOverride = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..");
+        }
+    }
+}
