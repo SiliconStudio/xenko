cedf4f585620dd7495229dc7352d18f4972dbee9
Giovanni <giovanni.petrantoni@siliconstudio.co.jp>
2017-04-13 16:36:56 +0900

    [Physics] Made sure to ignore collisions for disabled components

--- sources/engine/SiliconStudio.Xenko.Physics/Simulation.cs
+++ sources/engine/SiliconStudio.Xenko.Physics/Simulation.cs
@@ -1234,7 +1234,7 @@ namespace SiliconStudio.Xenko.Physics
                 var component1 = (PhysicsComponent)obj1.UserObject;
 
                 //disable static-static
-                if (component0 is StaticColliderComponent && component1 is StaticColliderComponent)
+                if (component0 is StaticColliderComponent && component1 is StaticColliderComponent || !component0.Enabled || !component1.Enabled)
                     continue;
 
                 currentFrameContacts.Add(new ContactPoint
