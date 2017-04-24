28a20bf6966eafb1077aaa43ef9879b5b4e4f6b6
Wojciech Figat <g-wc-figat@gametools.jp>
2017-04-24 19:09:13 +0900

    [Streaming] Added streaming textures

---
+++ sources/engine/SiliconStudio.Xenko.Engine/Streaming/StreamingTexture.cs
@@ -0,0 +1,37 @@
+﻿using SiliconStudio.Core.Annotations;
+using SiliconStudio.Xenko.Graphics;
+
+namespace SiliconStudio.Xenko.Streaming
+{
+    /// <summary>
+    /// Texture streaming object.
+    /// </summary>
+    public class StreamingTexture : StreamableResource
+    {
+        protected Texture _texture;
+        protected int _residentMips;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StreamingTexture"/> class.
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        /// <param name="texture">The texture to stream.</param>
+        public StreamingTexture(StreamingManager manager, [NotNull] Texture texture)
+            : base(manager)
+        {
+            _texture = texture;
+            _residentMips = 0;
+        }
+
+        /// <summary>
+        /// Gets the texture object.
+        /// </summary>
+        public Texture Texture => _texture;
+
+        /// <inheritdoc />
+        public override int CurrentResidency => _residentMips;
+
+        /// <inheritdoc />
+        public override int AllocatedResidency => _texture.MipLevels;
+    }
+}
