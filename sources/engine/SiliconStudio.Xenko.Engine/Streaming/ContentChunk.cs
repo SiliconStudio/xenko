b7c8b723299f66b11f3a0acf5c8c33d164f915dd
Wojciech Figat <g-wc-figat@gametools.jp>
2017-04-26 17:18:41 +0900

    [Content] Added data storage containers for streaming layer

---
+++ sources/engine/SiliconStudio.Xenko.Engine/Streaming/ContentChunk.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2014-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+
+using System;
+
+namespace SiliconStudio.Xenko.Streaming
+{
+    /// <summary>
+    /// Content storage data chunk.
+    /// </summary>
+    public class ContentChunk
+    {
+        /// <summary>
+        /// Gets the parent storage container.
+        /// </summary>
+        public ContentStorage Storage { get; }
+
+        /// <summary>
+        /// Gets the chunk location in file (adress of the first byte).
+        /// </summary>
+        public int Location { get; }
+
+        /// <summary>
+        /// Gets the chunk size in file (in bytes).
+        /// </summary>
+        public int Size { get; }
+
+        /// <summary>
+        /// Gets the data. Can be null if hasn't been laoded yet.
+        /// </summary>
+        public byte[] Data { get; protected set; }
+
+        /// <summary>
+        /// Gets the last access time.
+        /// </summary>
+        public DateTime LastAccessTime { get; protected set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this chunk is loaded.
+        /// </summary>
+        public bool IsLoaded => Data != null;
+
+        /// <summary>
+        /// Gets a value indicating whether this chunk is not loaded.
+        /// </summary>
+        public bool IsMissing => Data == null;
+
+        /// <summary>
+        /// Gets a value indicating whether this exists in file.
+        /// </summary>
+        public bool ExistsInFile => Size > 0;
+
+        internal ContentChunk(ContentStorage storage, int location, int size)
+        {
+            Storage = storage;
+            Location = location;
+            Size = size;
+        }
+        
+        /// <summary>
+        /// Registers the usage operation of chunk data.
+        /// </summary>
+        public void RegisterUsage()
+        {
+            LastAccessTime = DateTime.UtcNow;
+        }
+    }
+}
---
+++ sources/engine/SiliconStudio.Xenko.Engine/Streaming/ContentStorage.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2014-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconStudio.Xenko.Streaming
+{
+    /// <summary>
+    /// Streamable resources content storage containter.
+    /// </summary>
+    public class ContentStorage
+    {
+        private ContentChunk[] _chunks;
+
+        /// <summary>
+        /// Gets the last access time.
+        /// </summary>
+        public DateTime LastAccessTime
+        {
+            get
+            {
+                if (_chunks == null)
+                    return DateTime.MinValue;
+
+                var result = _chunks[0].LastAccessTime;
+                for (int i = 1; i < _chunks.Length; i++)
+                {
+                    if (result < _chunks[i].LastAccessTime)
+                        result = _chunks[i].LastAccessTime;
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Gets the chunk.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns>Chunk</returns>
+        public ContentChunk GetChunk(int index)
+        {
+            Debug.Assert(_chunks != null && index >= 0 && _chunks.Length > index);
+
+            var chunk = _chunks[index];
+            chunk.RegisterUsage();
+            return chunk;
+        }
+    }
+}
