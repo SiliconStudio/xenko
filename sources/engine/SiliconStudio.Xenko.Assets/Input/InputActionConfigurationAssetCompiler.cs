643d2975d5b01e5ac2436ffbe0ca1088cd4cb354
guusw <g-gj-waals@gametools.jp>
2017-04-26 18:50:51 +0900

    [Assets] Upgrade InputActionConfigurationAsset compiler
    # Original commit: '36094c1ac588237cf8e2a141fd592209a4b5ecce'

--- sources/engine/SiliconStudio.Xenko.Assets/Input/InputActionConfigurationAssetCompiler.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/Input/InputActionConfigurationAssetCompiler.cs
@@ -5,25 +5,26 @@ using System.Threading.Tasks;
 using SiliconStudio.Assets;
 using SiliconStudio.Assets.Compiler;
 using SiliconStudio.BuildEngine;
-using SiliconStudio.Core.Serialization;
 using SiliconStudio.Core.Serialization.Contents;
 using SiliconStudio.Xenko.Input.Mapping;
 
 namespace SiliconStudio.Xenko.Assets.Input
 {
+    [AssetCompiler(typeof(InputActionConfigurationAsset), typeof(AssetCompilationContext))]
     public class InputActionConfigurationAssetCompiler : AssetCompilerBase
     {
-        protected override void Compile(AssetCompilerContext context, AssetItem assetItem, string targetUrlInStorage, AssetCompilerResult result)
+        protected override void Prepare(AssetCompilerContext context, AssetItem assetItem, string targetUrlInStorage, AssetCompilerResult result)
         {
+            var asset = (InputActionConfigurationAsset)assetItem.Asset;
             result.BuildSteps = new AssetBuildStep(assetItem)
             {
-                new InputActionMappingAssetCompileCommand(targetUrlInStorage, (InputActionConfigurationAsset)assetItem.Asset)
+                new InputActionMappingAssetCompileCommand(targetUrlInStorage, assetItem, asset)
             };
         }
 
         internal class InputActionMappingAssetCompileCommand : AssetCommand<InputActionConfigurationAsset>
         {
-            public InputActionMappingAssetCompileCommand(string url, InputActionConfigurationAsset parameters) : base(url, parameters)
+            public InputActionMappingAssetCompileCommand(string url, AssetItem assetItem, InputActionConfigurationAsset value) : base(url, value, assetItem.Package)
             {
             }
             protected override Task<ResultStatus> DoCommandOverride(ICommandContext commandContext)
