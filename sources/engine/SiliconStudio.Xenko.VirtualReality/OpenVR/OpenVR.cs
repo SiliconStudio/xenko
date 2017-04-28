834a6715da24b023d19bebc57dff2f852e927d01
Giovanni <giovanni.petrantoni@siliconstudio.co.jp>
2017-04-28 15:46:38 +0900

    [VirtualReality] Fixed devices shutdown and rendering in editor mode
    # Original commit: 'c5b43523b98d9b798a0aed963e9cec91ef0122cd'

--- sources/engine/SiliconStudio.Xenko.VirtualReality/OpenVR/OpenVR.cs
+++ sources/engine/SiliconStudio.Xenko.VirtualReality/OpenVR/OpenVR.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+﻿// Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
 // See LICENSE.md for full license information.
 #if SILICONSTUDIO_XENKO_GRAPHICS_API_DIRECT3D11
 
@@ -175,6 +175,13 @@ namespace SiliconStudio.Xenko.VirtualReality
             Valve.VR.OpenVR.Compositor.SetTrackingSpace(ETrackingUniverseOrigin.TrackingUniverseSeated);
 
             return true;
+        }
+
+        public static void Shutdown()
+        {
+            if (!InitDone) return;
+            Valve.VR.OpenVR.Shutdown();
+            InitDone = false;
         }
 
         public static bool Submit(int eyeIndex, Texture texture, ref RectangleF viewport)
