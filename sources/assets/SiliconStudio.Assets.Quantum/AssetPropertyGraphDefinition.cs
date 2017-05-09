512996432c48ddfdf925c0d7e855b2829262ffca
Benjamin Litzelmann <benjamin.litzelmann@siliconstudio.co.jp>
2017-05-09 12:46:33 +0900

    [Build] Add some missing copyright headers

--- sources/assets/SiliconStudio.Assets.Quantum/AssetPropertyGraphDefinition.cs
+++ sources/assets/SiliconStudio.Assets.Quantum/AssetPropertyGraphDefinition.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+
+using System;
 using System.Collections.Generic;
 using SiliconStudio.Quantum;
 
