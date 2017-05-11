0974055742cc8ee9de428d265071899b2855733e
guusw <g-gj-waals@gametools.jp>
2017-05-11 20:00:59 +0900

    [Input] Add Pressed/Released/Down state to PointerDevice

--- sources/engine/SiliconStudio.Xenko.Input/IPointerDevice.cs
+++ sources/engine/SiliconStudio.Xenko.Input/IPointerDevice.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using SiliconStudio.Core.Collections;
 using SiliconStudio.Core.Mathematics;
 
 namespace SiliconStudio.Xenko.Input
@@ -12,11 +13,6 @@ namespace SiliconStudio.Xenko.Input
     /// </summary>
     public interface IPointerDevice : IInputDevice
     {
-        /// <summary>
-        /// A list of pointer points currently active
-        /// </summary>
-        IReadOnlyList<PointerPoint> PointerPoints { get; }
-
         /// <summary>
         /// The type of the pointer device
         /// </summary>
@@ -28,9 +24,24 @@ namespace SiliconStudio.Xenko.Input
         Vector2 SurfaceSize { get; }
 
         /// <summary>
-        /// The size of the surface used by the pointer, for a mouse this is the size of the window, for a touch device, the size of the touch area, etc.
+        /// The aspect ratio of the touch surface area
         /// </summary>
         float SurfaceAspectRatio { get; }
+        
+        /// <summary>
+        /// The index of the pointers that have been pressed since the last frame
+        /// </summary>
+        IReadOnlySet<PointerPoint> PressedPointers { get; }
+
+        /// <summary>
+        /// The index of the pointers that have been released since the last frame
+        /// </summary>
+        IReadOnlySet<PointerPoint> ReleasedPointers { get; }
+
+        /// <summary>
+        /// The index of the pointers that are down
+        /// </summary>
+        IReadOnlySet<PointerPoint> DownPointers { get; }
 
         /// <summary>
         /// Raised when the surface size of this pointer changed
