0427e76d91ea1c4df1ca8dfdfd99d8f9f478126d
guusw <g-gj-waals@gametools.jp>
2017-05-11 15:05:44 +0900

    [Input] Add display name to GameControllerDirectionEvent.ToString
    # Original commit: '9901128fdb39a95e2f8f7d4afa244e6723b4a82c'

--- sources/engine/SiliconStudio.Xenko.Input/GameControllerDirectionEvent.cs
+++ sources/engine/SiliconStudio.Xenko.Input/GameControllerDirectionEvent.cs
@@ -25,7 +25,34 @@ namespace SiliconStudio.Xenko.Input
 
         public override string ToString()
         {
-            return $"{nameof(Index)}: {Index} ({GameController.DirectionInfos[Index].Name}), {nameof(Direction)}: {Direction}, {nameof(GameController)}: {GameController.Name}";
+            return $"{nameof(Index)}: {Index} ({GameController.DirectionInfos[Index].Name}), {nameof(Direction)}: {Direction} ({GetDirectionFriendlyName()}), {nameof(GameController)}: {GameController.Name}";
+        }
+
+        private string GetDirectionFriendlyName()
+        {
+            if (Direction.IsNeutral)
+                return "Neutral";
+            switch (Direction.GetTicks(8))
+            {
+                case 0:
+                    return "Up";
+                case 1:
+                    return "RightUp";
+                case 2:
+                    return "Right";
+                case 3:
+                    return "RightDown";
+                case 4:
+                    return "Down";
+                case 5:
+                    return "LeftDown";
+                case 6:
+                    return "Left";
+                case 7:
+                    return "LeftUp";
+                default:
+                    return "Out of range";
+            }
         }
     }
 }
