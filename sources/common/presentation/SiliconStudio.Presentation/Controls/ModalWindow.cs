8e91bf898893b36e7afede6cdce78e3f86cf7947
Wojciech Figat <g-wc-figat@gametools.jp>
2017-05-16 10:35:33 +0900

    Fix merge conflict

--- sources/common/presentation/SiliconStudio.Presentation/Controls/ModalWindow.cs
+++ sources/common/presentation/SiliconStudio.Presentation/Controls/ModalWindow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
