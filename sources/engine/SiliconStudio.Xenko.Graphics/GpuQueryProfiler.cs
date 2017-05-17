a6ffb5ef35b9f0c085dd97e1b83f8b8514e64580
Baptiste Prevost <g-ba-prevost@gametools.jp>
2017-05-17 15:09:16 +0900

    [Profiling] Renamed ProfilerManager to GpuQueryProfiler; now sends results asynchronously to Profiler

---
+++ sources/engine/SiliconStudio.Xenko.Graphics/GpuQueryProfiler.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) 2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+
+using SiliconStudio.Core.Collections;
+using SiliconStudio.Core.Diagnostics;
+
+namespace SiliconStudio.Xenko.Graphics
+{
+    public class GpuQueryProfiler
+    {
+        private const int TimestampQueryPoolCapacity = 64;
+
+        private struct QueryEvent
+        {
+            public Query? BeginQuery;
+            public Query? EndQuery;
+
+            public ProfilingState ProfilingState;
+        }
+
+        private readonly QueryPool timestampQueryPool;  
+        private long[] queryResults = new long[TimestampQueryPoolCapacity];
+        private readonly FastList<QueryEvent> queryEvents = new FastList<QueryEvent>();
+
+        public GpuQueryProfiler(CommandList commandList)
+        {
+            timestampQueryPool = commandList.QueryPoolManager.GetOrCreatePool(commandList, QueryType.Timestamp, TimestampQueryPoolCapacity);
+        }
+
+        public void SubmitResults(CommandList commandList)
+        {
+            timestampQueryPool.GetData(commandList, ref queryResults);
+
+            foreach (QueryEvent queryEvent in queryEvents)
+            {
+                queryEvent.ProfilingState.Begin(queryResults[queryEvent.BeginQuery.Value.InternalIndex]);
+                queryEvent.ProfilingState.End(queryResults[queryEvent.EndQuery.Value.InternalIndex]);
+            }
+
+            queryEvents.Clear(true);
+        }
+
+        public int BeginProfile(CommandList commandList, ProfilingKey profilingKey)
+        {
+            QueryEvent queryEvent = new QueryEvent()
+            {
+                BeginQuery = timestampQueryPool.AllocateQuery(),
+                EndQuery = timestampQueryPool.AllocateQuery(),
+                ProfilingState = Profiler.New(profilingKey),
+            };
+
+            int eventId = queryEvents.Count;
+
+            if (queryEvent.BeginQuery.HasValue)
+            {
+                commandList.WriteTimestamp(timestampQueryPool, queryEvent.BeginQuery.Value);
+            }
+
+            queryEvents.Add(queryEvent);
+
+            return eventId;
+        }
+
+        public void EndProfiler(CommandList commandList, int eventId)
+        {
+            if (queryEvents[eventId].EndQuery.HasValue)
+            {
+                commandList.WriteTimestamp(timestampQueryPool, queryEvents[eventId].EndQuery.Value);
+            }
+        }
+    }
+}
