0f4b5e9b2f4769e79130c1501d83a1a1fd1cb9a7
Baptiste Prevost <g-ba-prevost@gametools.jp>
2017-05-22 18:12:12 +0900

    [Profiling]Added isGpuQuery attribute (in case the profiling result uses the GPU clock frequency)
    # Original commit: '065d3154dfae9d74540d1991df0b023c50eea779'

--- sources/common/core/SiliconStudio.Core/Diagnostics/Profiler.cs
+++ sources/common/core/SiliconStudio.Core/Diagnostics/Profiler.cs
@@ -65,6 +65,8 @@ namespace SiliconStudio.Core.Diagnostics
         private static bool enableAll;
         private static int profileId;
 
+        public static long GpuClockFrequency;
+
         /// <summary>
         /// Enables all profilers.
         /// </summary>
