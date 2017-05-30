0858ada325b4966b0e527e8490845fb8d560b93b
Wojciech Figat <g-wc-figat@gametools.jp>
2017-05-30 14:46:22 +0900

    [Graphics] Added proper resource transition check function

--- sources/engine/SiliconStudio.Xenko.Graphics/Direct3D12/GraphicsResource.Direct3D12.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics/Direct3D12/GraphicsResource.Direct3D12.cs
@@ -18,12 +18,19 @@ namespace SiliconStudio.Xenko.Graphics
         internal CpuDescriptorHandle NativeShaderResourceView;
         internal ResourceStates NativeResourceState;
 
-        protected bool IsDebugMode
+        protected bool IsDebugMode => GraphicsDevice != null && GraphicsDevice.IsDebugMode;
+
+        /// <summary>
+        /// Returns true if resource state transition is needed in order to use resource in given state
+        /// </summary>
+        /// <param name="targeState">Destination resource state</param>
+        /// <returns>True if need to perform a transition, otherwsie false</returns>
+        internal bool IsTransitionNeeded(ResourceStates targeState)
         {
-            get
-            {
-                return GraphicsDevice != null && GraphicsDevice.IsDebugMode;
-            }
+            // If 'targeState' is a subset of 'before', then there's no need for a transition
+            // Note: COMMON is an oddball state that doesn't follow the RESOURE_STATE pattern of 
+            // having exactly one bit set so we need to special case these
+            return NativeResourceState != targeState && ((NativeResourceState | targeState) != NativeResourceState || targeState == ResourceStates.Common);
         }
     }
 }
