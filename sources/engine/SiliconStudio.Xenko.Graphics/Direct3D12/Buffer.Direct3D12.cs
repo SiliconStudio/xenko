fb9ef9482e845203d660386dd6c33fca3a7fa287
Wojciech Figat <g-wc-figat@gametools.jp>
2017-05-31 19:14:01 +0900

    [Graphics] Minor fix, enable TestTexture for dx12

--- sources/engine/SiliconStudio.Xenko.Graphics/Direct3D12/Buffer.Direct3D12.cs
+++ sources/engine/SiliconStudio.Xenko.Graphics/Direct3D12/Buffer.Direct3D12.cs
@@ -92,12 +92,9 @@ namespace SiliconStudio.Xenko.Graphics
 
             if ((bufferFlags & BufferFlags.StructuredBuffer) != 0)
             {
-                if (bufferDescription.StructureByteStride == 0)
-                    throw new ArgumentException("Element size cannot be set to 0 for structured buffer");
+                if (bufferDescription.StructureByteStride <= 0)
+                    throw new ArgumentException("Element size cannot be less or equal 0 for structured buffer");
             }
-
-            if ((bufferFlags & BufferFlags.RawBuffer) == BufferFlags.RawBuffer)
-                throw new NotImplementedException();
 
             if ((bufferFlags & BufferFlags.ArgumentBuffer) == BufferFlags.ArgumentBuffer)
                 NativeResourceState |= ResourceStates.IndirectArgument;
