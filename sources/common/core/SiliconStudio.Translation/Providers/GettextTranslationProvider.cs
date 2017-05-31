f6e1032e504c9273ea9973a3cfb01461243b367e
Nicolas Musset <nicolas.musset@xenko.com>
2017-05-31 16:49:47 +0900

    [Translation] Add project `SiliconStudio.Translation` that provides an implementation of a translation manager and translation providers.
    Note: also add GNU.Gettext dependency.

---
+++ sources/common/core/SiliconStudio.Translation/Providers/GettextTranslationProvider.cs
@@ -0,0 +1,76 @@
+// Copyright (c) 2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+using System;
+using System.Reflection;
+using GNU.Gettext;
+using SiliconStudio.Core.Annotations;
+
+namespace SiliconStudio.Translation.Providers
+{
+    /// <summary>
+    /// Translation provider using the Gettext library.
+    /// </summary>
+    public sealed class GettextTranslationProvider : ITranslationProvider
+    {
+        private readonly GettextResourceManager resourceManager;
+
+        /// <seealso cref="GettextResourceManager()"/>
+        public GettextTranslationProvider()
+            : this(Assembly.GetCallingAssembly())
+        {
+        }
+
+        /// <seealso cref="GettextResourceManager(Assembly)"/>
+        public GettextTranslationProvider([NotNull] Assembly assembly)
+            : this(assembly.GetName().Name, assembly)
+        {
+        }
+
+        /// <seealso cref="GettextResourceManager(string, Assembly)"/>
+        private GettextTranslationProvider([NotNull] string baseName, [NotNull] Assembly assembly)
+        {
+            if (baseName == null) throw new ArgumentNullException(nameof(baseName));
+            if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+            resourceManager = new GettextResourceManager(baseName, assembly);
+            BaseName = baseName;
+        }
+
+        public string BaseName { get; }
+
+        /// <inheritdoc />
+        /// <seealso cref="GettextResourceManager.GetString(string)"/>
+        public string GetString(string text)
+        {
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            return resourceManager.GetString(text);
+        }
+
+        /// <inheritdoc />
+        /// <seealso cref="GettextResourceManager.GetPluralString(string,string,long)"/>
+        public string GetPluralString(string text, [NotNull] string textPlural, long count)
+        {
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            if (textPlural == null) throw new ArgumentNullException(nameof(textPlural));
+            return resourceManager.GetPluralString(text, textPlural, count);
+        }
+
+        /// <inheritdoc />
+        /// <seealso cref="GettextResourceManager.GetParticularString(string,string)"/>
+        public string GetParticularString(string text, [NotNull] string context)
+        {
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            return resourceManager.GetParticularString(context, text);
+        }
+
+        /// <inheritdoc />
+        /// <seealso cref="GettextResourceManager.GetParticularPluralString(string,string,string,long)"/>
+        public string GetParticularPluralString(string text, [NotNull] string textPlural, long count, [NotNull] string context)
+        {
+            if (text == null) throw new ArgumentNullException(nameof(text));
+            if (textPlural == null) throw new ArgumentNullException(nameof(textPlural));
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            return resourceManager.GetParticularPluralString(context, text, textPlural, count);
+        }
+    }
+}
---
+++ sources/common/core/SiliconStudio.Translation/TranslationManager.cs
@@ -0,0 +1,120 @@
+// Copyright (c) 2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using SiliconStudio.Core.Annotations;
+
+namespace SiliconStudio.Translation
+{
+    public static class TranslationManager
+    {
+        private static readonly Lazy<ITranslationManager> Lazy = new Lazy<ITranslationManager>(() => new TranslationManagerImpl());
+
+        /// <summary>
+        /// Gets the instance of the <see cref="ITranslationManager"/>.
+        /// </summary>
+        public static ITranslationManager Instance => Lazy.Value;
+
+        /// <summary>
+        /// Implementation of <see cref="ITranslationManager"/>.
+        /// </summary>
+        private sealed class TranslationManagerImpl : ITranslationManager
+        {
+            private readonly Dictionary<string, ITranslationProvider> translationProviders = new Dictionary<string, ITranslationProvider>();
+
+            /// <inheritdoc />
+            public CultureInfo CurrentLanguage
+            {
+                get => Thread.CurrentThread.CurrentUICulture;
+                set
+                {
+                    if (Equals(Thread.CurrentThread.CurrentUICulture, value))
+                        return;
+
+                    Thread.CurrentThread.CurrentUICulture = value;
+                    OnLanguageChanged();
+                }
+            }
+
+            /// <inheritdoc />
+            public event EventHandler LanguageChanged;
+
+            /// <inheritdoc />
+            string ITranslationProvider.BaseName => nameof(TranslationManager);
+
+            /// <inheritdoc />
+            public string GetString(string text)
+            {
+                return GetString(text, Assembly.GetCallingAssembly());
+            }
+
+            /// <inheritdoc />
+            public string GetString(string text, [NotNull] Assembly assembly)
+            {
+                if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+                return GetProvider(assembly)?.GetString(text) ?? text;
+            }
+
+            /// <inheritdoc />
+            public string GetPluralString(string text, string textPlural, long count)
+            {
+                return GetPluralString(text, textPlural, count, Assembly.GetCallingAssembly());
+            }
+
+            /// <inheritdoc />
+            public string GetPluralString(string text, string textPlural, long count, [NotNull] Assembly assembly)
+            {
+                if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+                return GetProvider(assembly)?.GetPluralString(text, textPlural, count) ?? text;
+            }
+
+            /// <inheritdoc />
+            public string GetParticularString(string text, string context)
+            {
+                return GetParticularString(text, context, Assembly.GetCallingAssembly());
+            }
+
+            /// <inheritdoc />
+            public string GetParticularString(string text, string context, [NotNull] Assembly assembly)
+            {
+                if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+                return GetProvider(assembly)?.GetParticularString(text, context) ?? text;
+            }
+
+            /// <inheritdoc />
+            public string GetParticularPluralString(string text, string textPlural, long count, string context)
+            {
+                return GetParticularPluralString(text, textPlural, count, context, Assembly.GetCallingAssembly());
+            }
+
+            /// <inheritdoc />
+            public string GetParticularPluralString(string text, string textPlural, long count, string context, [NotNull] Assembly assembly)
+            {
+                if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+                return GetProvider(assembly)?.GetParticularPluralString(text, textPlural, count, context) ?? text;
+            }
+
+            /// <inheritdoc />
+            public void RegisterProvider(ITranslationProvider provider)
+            {
+                if (provider == null) throw new ArgumentNullException(nameof(provider));
+                translationProviders.Add(provider.BaseName, provider);
+            }
+
+            [CanBeNull]
+            private ITranslationProvider GetProvider([NotNull] Assembly assembly)
+            {
+                translationProviders.TryGetValue(assembly.GetName().Name, out var provider);
+                return provider;
+            }
+
+            private void OnLanguageChanged()
+            {
+                LanguageChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
