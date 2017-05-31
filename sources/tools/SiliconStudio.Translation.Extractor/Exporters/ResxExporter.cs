b621e58a607b9e9587cb3dca8b7c2b70fefa9b17
Nicolas Musset <nicolas.musset@xenko.com>
2017-05-31 17:08:09 +0900

    [Translation] Add project `SiliconStudio.Translation.Extractor` that allows to extract and export localizable texts from sources.
    Note: also add GNU.Getopt and GNU.Gettext.Msgfmt dependencies.

---
+++ sources/tools/SiliconStudio.Translation.Extractor/Exporters/ResxExporter.cs
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
+// See LICENSE.md for full license information.
+
+namespace SiliconStudio.Translation.Extractor
+{
+    internal class ResxExporter
+    {
+    }
+}
