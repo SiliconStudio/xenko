36113b09fb017ba001e817513635e11377321f15
guusw <g-gj-waals@gametools.jp>
2017-06-02 10:45:49 +0900

    [Input] Fix compilation issue and wrong pointer id's on android

--- sources/engine/SiliconStudio.Xenko.Input/Android/PointerAndroid.cs
+++ sources/engine/SiliconStudio.Xenko.Input/Android/PointerAndroid.cs
@@ -3,6 +3,7 @@
 
 #if SILICONSTUDIO_PLATFORM_ANDROID
 using System;
+using System.Collections.Generic;
 using Android.Views;
 using SiliconStudio.Core.Mathematics;
 using SiliconStudio.Xenko.Games.Android;
@@ -24,21 +25,32 @@ namespace SiliconStudio.Xenko.Input
             OnResize(this, null);
         }
 
-        public override string Name => "Android Pointer";
-
-        public override Guid Id => new Guid("21370b00-aaf9-4ecf-afb2-575dde6c6c56");
-
-        public override IInputSource Source { get; }
-
         public void Dispose()
         {
             uiControl.Resize -= OnResize;
             uiControl.SetOnTouchListener(null);
         }
 
+        public override string Name => "Android Pointer";
+
+        public override Guid Id => new Guid("21370b00-aaf9-4ecf-afb2-575dde6c6c56");
+
+        public override IInputSource Source { get; }
+
+        public override void Update(List<InputEvent> inputEvents)
+        {
+            PointerState.Update(inputEvents);
+            base.Update(inputEvents);
+        }
+
         private void OnResize(object sender, EventArgs eventArgs)
         {
             SetSurfaceSize(new Vector2(uiControl.Size.Width, uiControl.Size.Height));
+        }
+
+        private void AddPointerEvent(PointerDeviceState.InputEvent evt)
+        { 
+            PointerState.PointerInputEvents.Add(evt);
         }
 
         protected class Listener : Java.Lang.Object, View.IOnTouchListener
@@ -76,21 +88,28 @@ namespace SiliconStudio.Xenko.Input
                         break;
                 }
 
-                // Generate events for each pointer
-                for (int i = 0; i < e.PointerCount; i++)
+                if (actionType != PointerEventType.Moved)
                 {
-                    var evt = new PointerInputEvent
+                    pointer.AddPointerEvent(new PointerDeviceState.InputEvent
                     {
-                        Id = e.GetPointerId(i),
-                        Position = Normalize(new Vector2(e.GetX(i), e.GetY(i))),
+                        Id = e.GetPointerId(e.ActionIndex),
+                        Position = pointer.Normalize(new Vector2(e.GetX(e.ActionIndex), e.GetY(e.ActionIndex))),
                         Type = actionType
-                    };
-
-                    // Store pointer ID
-                    // Normalize
-                    pointer.PointerInputEvents.Add(evt);
+                    });
                 }
-                
+                else
+                {
+                    for (int i = 0; i < e.PointerCount; i++)
+                    {
+                        pointer.AddPointerEvent(new PointerDeviceState.InputEvent
+                        {
+                            Id = e.GetPointerId(i),
+                            Position = pointer.Normalize(new Vector2(e.GetX(i), e.GetY(i))),
+                            Type = actionType
+                        });
+                    }
+                }
+
                 return true;
             }
         }
