2c91d195685e5bc2df361cfa4ef8c35980cfc463
guusw <g-gj-waals@gametools.jp>
2017-06-06 17:10:10 +0900

    [Samples] Correctly wait for the dynamic navigation system to be added inside of EnableDynamicNavigation script

--- samples/Templates/TopDownRPG/TopDownRPG/TopDownRPG.Game/Gameplay/EnableDynamicNavigation.cs
+++ samples/Templates/TopDownRPG/TopDownRPG/TopDownRPG.Game/Gameplay/EnableDynamicNavigation.cs
@@ -1,6 +1,9 @@
 // Copyright (c) 2011-2017 Silicon Studio Corp. All rights reserved. (https://www.siliconstudio.co.jp)
 // See LICENSE.md for full license information.
+
+using System.Collections.Specialized;
 using System.Linq;
+using SiliconStudio.Core.Collections;
 using SiliconStudio.Xenko.Engine;
 using SiliconStudio.Xenko.Navigation;
 
@@ -10,7 +13,31 @@ namespace Gameplay
     {
         public override void Start()
         {
-            Game.GameSystems.OfType<DynamicNavigationMeshSystem>().FirstOrDefault().Enabled = true;
+            var dynamicNavigationMeshSystem = Game.GameSystems.OfType<DynamicNavigationMeshSystem>().FirstOrDefault();
+
+            // Wait for the dynamic navigation to be registered
+            if(dynamicNavigationMeshSystem == null)
+                Game.GameSystems.CollectionChanged += GameSystemsOnCollectionChanged;
+        }
+
+        public override void Cancel()
+        {
+            Game.GameSystems.CollectionChanged -= GameSystemsOnCollectionChanged;
+        }
+
+        private void GameSystemsOnCollectionChanged(object sender, TrackingCollectionChangedEventArgs trackingCollectionChangedEventArgs)
+        {
+            if (trackingCollectionChangedEventArgs.Action == NotifyCollectionChangedAction.Add)
+            {
+                var dynamicNavigationMeshSystem = trackingCollectionChangedEventArgs.Item as DynamicNavigationMeshSystem;
+                if (dynamicNavigationMeshSystem != null)
+                {
+                    dynamicNavigationMeshSystem.Enabled = true;
+
+                    // No longer need to listen to changes
+                    Game.GameSystems.CollectionChanged -= GameSystemsOnCollectionChanged;
+                }
+            }
         }
     }
 }
