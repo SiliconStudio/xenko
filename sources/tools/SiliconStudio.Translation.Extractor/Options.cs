1ca388f3ed9c7109de3300304e83ca6f5698e82e
Nicolas Musset <nicolas.musset@xenko.com>
2017-06-08 12:37:29 +0900

    [Translation] Allow to exclude files or patterns from the list of inputs when extracting messages.

--- sources/tools/SiliconStudio.Translation.Extractor/Options.cs
+++ sources/tools/SiliconStudio.Translation.Extractor/Options.cs
@@ -6,14 +6,34 @@ namespace SiliconStudio.Translation.Extractor
 {
     internal class Options
     {
+        /// <summary>
+        /// <c>true</c> if a backup of an existing output file should be created; otherwise, <c>false</c>.
+        /// </summary>
         public bool Backup { get; set; }
 
+        /// <summary>
+        /// Patterns to exclude from the list of inputs.
+        /// </summary>
+        public List<string> Excludes { get; } = new List<string>();
+
+        /// <summary>
+        /// Directories to search for input files.
+        /// </summary>
         public List<string> InputDirs { get; } = new List<string>();
 
+        /// <summary>
+        /// Patterns or input filnames to extract the messages from.
+        /// </summary>
         public List<string> InputFiles { get; } = new List<string>();
 
+        /// <summary>
+        /// Name of generated catalog file.
+        /// </summary>
         public string OutputFile { get; set; } = "messages.pot";
 
+        /// <summary>
+        /// <c>true</c> if an existing catalog should be ignored and not merged into the new one; otherwise, <c>false</c>.
+        /// </summary>
         public bool Overwrite { get; set; } = true;
 
         public bool Recursive { get; set; }
