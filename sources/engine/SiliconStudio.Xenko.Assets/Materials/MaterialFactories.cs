4b13b38d74b672bf9240e9396912cc70eea96c4a
Baptiste Prevost <g-ba-prevost@gametools.jp>
2017-07-18 16:15:37 +0900

    [Misc] Removed references to car paint from source files and resources

--- sources/engine/SiliconStudio.Xenko.Assets/Materials/MaterialFactories.cs
+++ sources/engine/SiliconStudio.Xenko.Assets/Materials/MaterialFactories.cs
@@ -145,58 +145,59 @@ namespace SiliconStudio.Xenko.Assets.Materials
         }
     }
 
-    public class CarPaintMaterialFactory : AssetFactory<MaterialAsset>
+    public class ClearCoatMaterialFactory : AssetFactory<MaterialAsset>
     {
         public static MaterialAsset Create()
         {
-            // Mazda Soul Red Paint (Approximation)
-            // Color4 defaultCarPaintColor = new Color4(0.274509817f, 0.003921569f, 0.0470588244f, 1.0f);
-            // Color4 defaultMetalFlakesColor = new Color4(defaultCarPaintColor.R * 2.0f, defaultCarPaintColor.G * 2.0f, defaultCarPaintColor.B * 2.0f, 1.0f);
-            
-            var ClearCoatLayerNormalMap = new ComputeTextureColor
+            // Load default texture assets
+            var clearCoatLayerNormalMap = new ComputeTextureColor
             {
                 Texture = AttachedReferenceManager.CreateProxyObject<Texture>(new AssetId("2f76bcba-ae9f-4954-b98d-f94c2102ff86"), "XenkoCarPaintOrangePeelNM"),
                 Scale = new Vector2(8, 8)
             };
             
-            var MetalFlakesNormalMap = new ComputeTextureColor
+            var metalFlakesNormalMap = new ComputeTextureColor
             {
                 Texture = AttachedReferenceManager.CreateProxyObject<Texture>(new AssetId("7e2761d1-ef86-420a-b7a7-a0ed1c16f9bb"), "XenkoCarPaintMetalFlakesNM"),
                 Scale = new Vector2(128, 128),
                 UseRandomTexCoordinates = true
             };
 
-            var MetalFlakesMask = new ComputeTextureScalar
+            var metalFlakesMask = new ComputeTextureScalar
             {
                 Texture = AttachedReferenceManager.CreateProxyObject<Texture>(new AssetId("7e2761d1-ef86-420a-b7a7-a0ed1c16f9bb"), "XenkoCarPaintMetalFlakesNM"),
                 Scale = new Vector2(128, 128),
                 UseRandomTexCoordinates = true
             };
 
-            // Blue Car Paint
-            Color4 defaultCarPaintColor = new Color4(0, 0.09411765f, 0.329411775f, 1.0f);
+            // Red Paint
+            // Color4 defaultCarPaintColor = new Color4(0.274509817f, 0.003921569f, 0.0470588244f, 1.0f);
+            // Color4 defaultMetalFlakesColor = new Color4(defaultCarPaintColor.R * 2.0f, defaultCarPaintColor.G * 2.0f, defaultCarPaintColor.B * 2.0f, 1.0f);
+
+            // Blue Paint
+            Color4 defaultPaintColor = new Color4(0, 0.09411765f, 0.329411775f, 1.0f);
             Color4 defaultMetalFlakesColor = new Color4(0, 0.180392161f, 0.6313726f, 1.0f);
 
             var material = new MaterialAsset
             {
                 Attributes =
                 {
-                    Diffuse = new MaterialDiffuseMapFeature(new ComputeColor(defaultCarPaintColor)),
+                    Diffuse = new MaterialDiffuseMapFeature(new ComputeColor(defaultPaintColor)),
                     DiffuseModel = new MaterialDiffuseLambertModelFeature(),
 
                     SpecularModel = new MaterialSpecularMicrofacetModelFeature(),
                     Specular = new MaterialMetalnessMapFeature(new ComputeFloat(1.00f)),
 
-                    MicroSurface = new MaterialGlossinessMapFeature(new ComputeBinaryScalar(new ComputeFloat(2.00f), MetalFlakesMask, BinaryOperator.Multiply)),
+                    MicroSurface = new MaterialGlossinessMapFeature(new ComputeBinaryScalar(new ComputeFloat(2.00f), metalFlakesMask, BinaryOperator.Multiply)),
 
-                    Surface = new MaterialNormalMapFeature(MetalFlakesNormalMap),
+                    Surface = new MaterialNormalMapFeature(metalFlakesNormalMap),
 
                     ClearCoat = new MaterialClearCoatFeature
                     {
-                        BasePaintGlossinessMap = new ComputeBinaryScalar(new ComputeFloat(0.00f), MetalFlakesMask, BinaryOperator.Multiply),
+                        BasePaintGlossinessMap = new ComputeBinaryScalar(new ComputeFloat(0.00f), metalFlakesMask, BinaryOperator.Multiply),
 
                         ClearCoatGlossinessMap = new ComputeFloat(1.00f),
-                        ClearCoatLayerNormalMap = ClearCoatLayerNormalMap,
+                        ClearCoatLayerNormalMap = clearCoatLayerNormalMap,
                         ScaleAndBiasOrangePeel = true,
                         ClearCoatMetalnessMap = new ComputeFloat(0.50f),
 
